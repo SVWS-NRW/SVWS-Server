b03362a0ae0e36b7d30661aefb27b377c7c05ad5
Frank Pfotenhauer <pfotenhauer@schildnrw.de>
2022-09-26 18:02:07 +0200

    Alle Tests SchulformnummerByKuerzel ergänzt.

--- svws-core/src/test/java/de/nrw/schule/svws/core/types/statkue/test/TestCoreTypesStatkue.java
+++ svws-core/src/test/java/de/nrw/schule/svws/core/types/statkue/test/TestCoreTypesStatkue.java
@@ -31,7 +31,26 @@ public class TestCoreTypesStatkue {
     void testSchulform() {
     	assertTrue(Schulform.GY.daten.hatGymOb);
     	assertEquals(Schulform.R, Schulform.getByKuerzel("R"));
+    	assertEquals(Schulform.G, Schulform.getByNummer("02"));
+    	assertEquals(Schulform.H, Schulform.getByNummer("04"));
+    	assertEquals(Schulform.V, Schulform.getByNummer("06"));
+    	assertEquals(Schulform.S, Schulform.getByNummer("08"));
+    	assertEquals(Schulform.PS, Schulform.getByNummer("13"));
+    	assertEquals(Schulform.SK, Schulform.getByNummer("14"));
+    	assertEquals(Schulform.R, Schulform.getByNummer("10"));
     	assertEquals(Schulform.GE, Schulform.getByNummer("15"));
+    	assertEquals(Schulform.GM, Schulform.getByNummer("16"));
+    	assertEquals(Schulform.FW, Schulform.getByNummer("17"));
+    	assertEquals(Schulform.HI, Schulform.getByNummer("18"));
+    	assertEquals(Schulform.FW, Schulform.getByNummer("17"));
+    	assertEquals(Schulform.WF, Schulform.getByNummer("19"));
+    	assertEquals(Schulform.GY, Schulform.getByNummer("20"));
+    	assertEquals(Schulform.WB, Schulform.getByNummer("25"));
+    	assertEquals(Schulform.BK, Schulform.getByNummer("30"));
+    	assertEquals(Schulform.KS, Schulform.getByNummer("83"));
+    	assertEquals(Schulform.SR, Schulform.getByNummer("85"));
+    	assertEquals(Schulform.SG, Schulform.getByNummer("87"));
+    	assertEquals(Schulform.SB, Schulform.getByNummer("88"));
     	// TODO add additional tests
     }
     
