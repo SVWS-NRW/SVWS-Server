5385832f44295d32638c7024631be7646744165b
Frank Pfotenhauer <pfotenhauer@schildnrw.de>
2022-09-27 10:28:47 +0200

    Weitere Tests Schulfom by Kuerzel. Alle Schulformen ergänzt.

--- svws-core/src/test/java/de/nrw/schule/svws/core/types/statkue/test/TestCoreTypesStatkue.java
+++ svws-core/src/test/java/de/nrw/schule/svws/core/types/statkue/test/TestCoreTypesStatkue.java
@@ -30,7 +30,25 @@ public class TestCoreTypesStatkue {
     @DisplayName("Teste den Typ Schulform")
     void testSchulform() {
     	assertTrue(Schulform.GY.daten.hatGymOb);
+    	assertEquals(Schulform.G, Schulform.getByKuerzel("G"));
+    	assertEquals(Schulform.H, Schulform.getByKuerzel("H"));
+    	assertEquals(Schulform.V, Schulform.getByKuerzel("V"));
+    	assertEquals(Schulform.S, Schulform.getByKuerzel("S"));
+    	assertEquals(Schulform.PS, Schulform.getByKuerzel("PS"));
+    	assertEquals(Schulform.SK, Schulform.getByKuerzel("SK"));
     	assertEquals(Schulform.R, Schulform.getByKuerzel("R"));
+    	assertEquals(Schulform.GE, Schulform.getByKuerzel("GE"));
+    	assertEquals(Schulform.GM, Schulform.getByKuerzel("GM"));
+    	assertEquals(Schulform.FW, Schulform.getByKuerzel("FW"));
+    	assertEquals(Schulform.HI, Schulform.getByKuerzel("HI"));
+    	assertEquals(Schulform.WF, Schulform.getByKuerzel("WF"));
+    	assertEquals(Schulform.GY, Schulform.getByKuerzel("GY"));
+    	assertEquals(Schulform.WB, Schulform.getByKuerzel("WB"));
+    	assertEquals(Schulform.BK, Schulform.getByKuerzel("BK"));
+    	assertEquals(Schulform.KS, Schulform.getByKuerzel("KS"));
+    	assertEquals(Schulform.SR, Schulform.getByKuerzel("SR"));
+    	assertEquals(Schulform.SG, Schulform.getByKuerzel("SG"));
+    	assertEquals(Schulform.SB, Schulform.getByKuerzel("SB"));
     	assertEquals(Schulform.G, Schulform.getByNummer("02"));
     	assertEquals(Schulform.H, Schulform.getByNummer("04"));
     	assertEquals(Schulform.V, Schulform.getByNummer("06"));
@@ -42,7 +60,6 @@ public class TestCoreTypesStatkue {
     	assertEquals(Schulform.GM, Schulform.getByNummer("16"));
     	assertEquals(Schulform.FW, Schulform.getByNummer("17"));
     	assertEquals(Schulform.HI, Schulform.getByNummer("18"));
-    	assertEquals(Schulform.FW, Schulform.getByNummer("17"));
     	assertEquals(Schulform.WF, Schulform.getByNummer("19"));
     	assertEquals(Schulform.GY, Schulform.getByNummer("20"));
     	assertEquals(Schulform.WB, Schulform.getByNummer("25"));
