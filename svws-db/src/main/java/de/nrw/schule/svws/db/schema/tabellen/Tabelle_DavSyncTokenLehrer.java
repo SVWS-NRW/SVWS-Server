56f8ad9dca16778260a07ce111c6b37adb403489
thomas <thomas@PANDORA>
2022-10-12 08:52:38 +0200

    DAV-SyncToken Lehrer: Trigger für die Tabelle K_Ort

--- svws-db/src/main/java/de/nrw/schule/svws/db/schema/tabellen/Tabelle_DavSyncTokenLehrer.java
+++ svws-db/src/main/java/de/nrw/schule/svws/db/schema/tabellen/Tabelle_DavSyncTokenLehrer.java
@@ -36,9 +36,9 @@ public class Tabelle_DavSyncTokenLehrer extends SchemaTabelle {
 			new Pair<>(col_ID, Schema.tab_K_Lehrer.col_ID)
 		);
 
-    /** Trigger t_UPDATE_DavSyncTokenSchueler_Lehrer */
-    public SchemaTabelleTrigger trigger_MariaDB_UPDATE_DavSyncTokenSchueler_Lehrer = addTrigger(
-            "t_UPDATE_DavSyncTokenSchueler_Lehrer",
+    /** Trigger t_UPDATE_DavSyncTokenLehrer_Lehrer */
+    public SchemaTabelleTrigger trigger_MariaDB_UPDATE_DavSyncTokenLehrer_Lehrer = addTrigger(
+            "t_UPDATE_DavSyncTokenLehrer_Lehrer",
             DBDriver.MARIA_DB,
             """
             AFTER UPDATE ON K_Lehrer FOR EACH ROW
@@ -66,7 +66,67 @@ public class Tabelle_DavSyncTokenLehrer extends SchemaTabelle {
             """,
             Schema.tab_K_Lehrer, Schema.tab_DavSyncTokenLehrer);
 
-    // TODO Trigger für MariaDB 
+    /** Trigger t_UPDATE_DavSyncTokenLehrer_K_Ort */
+    public SchemaTabelleTrigger trigger_MariaDB_UPDATE_DavSyncTokenLehrer_K_Ort = addTrigger(
+            "t_UPDATE_DavSyncTokenLehrer_K_Ort",
+            DBDriver.MARIA_DB,
+            """
+            AFTER UPDATE ON K_Ort FOR EACH ROW
+            BEGIN
+                DECLARE token DATETIME;
+                FOR sid IN (SELECT ID AS Lehrer_ID FROM K_Lehrer WHERE Ort_ID = NEW.ID OR Ort_ID = OLD.ID) DO
+                    SET token := (SELECT SyncToken FROM DavSyncTokenLehrer WHERE ID = sid.Lehrer_ID);
+                    IF token IS NULL THEN
+                        INSERT INTO DavSyncTokenLehrer(ID, SyncToken) VALUES (sid.Lehrer_ID, CURTIME(3));
+                    ELSE
+                        UPDATE DavSyncTokenLehrer SET SyncToken = CURTIME(3) WHERE ID = sid.Lehrer_ID;
+                    END IF;
+                END FOR;
+            END
+            """,
+            Schema.tab_K_Ort, Schema.tab_K_Lehrer, Schema.tab_DavSyncTokenLehrer);
+
+    /** Trigger t_INSERT_DavSyncTokenLehrer_K_Ort */
+    public SchemaTabelleTrigger trigger_MariaDB_INSERT_DavSyncTokenLehrer_K_Ort = addTrigger(
+            "t_INSERT_DavSyncTokenLehrer_K_Ort",
+            DBDriver.MARIA_DB,
+            """
+            AFTER INSERT ON K_Ort FOR EACH ROW
+            BEGIN
+                DECLARE token DATETIME;
+                FOR sid IN (SELECT ID AS Lehrer_ID FROM K_Lehrer WHERE Ort_ID = NEW.ID) DO
+                    SET token := (SELECT SyncToken FROM DavSyncTokenLehrer WHERE ID = sid.Lehrer_ID);
+                    IF token IS NULL THEN
+                        INSERT INTO DavSyncTokenLehrer(ID, SyncToken) VALUES (sid.Lehrer_ID, CURTIME(3));
+                    ELSE
+                        UPDATE DavSyncTokenLehrer SET SyncToken = CURTIME(3) WHERE ID = sid.Lehrer_ID;
+                    END IF;
+                END FOR;
+            END
+            """,
+            Schema.tab_K_Ort, Schema.tab_K_Lehrer, Schema.tab_DavSyncTokenLehrer);
+
+    /** Trigger t_DELETE_DavSyncTokenLehrer_K_Ort */
+    public SchemaTabelleTrigger trigger_MariaDB_DELETE_DavSyncTokenLehrer_K_Ort = addTrigger(
+            "t_DELETE_DavSyncTokenLehrer_K_Ort",
+            DBDriver.MARIA_DB,
+            """
+            AFTER DELETE ON K_Ort FOR EACH ROW
+            BEGIN
+                DECLARE token DATETIME;
+                FOR sid IN (SELECT ID AS Lehrer_ID FROM K_Lehrer WHERE Ort_ID = OLD.ID) DO
+                    SET token := (SELECT SyncToken FROM DavSyncTokenLehrer WHERE ID = sid.Lehrer_ID);
+                    IF token IS NULL THEN
+                        INSERT INTO DavSyncTokenLehrer(ID, SyncToken) VALUES (sid.Lehrer_ID, CURTIME(3));
+                    ELSE
+                        UPDATE DavSyncTokenLehrer SET SyncToken = CURTIME(3) WHERE ID = sid.Lehrer_ID;
+                    END IF;
+                END FOR;
+            END
+            """,
+            Schema.tab_K_Ort, Schema.tab_K_Lehrer, Schema.tab_DavSyncTokenLehrer);
+
+    // TODO weitere Trigger für MariaDB 
 
     // TODO Trigger für SQLite
 
