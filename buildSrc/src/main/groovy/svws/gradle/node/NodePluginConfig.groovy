9c0a722a0ab5b5e5e8925da76d4c604e0f913eb5
hmt <dev@hmt.im>
2022-12-12 10:27:50 +0100

    node: update minor deps

--- buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
+++ buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
@@ -34,7 +34,7 @@ abstract class NodePluginConfig {
     	version.convention('18.12.1'); // https://nodejs.org/en/download/
     	npmVersion.convention('9.2.0');
     	tsVersion.convention('4.9.4'); // https://github.com/microsoft/TypeScript/releases
-    	tsNodeTypesVersion.convention('18.11.12'); // https://www.npmjs.com/package/@types/node
+    	tsNodeTypesVersion.convention('18.11.13'); // https://www.npmjs.com/package/@types/node
     }
     
     boolean isLinux() {
