8dbc877136046b2d4c8238b2b2c908989b4b54a9
Fabian Fushöller <fabian.fushoeller@isb-ag.de>
2022-12-16 18:08:39 +0100

    docker: automatischer download der testdatenbanken von github und import beim hochfahren des containers

--- deployment/docker/init-scripts/import-test-db.sh
+++ deployment/docker/init-scripts/import-test-db.sh
@@ -3,8 +3,18 @@
 CURRENT_SCRIPT=$(readlink -f "$0")
 CURRENT_DIR=$(dirname "$CURRENT_SCRIPT")
 
-echo "Starte Testdaten-Import ..."
-MDBFILE=$(ls "$CURRENT_DIR"/*.mdb| head -1)
+# Testdatenbanken von github als zip-File herunterladen
+echo "Lade Testdatenbanken herunter ..."
+wget https://github.com/SVWS-NRW/SVWS-TestMDBs/archive/refs/heads/main.zip -O $CURRENT_DIR/databases.zip
+
+# Access-Datenbanken aus den heruntergeladenen zip-File extrahieren
+echo "Extrahiere Access-Datenbanken ..."
+unzip $CURRENT_DIR/databases.zip -d $CURRENT_DIR/databases "*.mdb"
+rm -rf $CURRENT_DIR/databases.zip
+
+# Import (MigrateDB) der Access-Datenbank(en) durchführen ...
+MDBFILE="$CURRENT_DIR/databases/SVWS-TestMDBs-main/GOST_Abitur/Abi-Test-Daten-01/GymAbi.mdb"
+echo "Importiere Datenbank: ${MDBFILE} ..."
 java -cp "svws-server-app-*.jar:./*:./lib/*" de.nrw.schule.svws.db.utils.app.MigrateDB -j -d -r -1 -sd "MDB" \
    -sl "${MDBFILE}" -sp "${TESTDB_PASSWORD}" \
    -td "MARIA_DB" \
