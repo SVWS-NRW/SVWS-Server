1b8eca9d63529c9f9bbafc9c4a4a2913cbd4cb2c
Antony Konstantinidis <toni@vuejs.de>
2023-01-05 16:47:09 +0000

    chore: stories fuer router tab bar komponenten
    Co-authored-by: Antony Konstantinidis <ak@digitalessence.de>

---
+++ svws-webclient/src/ui-components/ts/src/components/Controls/RouterVerticalTabBar.story.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+	import { ref, h } from "vue";
+	import RouterVerticalTabBar from "./RouterVerticalTabBar.vue";
+
+	const routes = [
+		{ path: "/", name: "home", component: { render: () => h("h1", "Home") }, meta: { text: "Home" } },
+		{ path: "/about", name: "about", component: { render: () => h("h1", "About") }, meta: { text: "About" } },
+		{
+			path: "/settings",
+			name: "settings",
+			component: { render: () => h("h1", "Settings") },
+			meta: { text: "Settings" }
+		},
+		{ path: "/hidden", name: "hidden", component: { render: () => h("h1", "Hidden") }, meta: { text: "Hidden" } },
+		{ path: "/link1", name: "link1", component: { render: () => h("h1", "link1") }, meta: { text: "Link1" } },
+		{ path: "/link2", name: "link2", component: { render: () => h("h1", "link2") }, meta: { text: "Link2" } },
+		{ path: "/link3", name: "link3", component: { render: () => h("h1", "link3") }, meta: { text: "Link3" } }
+	];
+
+	const hidden = ref([false, false, false, true]);
+	const selectedRoute = ref(routes[0]);
+</script>
+
+<template>
+	<Story title="SVWS UI/Controls/Router Vertical Tab Bar">
+		<RouterVerticalTabBar :routes="routes" :hidden="hidden" v-model="selectedRoute">
+			<component :is="selectedRoute.component"></component>
+		</RouterVerticalTabBar>
+	</Story>
+</template>
