6bb67ca0437ed1e7d400ae84321b7a8e3e6eae18
hmt <dev@hmt.im>
2023-01-16 08:53:56 +0100

    node: minor deps update & node 18.13

--- buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
+++ buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
@@ -31,7 +31,7 @@ abstract class NodePluginConfig {
     	this.project = p;
     	this.rootProject = p.rootProject;
     	url.convention('https://nodejs.org/dist/v');
-    	version.convention('18.12.1'); // https://nodejs.org/en/download/
+    	version.convention('18.13.0'); // https://nodejs.org/en/download/
     	npmVersion.convention('9.2.0');
     	tsVersion.convention('4.9.4'); // https://github.com/microsoft/TypeScript/releases
     	tsNodeTypesVersion.convention('18.11.18'); // https://www.npmjs.com/package/@types/node
