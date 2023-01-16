acdf15db823e572ed85e9b9a9721eb0171243480
hmt <dev@hmt.im>
2023-01-16 12:40:52 +0100

    client: verwende svg-images statt inline svg

--- svws-webclient/src/webclient/ts/src/components/gost/SGostApp.vue
+++ svws-webclient/src/webclient/ts/src/components/gost/SGostApp.vue
@@ -12,15 +12,7 @@
 		</div>
 	</div>
 	<div v-else class="app-layout--main--placeholder">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1.2em" height="1.2em">
-			<g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-				stroke-width="2">
-				<path
-					d="m2.573 8.463l8.659-4.329a.6.6 0 0 1 .536 0l8.659 4.33a.6.6 0 0 1 0 1.073l-8.659 4.329a.6.6 0 0 1-.536 0l-8.659-4.33a.6.6 0 0 1 0-1.073Z" />
-				<path
-					d="M22.5 13V9.5l-2-1m-16 2v5.412a2 2 0 0 0 1.142 1.807l5 2.374a2 2 0 0 0 1.716 0l5-2.374a2 2 0 0 0 1.142-1.807V10.5" />
-			</g>
-		</svg>
+		<img src="/images/gradcap.svg" alt="Platzhalter">
 	</div>
 </template>
 
@@ -35,7 +27,7 @@
 
 	import { routeGost } from "~/router/apps/RouteGost";
 
-	const { item } = defineProps<{
+	const props = defineProps<{
 		item: ShallowRef<GostJahrgang | undefined>;
 		schule: DataSchuleStammdaten;
 		jahrgangsdaten: DataGostJahrgang;
@@ -47,11 +39,11 @@
 
 
 	const jahrgang: ComputedRef<string | undefined> = computed(() => {
-		return item.value?.jahrgang?.toString();
+		return props.item.value?.jahrgang?.toString();
 	});
 
 	const bezeichnung_abiturjahr: ComputedRef<string | undefined> = computed(() => {
-		return item.value?.bezeichnung?.toString();
+		return props.item.value?.bezeichnung?.toString();
 	});
 
 </script>
