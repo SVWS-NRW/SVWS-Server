d6adee20c74620099e358d0d827538406f85e3d4
thomas <thomas@PANDORA>
2023-01-25 14:38:17 +0100

    Removed unused import

--- svws-db-utils/src/main/java/de/nrw/schule/svws/data/benutzer/DataBenutzerliste.java
+++ svws-db-utils/src/main/java/de/nrw/schule/svws/data/benutzer/DataBenutzerliste.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
-import java.util.Vector;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
