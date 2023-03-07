ffcf5e4c71bcfe1f508c82fa117ade921698dc1c
hmt <dev@hmt.im>
2023-03-07 09:01:21 +0100

    Klassen: Auswahlfilter

--- svws-webclient/src/webclient/ts/src/components/klassen/SKlassenAuswahl.vue
+++ svws-webclient/src/webclient/ts/src/components/klassen/SKlassenAuswahl.vue
@@ -4,10 +4,15 @@
 		<template #abschnitt>
 			<abschnitt-auswahl :akt-abschnitt="aktAbschnitt" :abschnitte="abschnitte" :set-abschnitt="setAbschnitt" :akt-schulabschnitt="aktSchulabschnitt" />
 		</template>
-		<template #header />
+		<template #header>
+			<div class="mt-6 mb-2 flex gap-2">
+				<svws-ui-text-input v-model="search" type="search" placeholder="Suche nach Klasse"><i-ri-search-line /></svws-ui-text-input>
+				<svws-ui-toggle v-model="sichtbar">Sichtbar</svws-ui-toggle>
+			</div>
+		</template>
 		<template #content>
 			<div class="container">
-				<svws-ui-data-table :clicked="auswahl" @update:clicked="setKlasse" clickable :columns="cols" :items="listKlassen" :footer="false" />
+				<svws-ui-data-table :clicked="auswahl" @update:clicked="setKlasse" clickable :columns="cols" :items="rowsFiltered" :footer="false" />
 			</div>
 		</template>
 	</svws-ui-secondary-menu>
@@ -16,9 +21,21 @@
 <script setup lang="ts">
 
 	import { DataTableColumn } from "@svws-nrw/svws-ui";
+	import { computed, ref, Ref } from "vue";
 	import { KlassenAuswahlProps } from "./SKlassenAuswahlProps";
 
 	const props = defineProps<KlassenAuswahlProps>();
+
+	const sichtbar: Ref<boolean> = ref(true);
+	const search: Ref<string> = ref("");
+
+	const rowsFiltered = computed(() => {
+		const res = [];
+		for (const k of props.listKlassen)
+			if (k.kuerzel?.toLocaleLowerCase().includes(search.value.toLocaleLowerCase()) && k.istSichtbar === sichtbar.value)
+				res.push(k);
+		return res;
+	})
 
 	const cols: DataTableColumn[] = [
 		{ key: "kuerzel", label: "Kürzel", sortable: true, defaultSort: "asc" },
