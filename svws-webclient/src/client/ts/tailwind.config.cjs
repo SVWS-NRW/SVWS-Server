8d05fe94e87ccb452a8a5274012e888aab8a65be
hmt <dev@hmt.im>
2023-03-09 18:02:31 +0100

    client: Ergänzug Pfad Components

--- svws-webclient/src/client/ts/tailwind.config.cjs
+++ svws-webclient/src/client/ts/tailwind.config.cjs
@@ -1,7 +1,11 @@
 const config = require("../../../tailwind.config-base.cjs");
 
 config.content = [
+	// UI-Framework
 	"../../ui/ts/src/**/*.{vue,js,ts,jsx,tsx}",
+	// Komponenten wie z.B. die Laufbahnplanung
+	"../../components/ts/src/**/*.{vue,js,ts,jsx,tsx}",
+	// Lokale Komponenten des Clients selbst
 	"./index.html",
 	"./src/components/**/*.{vue,js,ts,jsx,tsx}",
 ]
