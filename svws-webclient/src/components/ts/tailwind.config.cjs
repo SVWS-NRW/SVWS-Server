8e6f310e1bab72990e5ad2b0dd48846f895fa992
hmt <dev@hmt.im>
2023-03-10 18:04:20 +0100

    erster bugfix tailwind

--- svws-webclient/src/components/ts/tailwind.config.cjs
+++ svws-webclient/src/components/ts/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	presets: [ require("../../../tailwind.config-base") ],
+	presets: [ require("../../../tailwind.preset.cjs") ],
 	content: [
 		"./src/components/**/*.{vue,js,ts,jsx,tsx}",
 	],
