531155ae62c13f5bb69b90fdfc13bc843a0db421
thomas <thomas@PANDORA>
2023-03-12 13:30:27 +0100

    JUnit-Test für den Core-Type Geschlecht ergänzt

---
+++ svws-core/src/test/java/de/nrw/schule/svws/core/types/statkue/TestCoreTypeGeschlecht.java
@@ -0,0 +1,33 @@
+package de.nrw.schule.svws.core.types.statkue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import de.nrw.schule.svws.core.types.Geschlecht;
+
+
+/**
+ * Diese Klasse enthält die Testroutinen für den Core-Type Geschlecht.
+ */
+@DisplayName("Teste den Core-Type Geschlecht")
+public class TestCoreTypeGeschlecht {
+	
+    @Test
+    @DisplayName("Teste den Typ Geschlecht")
+    void testGeschlecht() {
+    	assertEquals(Geschlecht.M, Geschlecht.fromStringValue("M"));
+    	assertEquals(Geschlecht.W, Geschlecht.fromStringValue("W"));
+    	assertEquals(Geschlecht.D, Geschlecht.fromStringValue("D"));
+    	assertEquals(Geschlecht.X, Geschlecht.fromStringValue("X"));
+
+    	assertEquals(Geschlecht.M, Geschlecht.fromValue(3));
+    	assertEquals(Geschlecht.W, Geschlecht.fromValue(4));
+    	assertEquals(Geschlecht.D, Geschlecht.fromValue(5));
+    	assertEquals(Geschlecht.X, Geschlecht.fromValue(6));    	
+    }
+
+	// TODO add additional tests
+    
+}
