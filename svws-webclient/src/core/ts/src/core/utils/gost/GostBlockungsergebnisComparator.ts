2987f652ec8085eaefcbd72e2ee993aa30e43f1e
thomas <thomas@PANDORA>
2023-03-23 12:02:06 +0100

    ts-files für letzten Commit.

--- svws-webclient/src/core/ts/src/core/utils/gost/GostBlockungsergebnisComparator.ts
+++ svws-webclient/src/core/ts/src/core/utils/gost/GostBlockungsergebnisComparator.ts
@@ -22,34 +22,34 @@ export class GostBlockungsergebnisComparator extends JavaObject implements Compa
 	public compare(o1 : GostBlockungsergebnisListeneintrag, o2 : GostBlockungsergebnisListeneintrag) : number {
 		let b1 : GostBlockungsergebnisBewertung = o1.bewertung;
 		let b2 : GostBlockungsergebnisBewertung = o2.bewertung;
-		const o1Wert1 : number = b1.regelVerletzungen.size() + b1.anzahlKurseNichtZugeordnet;
-		const o2Wert1 : number = b2.regelVerletzungen.size() + b2.anzahlKurseNichtZugeordnet;
+		let o1Wert1 : number = b1.regelVerletzungen.size() + b1.anzahlKurseNichtZugeordnet;
+		let o2Wert1 : number = b2.regelVerletzungen.size() + b2.anzahlKurseNichtZugeordnet;
 		if (o1Wert1 < o2Wert1)
 			return -1;
 		if (o1Wert1 > o2Wert1)
 			return +1;
-		const o1Wert2 : number = b1.anzahlSchuelerNichtZugeordnet + b1.anzahlSchuelerKollisionen;
-		const o2Wert2 : number = b2.anzahlSchuelerNichtZugeordnet + b2.anzahlSchuelerKollisionen;
+		let o1Wert2 : number = b1.anzahlSchuelerNichtZugeordnet + b1.anzahlSchuelerKollisionen;
+		let o2Wert2 : number = b2.anzahlSchuelerNichtZugeordnet + b2.anzahlSchuelerKollisionen;
 		if (o1Wert2 < o2Wert2)
 			return -1;
 		if (o1Wert2 > o2Wert2)
 			return +1;
-		const kdMax1 : number = b1.kursdifferenzMax;
-		const kdMax2 : number = b2.kursdifferenzMax;
+		let kdMax1 : number = b1.kursdifferenzMax;
+		let kdMax2 : number = b2.kursdifferenzMax;
 		if (kdMax1 < kdMax2)
 			return -1;
 		if (kdMax1 > kdMax2)
 			return +1;
-		const o1Kursdifferenzen : Array<number> | null = b1.kursdifferenzHistogramm;
-		const o2Kursdifferenzen : Array<number> | null = b2.kursdifferenzHistogramm;
+		let o1Kursdifferenzen : Array<number> | null = b1.kursdifferenzHistogramm;
+		let o2Kursdifferenzen : Array<number> | null = b2.kursdifferenzHistogramm;
 		for (let i : number = kdMax1; i >= 0; i--) {
 			if (o1Kursdifferenzen[i] < o2Kursdifferenzen[i])
 				return -1;
 			if (o1Kursdifferenzen[i] > o2Kursdifferenzen[i])
 				return +1;
 		}
-		const o1Wert4 : number = b1.anzahlKurseMitGleicherFachartProSchiene;
-		const o2Wert4 : number = b2.anzahlKurseMitGleicherFachartProSchiene;
+		let o1Wert4 : number = b1.anzahlKurseMitGleicherFachartProSchiene;
+		let o2Wert4 : number = b2.anzahlKurseMitGleicherFachartProSchiene;
 		if (o1Wert4 < o2Wert4)
 			return -1;
 		if (o1Wert4 > o2Wert4)
