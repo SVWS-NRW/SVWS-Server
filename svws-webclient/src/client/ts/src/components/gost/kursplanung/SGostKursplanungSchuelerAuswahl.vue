f4358e82e16e7941cd58e5c9a3ede624a0693c25
Jonas Kunst <info@jonaskun.st>
2023-04-04 11:06:40 +0200

    WIP: UI: GOST Kursplanung Schülerauswahl

--- svws-webclient/src/client/ts/src/components/gost/kursplanung/SGostKursplanungSchuelerAuswahl.vue
+++ svws-webclient/src/client/ts/src/components/gost/kursplanung/SGostKursplanungSchuelerAuswahl.vue
@@ -1,53 +1,64 @@
 <template>
 	<svws-ui-content-card>
-		<div class="flex justify-between items-center">
-			<svws-ui-checkbox v-model="kurs_filter_toggle" class="">Kursfilter<span v-if="kurs_filter_toggle">:</span></svws-ui-checkbox>
-			<svws-ui-multi-select v-if="kurs_filter_toggle" v-model="schuelerFilter.kurs.value" :items="schuelerFilter.getKurse()" headless
-				:item-text="(kurs: GostBlockungKurs) => getErgebnismanager().getOfKursName(kurs.id) ?? ''" class="w-48" />
-		</div>
-		<div class="flex justify-between items-center mb-3">
-			<svws-ui-checkbox v-model="fach_filter_toggle" class=""> Fachfilter<span v-if="fach_filter_toggle">:</span></svws-ui-checkbox>
-			<svws-ui-multi-select v-if="fach_filter_toggle" v-model="fach" :items="faecherManager.toVector()" headless :item-text="(fach: GostFach) => fach.bezeichnung ?? ''" class="w-32" />
-			<svws-ui-multi-select v-if="fach_filter_toggle" v-model="schuelerFilter.kursart.value" :items="GostKursart.values()" headless :item-text="(kursart: GostKursart) => kursart.kuerzel" class="w-16" />
-		</div>
-		<div class="mb-3">
-			<svws-ui-radio-group class="radio--row">
-				<svws-ui-radio-option v-model="radio_filter" value="alle" name="Alle" label="Alle" :icon="false" />
-				<svws-ui-radio-option v-model="radio_filter" value="kollisionen" name="Kollisionen" label="Kollisionen">
-					<i-ri-alert-line />
-				</svws-ui-radio-option>
-				<svws-ui-radio-option v-model="radio_filter" value="nichtwahlen" name="Nichtwahlen" label="Nichtverteilt">
-					<i-ri-forbid-2-line />
-				</svws-ui-radio-option>
-				<svws-ui-radio-option v-model="radio_filter" value="kollisionen_nichtwahlen" name="Kollisionen_Nichtwahlen" label="K/N">
-					<i-ri-alert-fill />
-				</svws-ui-radio-option>
-			</svws-ui-radio-group>
-		</div>
-		<div class="mb-2 relative z-20">
-			<svws-ui-text-input v-model="schuelerFilter.name.value" type="search" placeholder="Suche nach Namen">
-				<i-ri-search-line />
-			</svws-ui-text-input>
-		</div>
-		<div class="sticky h-8 -mt-8 -top-8 bg-white z-10" />
-		<div class="v-table--container v-table--rows-white">
-			<table class="v-table--complex">
-				<thead>
-					<tr>
-						<th class="table--th-padding">
-							Schülerauswahl
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<s-kurs-schueler-schueler v-for="s in schuelerFilter.filtered.value.values()" :key="s.id" :schueler="s" :selected="selected === s" @click="selected = s"
-						:get-ergebnismanager="getErgebnismanager" :schueler-filter="schuelerFilter" />
-					<tr v-if="!schuelerFilter.filtered.value.size">
-						<td class="opacity-50 text-sm">Keine Schüler zu diesem Filter gefunden.</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+		<svws-ui-data-table v-model="schuelerFilter.filtered"
+							v-model:clicked="selected"
+							clickable
+							:filter="true"
+							:filter-open="true">
+			<template #search>
+				<svws-ui-text-input type="search" v-model="schuelerFilter.name.value" placeholder="Suche" />
+			</template>
+			<template #filter>
+				<div class="flex justify-between items-center">
+					<svws-ui-checkbox v-model="kurs_filter_toggle" class="">Kursfilter<span v-if="kurs_filter_toggle">:</span></svws-ui-checkbox>
+					<svws-ui-multi-select v-if="kurs_filter_toggle" v-model="schuelerFilter.kurs.value" :items="schuelerFilter.getKurse()" headless
+										  :item-text="(kurs: GostBlockungKurs) => getErgebnismanager().getOfKursName(kurs.id) ?? ''" class="w-48" />
+				</div>
+				<div class="flex justify-between items-center mb-3">
+					<svws-ui-checkbox v-model="fach_filter_toggle" class=""> Fachfilter<span v-if="fach_filter_toggle">:</span></svws-ui-checkbox>
+					<svws-ui-multi-select v-if="fach_filter_toggle" v-model="fach" :items="faecherManager.toVector()" headless :item-text="(fach: GostFach) => fach.bezeichnung ?? ''" class="w-32" />
+					<svws-ui-multi-select v-if="fach_filter_toggle" v-model="schuelerFilter.kursart.value" :items="GostKursart.values()" headless :item-text="(kursart: GostKursart) => kursart.kuerzel" class="w-16" />
+				</div>
+				<div class="mb-3">
+					<svws-ui-radio-group class="radio--row">
+						<svws-ui-radio-option v-model="radio_filter" value="alle" name="Alle" label="Alle" :icon="false" />
+						<svws-ui-radio-option v-model="radio_filter" value="kollisionen" name="Kollisionen" label="Kollisionen">
+							<i-ri-alert-line />
+						</svws-ui-radio-option>
+						<svws-ui-radio-option v-model="radio_filter" value="nichtwahlen" name="Nichtwahlen" label="Nichtverteilt">
+							<i-ri-forbid-2-line />
+						</svws-ui-radio-option>
+						<svws-ui-radio-option v-model="radio_filter" value="kollisionen_nichtwahlen" name="Kollisionen_Nichtwahlen" label="K/N">
+							<i-ri-alert-fill />
+						</svws-ui-radio-option>
+					</svws-ui-radio-group>
+				</div>
+			</template>
+			<template #header>
+				<div role="row" class="data-table__tr data-table__thead__tr">
+					<div role="columnheader"
+						 class="data-table__th data-table__thead__th">
+						<div class="data-table__th-wrapper">
+							<span class="data-table__th-title">
+								Schülerauswahl
+							</span>
+						</div>
+					</div>
+				</div>
+			</template>
+			<template #body>
+				<div role="row"
+					 class="data-table__tr data-table__tbody__tr"
+					 v-for="(s, index) in schuelerFilter.filtered.value.values()"
+					 :key="index">
+					<s-kurs-schueler-schueler :schueler="s" :selected="selected === s" @click="selected = s"
+											  :get-ergebnismanager="getErgebnismanager" :schueler-filter="schuelerFilter" />
+				</div>
+				<tr v-if="!schuelerFilter.filtered.value.size">
+					<td class="opacity-50 text-sm">Keine Schüler zu diesem Filter gefunden.</td>
+				</tr>
+			</template>
+		</svws-ui-data-table>
 	</svws-ui-content-card>
 </template>
 
@@ -73,7 +84,7 @@
 		set: (value) => props.schuelerFilter.fach.value = value?.id
 	})
 
-	// const filtered = props.schuelerFilter.filtered();
+	// const filtered = props.schuelerFilter.filtered;
 
 	const selected: WritableComputedRef<SchuelerListeEintrag | undefined> = computed({
 		get: () => props.schueler,
--- svws-webclient/src/client/ts/src/components/gost/kursplanung/umwahlansicht/SKursSchuelerSchueler.vue
+++ svws-webclient/src/client/ts/src/components/gost/kursplanung/umwahlansicht/SKursSchuelerSchueler.vue
@@ -1,46 +1,44 @@
 <template>
-	<tr class="cursor-pointer" :class="{ 'selected': selected }">
-		<td>
-			<div class="flex justify-between items-center">
-				<span>
-					{{ `${schueler.nachname}, ${schueler.vorname}` }}
-				</span>
-				<div class="flex items-center">
-					<svws-ui-badge v-if="schueler.status !== 2" type="light" size="big" class="mr-1"> {{ SchuelerStatus.fromID(schueler.status) }} </svws-ui-badge>
-					<div class="leading-none overflow-hidden w-5"
-						style="margin-bottom: -0.1em;"
-						:class="{
+	<div role="cell" class="data-table__td">
+		<div class="flex items-center gap-1">
+			<span>
+				{{ `${schueler.nachname}, ${schueler.vorname}` }}
+			</span>
+			<div class="flex items-center">
+				<svws-ui-badge v-if="schueler.status !== 2" type="light" class="mr-1"> {{ SchuelerStatus.fromID(schueler.status) }} </svws-ui-badge>
+				<div class="leading-none overflow-hidden w-5"
+					 style="margin-bottom: -0.1em;"
+					 :class="{
 							'color--error': kollision,
 							'color--dark': !kollision,
 						}">
-						<svws-ui-popover v-if="kollision && !nichtwahl" class="popper--dark">
-							<template #trigger>
-								<svws-ui-icon> <i-ri-alert-line /> </svws-ui-icon>
-							</template>
-							<template #content>
-								<span>Kollision</span>
-							</template>
-						</svws-ui-popover>
-						<svws-ui-popover v-else-if="!kollision && nichtwahl" class="popper--dark">
-							<template #trigger>
-								<svws-ui-icon> <i-ri-forbid-2-line /> </svws-ui-icon>
-							</template>
-							<template #content>
-								<span>Nichtverteilt</span>
-							</template>
-						</svws-ui-popover>
-						<svws-ui-popover v-else-if="kollision && nichtwahl" class="popper--danger">
-							<template #trigger>
-								<svws-ui-icon> <i-ri-alert-fill /> </svws-ui-icon>
-							</template>
-							<template #content>
-								<span>Kollision und Nichtverteilt</span>
-							</template>
-						</svws-ui-popover>
-						<svws-ui-icon v-else class="opacity-25"> <i-ri-check-fill /> </svws-ui-icon>
-					</div>
+					<svws-ui-tooltip v-if="kollision && !nichtwahl">
+						<i-ri-alert-line />
+						<template #content>
+							<span>Kollision</span>
+						</template>
+					</svws-ui-tooltip>
+					<svws-ui-tooltip v-else-if="!kollision && nichtwahl">
+						<i-ri-forbid-2-line />
+						<template #content>
+							<span>Nichtverteilt</span>
+						</template>
+					</svws-ui-tooltip>
+					<svws-ui-tooltip v-else-if="kollision && nichtwahl" color="danger">
+						<i-ri-alert-fill />
+						<template #content>
+							<span>Kollision und Nichtverteilt</span>
+						</template>
+					</svws-ui-tooltip>
+					<svws-ui-icon v-else class="opacity-25"> <i-ri-check-fill /> </svws-ui-icon>
 				</div>
 			</div>
+
+		</div>
+	</div>
+	<tr class="cursor-pointer" :class="{ 'selected': selected }">
+		<td>
+			<div class="flex justify-between items-center"></div>
 		</td>
 	</tr>
 </template>
