3b40efdbe6010a46cbee54877e603acaf4acf550
hmt <dev@hmt.im>
2023-06-09 16:37:12 +0200

    Kursmodal ergänzt

---
+++ svws-webclient/src/client/ts/src/components/gost/kursplanung/SGostKursplanungUngueltigeKurswahlModal.vue
@@ -0,0 +1,34 @@
+<template>
+	<svws-ui-modal ref="modal" size="small">
+		<template #modalTitle>Ungültige Schüler-Kurszuordnungen</template>
+		<template #modalDescription>
+			Die folgenden Schüler haben ungültige Kurszuordnungen in dieser Blockung
+		</template>
+		<template #modalActions>
+			<svws-ui-button type="secondary" @click="modal.closeModal()">Abbrechen</svws-ui-button>
+			<svws-ui-button @click="remove_blockung">Ja</svws-ui-button>
+		</template>
+	</svws-ui-modal>
+</template>
+
+<script setup lang="ts">
+	import type { GostBlockungsergebnisManager } from '@core';
+	import { ref } from 'vue';
+
+	const props = defineProps<{
+		getErgebnismanager: () => GostBlockungsergebnisManager;
+	}>();
+
+
+		console.log(props.getErgebnismanager().getMappingSchuelerIDzuUngueltigeKurse())
+
+	const modal = ref();
+
+	async function remove_blockung() {
+		modal.value.closeModal()
+	}
+
+	const openModal = () => {
+		modal.value.openModal();
+	}
+</script>
