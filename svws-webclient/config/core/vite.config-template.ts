e3f2e2a0a178fd6d2ddc20e7b29ec46709d51033
hmt <dev@hmt.im>
2023-07-11 14:01:58 +0200

    Bugfix: Templates für TS anpassen

--- svws-webclient/config/core/vite.config-template.ts
+++ svws-webclient/config/core/vite.config-template.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  test: {},
-  resolve: {
-	alias: [{ find: "~", replacement: resolve(__dirname, '../../../src/core/ts/src') }],
-  },
-  build: { emptyOutDir: true }
+	test: {},
+	resolve: {
+		alias: [{ find: "~", replacement: resolve(__dirname, '../../../src/core/ts/src') }],
+	},
+	build: { emptyOutDir: true }
 })
--- svws-webclient/config/core/vitest.config-template.ts
+++ svws-webclient/config/core/vitest.config-template.ts
@@ -2,17 +2,18 @@ import { defineConfig } from "vitest/config";
 import { resolve } from 'path'
 
 export default defineConfig({
-  test: {
-    globals: true,
-    root: "../../../src",
-    reporters: ["default", "junit"],
-    outputFile: "../build/coverage/core/junit.xml",
-    coverage: {
-      provider: "v8",
-      reportsDirectory: "../build/coverage/core",
-    },
-  },
-  resolve: {
-	alias: [{ find: "~", replacement: resolve(__dirname, '../../../src/core/ts/src') }],
-  },
+	test: {
+		globals: true,
+		root: "../../../src",
+		reporters: ["default", "junit"],
+		outputFile: "../build/coverage/core/junit.xml",
+		coverage: {
+			provider: "v8",
+			reportsDirectory: "../build/coverage/core",
+		},
+		exclude: ["browser-test"]
+	},
+	resolve: {
+		alias: [{ find: "~", replacement: resolve(__dirname, '../../../src/core/ts/src') }],
+	},
 });
