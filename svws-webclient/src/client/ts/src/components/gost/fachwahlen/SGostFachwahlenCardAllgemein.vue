e0ca1aa552018b43a4c967bc9d339700d6fffe17
Thomas Bachran <thomas.bachran@gmx.de>
2023-07-12 08:10:43 +0200

    Gost-Fachwahlen: Zellen auswählbar (wip)

---
+++ svws-webclient/src/client/ts/src/components/gost/fachwahlen/SGostFachwahlenCardAllgemein.vue
@@ -0,0 +1,13 @@
+<template>
+	<svws-ui-content-card :title="`Weitere Informationen zu allen Fächern`">
+		<!-- -->
+	</svws-ui-content-card>
+</template>
+
+<script setup lang="ts">
+
+	import type { GostFachwahlenCardAllgemeinProps } from "./SGostFachwahlenCardAllgemeinProps";
+
+	const props = defineProps<GostFachwahlenCardAllgemeinProps>();
+
+</script>
---
+++ svws-webclient/src/client/ts/src/components/gost/fachwahlen/SGostFachwahlenCardAllgemeinProps.ts
@@ -0,0 +1,5 @@
+import type { List, GostStatistikFachwahl } from "@core";
+
+export interface GostFachwahlenCardAllgemeinProps {
+	fachwahlen: List<GostStatistikFachwahl>;
+}
