20f64b12ec7bfec574ab7daff49d24f1d7c78fd7
Thomas Bachran <thomas.bachran@gmx.de>
2023-07-12 08:10:31 +0200

    Gost-Fachwahlen: Zellen auswählbar (wip)

--- svws-webclient/src/client/ts/src/components/gost/fachwahlen/SGostFachwahlen.vue
+++ svws-webclient/src/client/ts/src/components/gost/fachwahlen/SGostFachwahlen.vue
@@ -1,52 +1,69 @@
 <template>
-	<div class="page--content">
-		<svws-ui-content-card class="col-span-full table--with-background">
-			<svws-ui-data-table :items="[]" :no-data="false" :columns="cols">
-				<template #header>
-					<div role="row" class="data-table__tr data-table__thead__tr data-table__thead__tr__compact select-none">
-						<div role="columnheader" v-for="heading in colHeadings" :key="heading.text" class="data-table__th data-table__thead__th data-table__th__align-center data-table__th__separate"
-							:class="{ [`col-span-${heading.cols.length}`]: true }">
-							<div class="data-table__th-wrapper">
-								<div class="data-table__th-title">
-									{{ heading.text }}
-								</div>
-							</div>
-						</div>
-					</div>
-					<div role="row" class="data-table__tr data-table__thead__tr data-table__thead__tr__compact select-none">
-						<template v-for="heading in colHeadings" :key="heading.text">
-							<div role="columnheader" v-for="(h, n) in heading.cols" :key="n" class="data-table__th data-table__thead__th"
-								:class="{ 'data-table__th__align-center': h.center, 'data-table__th__separate': (n === heading.cols.length - 1) }">
+	<div class="page--content page--content--full min-w-fit gap-x-8 2xl:gap-x-16 relative">
+		<div class="flex-grow">
+			<svws-ui-content-card class="table--with-background">
+				<svws-ui-data-table :items="[]" :no-data="false" :columns="cols" panel-height overflow-x-hidden>
+					<template #header>
+						<div role="row" class="data-table__tr data-table__thead__tr data-table__thead__tr__compact select-none">
+							<div role="columnheader" v-for="heading in colHeadings" :key="heading.text"
+								class="data-table__th data-table__thead__th data-table__th__align-center data-table__th__separate cursor-pointer"
+								:class="{ [`col-span-${heading.cols.length}`]: true, 'bg-svws/5 text-svws': isSelected(undefined, heading.text) }"
+								@click="selectData(undefined, heading.text)">
 								<div class="data-table__th-wrapper">
 									<div class="data-table__th-title">
-										{{ h.text }}
+										{{ heading.text }}
 									</div>
 								</div>
 							</div>
-						</template>
-					</div>
+						</div>
+						<div role="row" class="data-table__tr data-table__thead__tr data-table__thead__tr__compact select-none">
+							<template v-for="heading in colHeadings" :key="heading.text">
+								<div role="columnheader" v-for="(h, n) in heading.cols" :key="n" class="data-table__th data-table__thead__th cursor-pointer"
+									:class="{ 'data-table__th__align-center': h.center, 'data-table__th__separate': (n === heading.cols.length - 1),
+										'bg-svws/5 text-svws': isSelected(undefined, heading.text) }"
+									@click="selectData(undefined, heading.text)">
+									<div class="data-table__th-wrapper">
+										<div class="data-table__th-title">
+											{{ h.text }}
+										</div>
+									</div>
+								</div>
+							</template>
+						</div>
+					</template>
+					<template #body>
+						<div role="row" v-for="row in rows" :key="row.id" class="data-table__tr data-table__tbody__tr" :style="{ 'background-color': getBgColor(row) }">
+							<template v-for="heading in colHeadings" :key="heading.text">
+								<div role="cell" v-for="(h, n) in heading.cols" :key="n" class="data-table__td select-none"
+									:class="{
+										'data-table__th__align-center': h.center, 'data-table__th__separate': (n === heading.cols.length - 1),
+										'bg-white/75 text-svws font-bold': isSelected(row, heading.text),
+										'cursor-pointer': isSelectable(row, heading.text) }"
+									@click="selectData(row, heading.text)">
+									{{ getData(row, heading.text, h.text) }}
+								</div>
+							</template>
+						</div>
+					</template>
+				</svws-ui-data-table>
+			</svws-ui-content-card>
+		</div>
+		<div class="w-2/5 3xl:w-1/2 min-w-[36rem]">
+			<div v-if="(selected.row === undefined) && (selected.group === 'Fach')" class="flex flex-col gap-16">
+				<s-gost-fachwahlen-card-allgemein v-if="(selected.row === undefined) && (selected.group === 'Fach')" :fachwahlen="fachwahlen" />
+				<template v-if="(selected.row === undefined) && (selected.group !== 'Fach')">
+					Weitere Informationen zu allen Fächern in Bezug auf ...
 				</template>
-				<template #body>
-					<div role="row" v-for="row in rows" :key="row.id" class="data-table__tr data-table__tbody__tr" :style="{ 'background-color': getBgColor(row) }">
-						<template v-for="heading in colHeadings" :key="heading.text">
-							<div role="cell" v-for="(h, n) in heading.cols" :key="n" class="data-table__td select-none"
-								:class="{
-									'data-table__th__align-center': h.center, 'data-table__th__separate': (n === heading.cols.length - 1),
-									'bg-white/75 font-bold': isSelected(row, heading.text),
-									'cursor-pointer': isSelectable(row, heading.text) }"
-								@click="selectData(row, heading.text)">
-								{{ getData(row, heading.text, h.text) }}
-							</div>
-						</template>
-					</div>
+				<template v-if="(selected.row !== undefined)">
+					<template v-if="(selected.group === 'Fach')">
+						Fachwahlen zum Fach {{ selected.row.bezeichnung }}
+					</template>
+					<template v-if="(selected.group !== 'Fach')">
+						Fachwahlen zum Fach {{ selected.row.bezeichnung }} in Bezug auf...
+					</template>
 				</template>
-			</svws-ui-data-table>
-		</svws-ui-content-card>
-		<svws-ui-content-card :title="titleDetails">
-			<div v-if="true">
-				<!-- -->
 			</div>
-		</svws-ui-content-card>
+		</div>
 	</div>
 </template>
 
@@ -63,15 +80,54 @@
 
 	const getBgColor = (row: GostStatistikFachwahl) => ZulaessigesFach.getByKuerzelASD(row.kuerzelStatistik).getHMTLFarbeRGBA(1.0);
 
-	const selected = ref<{ row?: GostStatistikFachwahl, group?: string}>({ });
+	const selected = ref<{ row?: GostStatistikFachwahl, group: string}>({ group : "Fach" });
 
-	function selectData(row: GostStatistikFachwahl, group: string) {
-		if (isSelectable(row, group))
-			selected.value = { row, group };
+	async function doSelect(idFach: number | undefined, bereich: string | undefined, halbjahr?: GostHalbjahr) {
+		console.log(idFach, bereich);
 	}
 
-	function isSelected(row: GostStatistikFachwahl, group: string): boolean {
-		return ((selected.value.row?.id === row.id) && (selected.value.group === group));
+	async function selectData(row: GostStatistikFachwahl | undefined, group: string) {
+		// Lade die neue Auswahl
+		const fach_id = (row === undefined) ? undefined : row.id;
+		const halbjahr = GostHalbjahr.fromKuerzel(group); // nur für GKs
+		switch (group) {
+			case "Fach":
+				await doSelect(fach_id, undefined);
+				break;
+			case "ZK":
+			case "LK":
+				await doSelect(fach_id, group);
+				break;
+			case "Abiturfach":
+				await doSelect(fach_id, "Abi");
+				break;
+			default:
+				if (halbjahr !== null)
+					await doSelect(fach_id, "Halbjahr", halbjahr);
+				break;
+		}
+		// Setze die Auswahl
+		if (row === undefined) {
+			selected.value = { row: undefined, group };
+		} else if (isSelectable(row, group)) {
+			selected.value = { row, group };
+		}
+	}
+
+	function isSelected(row: GostStatistikFachwahl | undefined, group: string): boolean {
+		if (selected.value.row === undefined) {
+			// Prüfe, ob alle Fächer ausgewählt sind. In diesem Fall wird keine Auswahl angezeigt
+			if (selected.value.group === "Fach")
+				return false;
+			// Prüfe, ob eine Spalte ausgewählt ist. In diesem Fall werden alle Zellen der Spalte hervorgehoben
+			return (selected.value.group === group);
+		} else {
+			// Prüfe, ob ein Zeile, d.h. ein Fach ausgewählt ist
+			if ((selected.value.row.id === row?.id) && (selected.value.group === "Fach"))
+				return true;
+			// Prüfe, ob die jeweilige Zelle ausgewählt ist
+			return ((selected.value.row.id === row?.id) && (selected.value.group === group));
+		}
 	}
 
 	function isSelectable(row: GostStatistikFachwahl, group: string) : boolean {
@@ -79,13 +135,15 @@
 			return true;
 		if (group === "Abiturfach")
 			return (row.wahlenAB3 !== 0) || (row.wahlenAB4 !== 0);
+		if ((group == "ZK") || (group == "LK"))
+			return getData(row, group, "") !== "";
 		const hj = GostHalbjahr.fromKuerzel(group);
 		if (hj === null)
 			return false;
 		const fw = row.fachwahlen[hj.id];
 		if (fw === null)
 			return false;
-		return (fw.wahlenGK !== 0) || (fw.wahlenZK !== 0) || (fw.wahlenLK !== 0);
+		return (fw.wahlenGK !== 0);
 	}
 
 	function getData(row: GostStatistikFachwahl, group: string, item: string) {
@@ -97,6 +155,14 @@
 			return row.wahlenAB3 || "";
 		if ((group === "Abiturfach") && (item === "4"))
 			return row.wahlenAB4 || "";
+		if ((group === "ZK") && (item === "")) {
+			const maxZK = Math.max(row.fachwahlen[2].wahlenZK, row.fachwahlen[3].wahlenZK, row.fachwahlen[4].wahlenZK, row.fachwahlen[5].wahlenZK);
+			return maxZK === 0 ? "" : maxZK;
+		}
+		if ((group === "LK") && (item === "")) {
+			const maxLK = Math.max(row.fachwahlen[2].wahlenLK, row.fachwahlen[3].wahlenLK, row.fachwahlen[4].wahlenLK, row.fachwahlen[5].wahlenLK);
+			return maxLK === 0 ? "" : maxLK;
+		}
 		const hj = GostHalbjahr.fromKuerzel(group);
 		if (hj === null)
 			return "";
@@ -104,62 +170,44 @@
 		if (fw === null)
 			return "";
 		switch (item) {
-			case "M": return fw.wahlenGKMuendlich === 0 ? "" : fw.wahlenGKMuendlich;
-			case "S": return fw.wahlenGKSchriftlich === 0 ? "" : fw.wahlenGKSchriftlich;
+			case "S": return (fw.wahlenGKSchriftlich === 0 ? "" : fw.wahlenGKSchriftlich);
 			case "GK": return fw.wahlenGK === 0 ? "" : fw.wahlenGK;
-			case "ZK": return fw.wahlenZK === 0 ? "" : fw.wahlenZK;
-			case "LK": return fw.wahlenLK === 0 ? "" : fw.wahlenLK;
 		}
 		return "";
 	}
 
 	const colHeadings: Array<{ text: string, cols: Array<{ text: string, center? : boolean }> }> = [
 		{ text: "Fach", cols: [ { text : "Kürzel" }, { text: "Fach" } ] },
-		{ text: "EF.1", cols: [ { text : "M", center: true }, { text : "S", center: true }, { text : "GK", center: true } ] },
-		{ text: "EF.2", cols: [ { text : "M", center: true }, { text : "S", center: true }, { text : "GK", center: true } ] },
-		{ text: "Q1.1", cols: [ { text : "M", center: true }, { text : "S", center: true }, { text : "GK", center: true }, { text : "LK", center: true } ] },
-		{ text: "Q1.2", cols: [ { text : "M", center: true }, { text : "S", center: true }, { text : "GK", center: true }, { text : "LK", center: true } ] },
-		{ text: "Q2.1", cols: [ { text : "M", center: true }, { text : "S", center: true }, { text : "GK", center: true }, { text : "ZK", center: true }, { text : "LK", center: true } ] },
-		{ text: "Q2.2", cols: [ { text : "M", center: true }, { text : "S", center: true }, { text : "GK", center: true }, { text : "ZK", center: true }, { text : "LK", center: true } ] },
+		{ text: "EF.1", cols: [ { text : "GK", center: true }, { text : "S", center: true } ] },
+		{ text: "EF.2", cols: [ { text : "GK", center: true }, { text : "S", center: true } ] },
+		{ text: "Q1.1", cols: [ { text : "GK", center: true }, { text : "S", center: true } ] },
+		{ text: "Q1.2", cols: [ { text : "GK", center: true }, { text : "S", center: true } ] },
+		{ text: "Q2.1", cols: [ { text : "GK", center: true }, { text : "S", center: true } ] },
+		{ text: "Q2.2", cols: [ { text : "GK", center: true }, { text : "S", center: true } ] },
+		{ text: "ZK", cols: [ { text : "", center: true } ] },
+		{ text: "LK", cols: [ { text : "", center: true } ] },
 		{ text: "Abiturfach", cols: [ { text : "3", center: true }, { text : "4", center: true } ] },
 	]
 
 	const cols: Array<DataTableColumn> = [
-		{ key: "Kuerzel", label: "Kürzel", span: 0.5, minWidth: 6 },
-		{ key: "Fach", label: "Fach", span: 1.5, minWidth: 12},
-		{ key: "M", label: "M", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "S", label: "S", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "GK", label: "GK", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "M", label: "M", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "S", label: "S", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "GK", label: "GK", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "M", label: "M", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "S", label: "S", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "GK", label: "GK", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "LK", label: "LK", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "M", label: "M", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "S", label: "S", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "GK", label: "GK", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "LK", label: "LK", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "M", label: "M", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "S", label: "S", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "GK", label: "GK", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "ZK", label: "ZK", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "LK", label: "LK", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "M", label: "M", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "S", label: "S", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "GK", label: "GK", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "ZK", label: "ZK", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "LK", label: "LK", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "AB3", label: "3", align: 'center', span: 0.25, minWidth: 2.5 },
-		{ key: "AB4", label: "4", align: 'center', span: 0.25, minWidth: 2.5 }
+		{ key: "Kuerzel", label: "Kürzel", minWidth: 4 },
+		{ key: "Fach", label: "Fach", span: 3, minWidth: 12},
+		{ key: "GK", label: "GK", align: 'center', fixedWidth: 3 },
+		{ key: "S", label: "S", align: 'center', fixedWidth: 3 },
+		{ key: "GK", label: "GK", align: 'center', fixedWidth: 3 },
+		{ key: "S", label: "S", align: 'center', fixedWidth: 3 },
+		{ key: "GK", label: "GK", align: 'center', fixedWidth: 3 },
+		{ key: "S", label: "S", align: 'center', fixedWidth: 3 },
+		{ key: "GK", label: "GK", align: 'center', fixedWidth: 3 },
+		{ key: "S", label: "S", align: 'center', fixedWidth: 3 },
+		{ key: "GK", label: "GK", align: 'center', fixedWidth: 3 },
+		{ key: "S", label: "S", align: 'center', fixedWidth: 3 },
+		{ key: "GK", label: "GK", align: 'center', fixedWidth: 3 },
+		{ key: "S", label: "S", align: 'center', fixedWidth: 3 },
+		{ key: "LK", label: "LK", align: 'center', fixedWidth: 3 },
+		{ key: "ZK", label: "ZK", align: 'center', fixedWidth: 3 },
+		{ key: "AB3", label: "3", align: 'center', fixedWidth: 3 },
+		{ key: "AB4", label: "4", align: 'center', fixedWidth: 3 }
 	];
-
-	const titleDetails : ComputedRef<string> = computed(() => {
-		if (selected.value.row === undefined) {
-			return "Informationen zu allen Fächern";
-		}
-		return "Fachwahlen zum Fach " + selected.value.row.bezeichnung;
-	});
 
 </script>
