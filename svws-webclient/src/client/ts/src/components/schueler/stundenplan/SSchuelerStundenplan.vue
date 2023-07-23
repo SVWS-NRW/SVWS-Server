4bb78d54f0d5f18bba9ac3f0af2fe45ee44e92fc
ThomasBachran <thomas.bachran@gmx.de>
2023-07-23 14:01:03 +0200

    Client: Schüler-Stundenplanauswahl angepasst

--- svws-webclient/src/client/ts/src/components/schueler/stundenplan/SSchuelerStundenplan.vue
+++ svws-webclient/src/client/ts/src/components/schueler/stundenplan/SSchuelerStundenplan.vue
@@ -1,23 +1,45 @@
 <template>
-	<div v-if="mapStundenplaene.size === 0" class="flex flex-row">
-		Derzeit liegt kein Stundenplan für diesen Lernabschnitt vor.
-	</div>
-	<div v-else class="flex flex-row">
-		<svws-ui-data-table clickable :clicked="stundenplan" @update:clicked="gotoStundenplan" :columns="cols" :items="mapStundenplaene.values()" />
-		<div> <router-view :key="$route.hash" /> </div>
+	<div class="page--content">
+		<div v-if="stundenplan === undefined">
+			Derzeit liegt kein Stundenplan für diesen Lernabschnitt vor.
+		</div>
+		<div v-else class="flex flex-col">
+			<svws-ui-multi-select title="Stundenplan" v-model="stundenplan_auswahl" :items="mapStundenplaene.values()" span="full"
+				:item-text="(s: StundenplanListeEintrag) => s.bezeichnung + ' : ' + toDateStr(s.gueltigAb) + ' - ' + toDateStr(s.gueltigBis) + ' (KW ' + toKW(s.gueltigAb) + ' - ' + toKW(s.gueltigBis) + ')'" />
+			<div class="mt-2"> <router-view :key="$route.hash" /> </div>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 
+	import { type WritableComputedRef, computed } from "vue";
 	import type { SchuelerStundenplanAuswahlProps } from "./SSchuelerStundenplanAuswahlProps";
+	import { DateUtils, DeveloperNotificationException, type StundenplanListeEintrag } from "@core";
 
-	defineProps<SchuelerStundenplanAuswahlProps>();
+	const props = defineProps<SchuelerStundenplanAuswahlProps>();
 
-	const cols = [
-		{ key: "bezeichnung", label: "Bezeichnung", span: 2, sortable: false },
-		{ key: "gueltigAb", label: "von", span: 1, sortable: false, defaultSort: 'asc' },
-		{ key: "gueltigBis", label: "bis", span: 1, sortable: false }
-	];
+	const wochentag = ['So.', 'Mo.', 'Di.', 'Mi.', 'Do.', 'Fr.', 'Sa.', 'So.' ];
+
+	function toDateStr(iso: string) : string {
+		const date = DateUtils.extractFromDateISO8601(iso);
+		return wochentag[date[3] % 7] + " " + date[2] + "." + date[1] + "." + date[0];
+	}
+
+	function toKW(iso: string) : string {
+		const date = DateUtils.extractFromDateISO8601(iso);
+		return "" + date[5];
+	}
+
+	const stundenplan_auswahl : WritableComputedRef<StundenplanListeEintrag> = computed({
+		get: () : StundenplanListeEintrag => {
+			if (props.stundenplan === undefined)
+				throw new DeveloperNotificationException("Unerwarteter Fehler: Kein Stundenplan an dieser Stelle definiert.");
+			return props.stundenplan;
+		},
+		set: (value : StundenplanListeEintrag) => {
+			void props.gotoStundenplan(value);
+		}
+	});
 
 </script>
