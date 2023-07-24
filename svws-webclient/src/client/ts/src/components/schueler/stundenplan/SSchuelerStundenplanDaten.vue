764fef8c1e07472d45b434dedf065115b7f026fc
ThomasBachran <thomas.bachran@gmx.de>
2023-07-24 21:40:16 +0200

    Schüler-Stundenplan: Refactoring und Nutzung der neuen Methoden vom Stundenplan-Manager

--- svws-webclient/src/client/ts/src/components/schueler/stundenplan/SSchuelerStundenplanDaten.vue
+++ svws-webclient/src/client/ts/src/components/schueler/stundenplan/SSchuelerStundenplanDaten.vue
@@ -1,28 +1,28 @@
 <template>
 	<div class="grid gap-4 rounded-md select-none" :class="cols">
 		<div class="text-center rounded-md"> &ndash; </div>
-		<div v-for="wochentag in getRangeWochentage()" :key="wochentag.id" class="text-center rounded-md bg-svws-300">
+		<div v-for="wochentag in manager().zeitrasterGetWochentageAlsEnumRange()" :key="wochentag.id" class="text-center rounded-md bg-svws-300">
 			<div> {{ wochentage[wochentag.id] }} </div>
 		</div>
-		<template v-for="stunde in getRangeStunden()" :key="stunde">
+		<template v-for="stunde in manager().zeitrasterGetStundenRange()" :key="stunde">
 			<div class="grid gap-1 grid-cols-1 text-center rounded-md bg-yellow-200">
 				<div> {{ stunde }}. Stunde </div>
 				<div v-for="zeiten in manager().unterrichtsstundeGetUhrzeitenAsStrings(stunde)" :key="zeiten">
 					{{ zeiten }}
 				</div>
 			</div>
-			<div v-for="wochentag in getRangeWochentage()" :key="wochentag.id" class="bg-yellow-100">
-				<div v-if="hatWochentypAllgemein(wochentag, stunde)" class="flex flex-col gap-2">
-					<div v-for="unterricht in unterrichtsdaten(wochentag, stunde, 0)" :key="unterricht.id"
+			<div v-for="wochentag in manager().zeitrasterGetWochentageAlsEnumRange()" :key="wochentag.id" class="bg-yellow-100">
+				<div v-if="manager().zeitrasterHatUnterrichtMitWochentyp0ByWochentagAndStunde(wochentag, stunde)" class="flex flex-col gap-2">
+					<div v-for="unterricht in manager().unterrichtGetMengeByWochentagAndStundeAndWochentypOrEmptyList(wochentag, stunde, 0)" :key="unterricht.id"
 						class="grid gap-1 grid-cols-2 text-center rounded-md bg-cyan-100">
 						<div> {{ manager().unterrichtGetByIDStringOfFachOderKursKuerzel(unterricht.id) }} </div>
 						<div> {{ manager().unterrichtGetByIDStringOfRaeume(unterricht.id) }} </div>
 						<div> {{ manager().unterrichtGetByIDLehrerFirstAsStringOrEmpty(unterricht.id) }} </div>
 					</div>
 				</div>
-				<div v-else-if="hatWochentypSpeziell(wochentag, stunde)" class="flex flex-row gap-2">
+				<div v-else-if="manager().zeitrasterHatUnterrichtMitWochentyp1BisNByWochentagAndStunde(wochentag, stunde)" class="flex flex-row gap-2">
 					<div v-for="wochentyp in manager().getWochenTypModell()" :key="wochentyp" class="flex flex-col gap-2 bg-cyan-100">
-						<div v-for="unterricht in unterrichtsdaten(wochentag, stunde, wochentyp)" :key="unterricht.id"
+						<div v-for="unterricht in manager().unterrichtGetMengeByWochentagAndStundeAndWochentypOrEmptyList(wochentag, stunde, wochentyp)" :key="unterricht.id"
 							class="grid gap-1 grid-cols-2 text-center rounded-md">
 							<div class="col-span-2"> ({{ wochentyp }}) </div>
 							<div> {{ manager().unterrichtGetByIDStringOfFachOderKursKuerzel(unterricht.id) }} </div>
@@ -38,7 +38,6 @@
 
 <script setup lang="ts">
 
-	import { type List, type StundenplanUnterricht, Wochentag } from "@core";
 	import type { SchuelerStundenplanDatenProps } from "./SSchuelerStundenplanDatenProps";
 	import { computed } from "vue";
 
@@ -47,7 +46,7 @@
 	const props = defineProps<SchuelerStundenplanDatenProps>();
 
 	const cols = computed(() => {
-		const colcount = (getRangeWochentage().length + 1);
+		const colcount = (props.manager().zeitrasterGetWochentageAlsEnumRange().length + 1);
 		const result = {
 			'grid-cols-1': colcount === 1,
 			'grid-cols-2': colcount === 2,
@@ -59,39 +58,5 @@
 		};
 		return result;
 	});
-
-	function getRangeStunden(): number[] {
-		const min = props.manager().zeitrasterGetStundeMin();
-		const max = props.manager().zeitrasterGetStundeMax();
-		return Array.from({ length: (max-min+1) }, (value, index) => min + index);
-	}
-
-	function getRangeWochentage(): Wochentag[] {
-		const min = props.manager().zeitrasterGetWochentagMin();
-		const max = props.manager().zeitrasterGetWochentagMax();
-		return Array.from({ length: (max-min+1) }, (value, index) => Wochentag.fromIDorException(min + index));
-	}
-
-	function hatWochentypAllgemein(wochentag: Wochentag, stunde: number) : boolean {
-		if (!props.manager().zeitrasterExistsByWochentagAndStunde(wochentag.id, stunde))
-			return false;
-		const zeitraster = props.manager().zeitrasterGetByWochentagAndStundeOrException(wochentag.id, stunde);
-		return props.manager().zeitrasterHatUnterrichtMitWochentyp0(zeitraster.id);
-	}
-
-	function hatWochentypSpeziell(wochentag: Wochentag, stunde: number) : boolean {
-		if (!props.manager().zeitrasterExistsByWochentagAndStunde(wochentag.id, stunde))
-			return false;
-		const zeitraster = props.manager().zeitrasterGetByWochentagAndStundeOrException(wochentag.id, stunde);
-		return props.manager().zeitrasterHatUnterrichtMitWochentyp1BisN(zeitraster.id);
-	}
-
-	function unterrichtsdaten(wochentag: Wochentag, stunde: number, wochentyp: number) : List<StundenplanUnterricht> | undefined {
-		if (!props.manager().zeitrasterExistsByWochentagAndStunde(wochentag.id, stunde))
-			return undefined;
-		const zeitraster = props.manager().zeitrasterGetByWochentagAndStundeOrException(wochentag.id, stunde);
-		const unterricht = props.manager().unterrichtGetMengeByZeitrasterIdAndWochentypOrEmptyList(zeitraster.id, wochentyp);
-		return (unterricht === null) ? undefined : unterricht;
-	}
 
 </script>
