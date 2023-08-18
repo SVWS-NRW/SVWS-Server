7fafd908d2d8d33f76b85f71074efb6f879a3cb7
m.brass@babiel.com <m.brass@babiel.com>
2023-08-18 14:52:40 +0200

    refs #1115 umstellung auf version

--- deployment/setupVersion.sh
+++ deployment/setupVersion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -n "$CI_COMMIT_TAG" ]; then
 	apt-get install jq -y
-	jq --arg version "${CI_COMMIT_TAG#v}" '.project."version" = $version' ../buildconfig.json > temp.json && mv temp.json ../buildconfig.json
+	jq --arg version "${CI_COMMIT_TAG#v}" '.project."version" = $version' buildconfig.json > temp.json && mv temp.json buildconfig.json
 fi
 
-cat ../buildconfig.json
+cat buildconfig.json
