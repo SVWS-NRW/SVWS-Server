9db42ec63e1a123921343f5e30f6ea48c7a08308
Thomas Bachran <thomas.bachran@gmx.de>
2023-08-25 16:26:15 +0200

    Stundenplan-Ansicht (wip)

--- svws-webclient/src/components/ts/src/components/stundenplan/StundenplanAnsicht.vue
+++ svws-webclient/src/components/ts/src/components/stundenplan/StundenplanAnsicht.vue
@@ -183,10 +183,10 @@
 	}
 
 	function getUnterrichtWochentypSpeziell(wochentag: Wochentag, stunde: number, wochentyp: number) : List<StundenplanUnterricht> {
-		// if (props.mode === 'schueler')
-		// 	return props.manager().unterrichtGetMengeBySchuelerIdAndWochentagAndStundeAndWochentypAndInklusiveOrEmptyList(props.id, wochentag.id, stunde, wochentyp, true);
-		// if (props.mode === 'lehrer')
-		// 	return props.manager().unterrichtGetMengeByLehrerIdAndWochentagAndStundeAndWochentypAndInklusiveOrEmptyList(props.id, wochentag.id, stunde, wochentyp, true);
+		if (props.mode === 'schueler')
+			return props.manager().unterrichtGetMengeBySchuelerIdAndWochentagAndStundeAndWochentypAndInklusiveOrEmptyList(props.id, wochentag.id, stunde, wochentyp, true);
+		if (props.mode === 'lehrer')
+			return props.manager().unterrichtGetMengeByLehrerIdAndWochentagAndStundeAndWochentypAndInklusiveOrEmptyList(props.id, wochentag.id, stunde, wochentyp, true);
 		if (props.mode === 'klasse')
 			return props.manager().unterrichtGetMengeByKlasseIdAndWochentagAndStundeAndWochentypAndInklusiveOrEmptyList(props.id, wochentag.id, stunde, wochentyp, true);
 		return props.manager().unterrichtGetMengeByWochentagAndStundeAndWochentypAndInklusiveOrEmptyList(wochentag, stunde, wochentyp, true);
