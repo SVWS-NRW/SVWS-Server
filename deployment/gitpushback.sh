2cc76bb04c9cb1fe923b9ef4c61c6695e4099c1b
m.brass@babiel.com <m.brass@babiel.com>
2023-08-31 15:13:25 +0200

    refs #1115 git push back

--- deployment/gitpushback.sh
+++ deployment/gitpushback.sh
@@ -2,6 +2,8 @@
 git config --global user.name "${GITLAB_USER_NAME}"
 git config --global user.email "${GITLAB_USER_EMAIL}"
 
+git checkout -b ${NEW_VERSION}
+
 git add buildconfig.json
 git add package.json
 git add svws-transpile/src/test/ts/package.json
@@ -16,5 +18,5 @@ git add svws-webclient/src/core/ts/package.json
 git add svws-webclient/src/ui/ts/package.json
 
 git commit -m "pushback - ${NEW_VERSION}"
-
-git push http://root:$ACCESS_TOKEN@$CI_SERVER_HOST/$CI_PROJECT_PATH.git origin 1115-git-push-back
+git push http://root:$ACCESS_TOKEN@$CI_SERVER_HOST/$CI_PROJECT_PATH.git --set-upstream origin ${NEW_VERSION}
+#git push http://root:$ACCESS_TOKEN@$CI_SERVER_HOST/$CI_PROJECT_PATH.git
