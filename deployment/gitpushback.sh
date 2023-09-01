d1d7d4d0cd0c5a9b0e911977a3ccdb2b81fca589
m.brass@babiel.com <m.brass@babiel.com>
2023-09-01 12:45:27 +0200

    refs #1115 git push back

--- deployment/gitpushback.sh
+++ deployment/gitpushback.sh
@@ -9,18 +9,18 @@ git config --global user.email "${GITLAB_USER_EMAIL}"
 
 git checkout -b ${NEW_VERSION}
 
-git add buildconfig.json
-git add package.json
-git add svws-transpile/src/test/ts/package.json
-git add svws-webclient/src/api-test/ts/package.json
-git add svws-webclient/src/browser-test/ts/package.json
-git add svws-webclient/src/client-test/ts/package.json
-git add svws-webclient/src/client/ts/package.json
-git add svws-webclient/src/client/ts/version.ts
-git add svws-webclient/src/components/ts/package.json
-git add svws-webclient/src/core-test/ts/package.json
-git add svws-webclient/src/core/ts/package.json
-git add svws-webclient/src/ui/ts/package.json
+git add /builds/svws/SVWS-Server/buildconfig.json
+git add /builds/svws/SVWS-Server/package.json
+git add /builds/svws/SVWS-Server/svws-transpile/src/test/ts/package.json
+git add /builds/svws/SVWS-Server/svws-webclient/src/api-test/ts/package.json
+git add /builds/svws/SVWS-Server/svws-webclient/src/browser-test/ts/package.json
+git add /builds/svws/SVWS-Server/svws-webclient/src/client-test/ts/package.json
+git add /builds/svws/SVWS-Server/svws-webclient/src/client/ts/package.json
+git add /builds/svws/SVWS-Server/svws-webclient/src/client/ts/version.ts
+git add /builds/svws/SVWS-Server/svws-webclient/src/components/ts/package.json
+git add /builds/svws/SVWS-Server/svws-webclient/src/core-test/ts/package.json
+git add /builds/svws/SVWS-Server/svws-webclient/src/core/ts/package.json
+git add /builds/svws/SVWS-Server/svws-webclient/src/ui/ts/package.json
 git status
 git commit -m "pushback - ${NEW_VERSION}"
 git status
