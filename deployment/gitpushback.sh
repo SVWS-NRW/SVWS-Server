64fb9c98351c401d1392e16622b3c8d55daf34a8
m.brass@babiel.com <m.brass@babiel.com>
2023-09-01 12:59:53 +0200

    refs #1115 git push back

--- deployment/gitpushback.sh
+++ deployment/gitpushback.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-NEW_VERSION=v0.0.2
+NEW_VERSION=v0.0.3
 
 git status
 
-git config --global user.name "${GITLAB_USER_NAME}"
-git config --global user.email "${GITLAB_USER_EMAIL}"
+#git config --global user.name "${GITLAB_USER_NAME}"
+#git config --global user.email "${GITLAB_USER_EMAIL}"
 
-git checkout -b ${NEW_VERSION}
+#git checkout -b ${NEW_VERSION}
 
-git add /builds/svws/SVWS-Server/buildconfig.json
-git add /builds/svws/SVWS-Server/package.json
-git add /builds/svws/SVWS-Server/svws-transpile/src/test/ts/package.json
-git add /builds/svws/SVWS-Server/svws-webclient/src/api-test/ts/package.json
-git add /builds/svws/SVWS-Server/svws-webclient/src/browser-test/ts/package.json
-git add /builds/svws/SVWS-Server/svws-webclient/src/client-test/ts/package.json
-git add /builds/svws/SVWS-Server/svws-webclient/src/client/ts/package.json
-git add /builds/svws/SVWS-Server/svws-webclient/src/client/ts/version.ts
-git add /builds/svws/SVWS-Server/svws-webclient/src/components/ts/package.json
-git add /builds/svws/SVWS-Server/svws-webclient/src/core-test/ts/package.json
-git add /builds/svws/SVWS-Server/svws-webclient/src/core/ts/package.json
-git add /builds/svws/SVWS-Server/svws-webclient/src/ui/ts/package.json
-git status
-git commit -m "pushback - ${NEW_VERSION}"
-git status
-git push http://root:$ACCESS_TOKEN@$CI_SERVER_HOST/$CI_PROJECT_PATH.git --set-upstream origin ${NEW_VERSION}
+#git add /builds/svws/SVWS-Server/buildconfig.json
+#git add /builds/svws/SVWS-Server/package.json
+#git add /builds/svws/SVWS-Server/svws-transpile/src/test/ts/package.json
+#git add /builds/svws/SVWS-Server/svws-webclient/src/api-test/ts/package.json
+#git add /builds/svws/SVWS-Server/svws-webclient/src/browser-test/ts/package.json
+#git add /builds/svws/SVWS-Server/svws-webclient/src/client-test/ts/package.json
+#git add /builds/svws/SVWS-Server/svws-webclient/src/client/ts/package.json
+#git add /builds/svws/SVWS-Server/svws-webclient/src/client/ts/version.ts
+#git add /builds/svws/SVWS-Server/svws-webclient/src/components/ts/package.json
+#git add /builds/svws/SVWS-Server/svws-webclient/src/core-test/ts/package.json
+#git add /builds/svws/SVWS-Server/svws-webclient/src/core/ts/package.json
+#git add /builds/svws/SVWS-Server/svws-webclient/src/ui/ts/package.json
+#git status
+#git commit -m "pushback - ${NEW_VERSION}"
+#git status
+#git push http://root:$ACCESS_TOKEN@$CI_SERVER_HOST/$CI_PROJECT_PATH.git --set-upstream origin ${NEW_VERSION}
 #git push http://root:$ACCESS_TOKEN@$CI_SERVER_HOST/$CI_PROJECT_PATH.git
+
