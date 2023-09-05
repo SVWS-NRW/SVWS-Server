9edc94a77698927ca8aea131708224f2f54aaebe
hmt <dev@hmt.im>
2023-09-05 17:51:35 +0200

    ui: typo

--- svws-webclient/src/ui/ts/src/components/SvwsUiTextareaInput.vue
+++ svws-webclient/src/ui/ts/src/components/SvwsUiTextareaInput.vue
@@ -121,7 +121,7 @@
 
 	function onInput(event: Event) {
 		const value = (event.target as HTMLInputElement).value;
-		if (value != data.value)
+		if (value !== data.value)
 			updateData(value);
 	}
 
