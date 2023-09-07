08b276cf9887e3a5f7d3f565040f48e90ed3c399
esr <dev@mec0.de>
2023-09-07 20:29:28 +0200

    Klausurplan: Kalenderinformation Transaktionshandling

--- svws-db-utils/src/main/java/de/svws_nrw/data/gost/klausurplan/DataGostKlausurenKalenderinformation.java
+++ svws-db-utils/src/main/java/de/svws_nrw/data/gost/klausurplan/DataGostKlausurenKalenderinformation.java
@@ -4,17 +4,17 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.function.ObjLongConsumer;
 
 import de.svws_nrw.core.data.gost.klausurplanung.GostKlausurenKalenderinformation;
+import de.svws_nrw.data.DataBasicMapper;
 import de.svws_nrw.data.DataManager;
 import de.svws_nrw.data.JSONMapper;
 import de.svws_nrw.db.DBEntityManager;
 import de.svws_nrw.db.dto.current.gost.klausurplanung.DTOGostKlausurenKalenderinformationen;
-import de.svws_nrw.db.dto.current.schema.DTOSchemaAutoInkremente;
 import de.svws_nrw.db.schema.Schema;
-import de.svws_nrw.db.utils.OperationError;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
@@ -77,35 +77,22 @@ public final class DataGostKlausurenKalenderinformation extends DataManager<Long
 		throw new UnsupportedOperationException();
 	}
 
+	private static final Set<String> requiredCreateAttributes = Set.of("startdatum");
+
+	private final Map<String, DataBasicMapper<DTOGostKlausurenKalenderinformationen>> patchMappings =
+			Map.ofEntries(
+				Map.entry("bemerkung", (dto, value, map) -> dto.Bemerkungen = JSONMapper.convertToString(value, true, false, Schema.tab_Gost_Klausuren_Kalenderinformationen.col_Bemerkungen.datenlaenge())),
+				Map.entry("bezeichnung", (dto, value, map) -> dto.Bezeichnung = JSONMapper.convertToString(value, true, false, Schema.tab_Gost_Klausuren_Kalenderinformationen.col_Bezeichnung.datenlaenge())),
+				Map.entry("startdatum", (dto, value, map) -> dto.Startdatum = JSONMapper.convertToString(value, false, false, null)),
+				Map.entry("startzeit", (dto, value, map) -> dto.Startzeit = JSONMapper.convertToIntegerInRange(value, true, 0, 1440)),
+				Map.entry("enddatum", (dto, value, map) -> dto.Enddatum = JSONMapper.convertToString(value, true, false, null)),
+				Map.entry("endzeit", (dto, value, map) -> dto.Endzeit = JSONMapper.convertToIntegerInRange(value, true, 0, 1440)),
+				Map.entry("istSperrtermin", (dto, value, map) -> dto.IstSperrtermin = JSONMapper.convertToBoolean(value, false))
+			);
+
 	@Override
 	public Response patch(final Long id, final InputStream is) {
-		final Map<String, Object> map = JSONMapper.toMap(is);
-		if (map.size() > 0) {
-			final DTOGostKlausurenKalenderinformationen kalInfo = conn.queryByKey(DTOGostKlausurenKalenderinformationen.class, id);
-			if (kalInfo == null)
-				throw OperationError.NOT_FOUND.exception();
-			for (final Entry<String, Object> entry : map.entrySet()) {
-				final String key = entry.getKey();
-				final Object value = entry.getValue();
-				switch (key) {
-				case "id" -> {
-					final Long patch_id = JSONMapper.convertToLong(value, true);
-					if ((patch_id == null) || (patch_id.longValue() != id.longValue()))
-						throw OperationError.BAD_REQUEST.exception();
-				}
-				case "bemerkung" -> kalInfo.Bemerkungen = JSONMapper.convertToString(value, true, false, Schema.tab_Gost_Klausuren_Kalenderinformationen.col_Bemerkungen.datenlaenge());
-				case "bezeichnung" -> kalInfo.Bezeichnung = JSONMapper.convertToString(value, true, false, Schema.tab_Gost_Klausuren_Kalenderinformationen.col_Bezeichnung.datenlaenge());
-				case "startdatum" -> kalInfo.Startdatum = JSONMapper.convertToString(value, false, false, null);
-				case "startzeit" -> kalInfo.Startzeit = JSONMapper.convertToIntegerInRange(value, true, 0, 1440);
-				case "enddatum" -> kalInfo.Enddatum = JSONMapper.convertToString(value, true, false, null);
-				case "endzeit" -> kalInfo.Endzeit = JSONMapper.convertToIntegerInRange(value, true, 0, 1440);
-				case "istSperrtermin" -> kalInfo.IstSperrtermin = JSONMapper.convertToBoolean(value, false);
-				default -> throw OperationError.BAD_REQUEST.exception();
-				}
-			}
-			conn.transactionPersist(kalInfo);
-		}
-		return Response.status(Status.OK).build();
+		return super.patchBasic(id, is, DTOGostKlausurenKalenderinformationen.class, patchMappings);
 	}
 
 	@Override
@@ -121,36 +108,8 @@ public final class DataGostKlausurenKalenderinformation extends DataManager<Long
 	 * @return Eine Response mit der neuen Gost-KlausurenKalenderinformation
 	 */
 	public Response create(final InputStream is) {
-		DTOGostKlausurenKalenderinformationen kalInfo = null;
-		// Bestimme die ID der neuen KlausurenKalenderinformation
-		final DTOSchemaAutoInkremente lastID = conn.queryByKey(DTOSchemaAutoInkremente.class, "Gost_Klausuren_Kalenderinformationen");
-		final Long id = lastID == null ? 1 : lastID.MaxID + 1;
-		kalInfo = new DTOGostKlausurenKalenderinformationen(id, false);
-
-		final Map<String, Object> map = JSONMapper.toMap(is);
-		if (map.size() > 0) {
-			for (final Entry<String, Object> entry : map.entrySet()) {
-				final String key = entry.getKey();
-				final Object value = entry.getValue();
-				switch (key) {
-				case "id" -> {
-					throw OperationError.BAD_REQUEST.exception();
-				}
-				case "bemerkung" -> kalInfo.Bemerkungen = JSONMapper.convertToString(value, true, false, Schema.tab_Gost_Klausuren_Kalenderinformationen.col_Bemerkungen.datenlaenge());
-				case "bezeichnung" -> kalInfo.Bezeichnung = JSONMapper.convertToString(value, true, false, Schema.tab_Gost_Klausuren_Kalenderinformationen.col_Bezeichnung.datenlaenge());
-				case "startdatum" -> kalInfo.Startdatum = JSONMapper.convertToString(value, false, false, null);
-				case "startzeit" -> kalInfo.Startzeit = JSONMapper.convertToIntegerInRange(value, true, 0, 1440);
-				case "enddatum" -> kalInfo.Enddatum = JSONMapper.convertToString(value, true, false, null);
-				case "endzeit" -> kalInfo.Endzeit = JSONMapper.convertToIntegerInRange(value, true, 0, 1440);
-				case "istSperrtermin" -> kalInfo.IstSperrtermin = JSONMapper.convertToBoolean(value, false);
-				default -> throw OperationError.BAD_REQUEST.exception();
-				}
-			}
-		}
-
-		conn.transactionPersist(kalInfo);
-		final GostKlausurenKalenderinformation daten = dtoMapper.apply(kalInfo);
-		return Response.status(Status.OK).type(MediaType.APPLICATION_JSON).entity(daten).build();
+		final ObjLongConsumer<DTOGostKlausurenKalenderinformationen> initDTO = (dto, id) -> dto.ID = id;
+		return super.addBasic(is, DTOGostKlausurenKalenderinformationen.class, initDTO, dtoMapper, requiredCreateAttributes, patchMappings);
 	}
 
 	/**
@@ -161,14 +120,7 @@ public final class DataGostKlausurenKalenderinformation extends DataManager<Long
 	 * @return die Response
 	 */
 	public Response delete(final Long id) {
-		// TODO use transaction
-		// Bestimme die KlausurenKalenderinformation
-		final DTOGostKlausurenKalenderinformationen kalInfo = conn.queryByKey(DTOGostKlausurenKalenderinformationen.class, id);
-		if (kalInfo == null)
-			return OperationError.NOT_FOUND.getResponse();
-		// Entferne die KlausurenKalenderinformation
-		conn.remove(kalInfo);
-		return Response.status(Status.OK).type(MediaType.APPLICATION_JSON).entity(id).build();
+		return super.deleteBasic(id, DTOGostKlausurenKalenderinformationen.class, dtoMapper);
 	}
 
 }
