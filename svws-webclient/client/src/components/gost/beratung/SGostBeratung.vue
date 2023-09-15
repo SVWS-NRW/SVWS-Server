7e5a4794ac1e9e29df35fd61ca80d4702c8c0871
Jonas Kunst <info@jonaskun.st>
2023-09-15 14:48:54 +0200

    Client: Tabellen für Gost Beratung angepasst

--- svws-webclient/client/src/components/gost/beratung/SGostBeratung.vue
+++ svws-webclient/client/src/components/gost/beratung/SGostBeratung.vue
@@ -8,33 +8,30 @@
 						<svg width="1.2em" height="1.2em" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3 16V5.75C3 5.06 3.56 4.5 4.25 4.5S5.5 5.06 5.5 5.75V12H6.5V2.75C6.5 2.06 7.06 1.5 7.75 1.5C8.44 1.5 9 2.06 9 2.75V12H10V1.25C10 .56 10.56 0 11.25 0S12.5 .56 12.5 1.25V12H13.5V3.25C13.5 2.56 14.06 2 14.75 2S16 2.56 16 3.25V15H16.75L18.16 11.47C18.38 10.92 18.84 10.5 19.4 10.31L20.19 10.05C21 9.79 21.74 10.58 21.43 11.37L18.4 19C17.19 22 14.26 24 11 24C6.58 24 3 20.42 3 16Z" /></svg>
 					</template>
 				</svws-ui-button>
+				<span class="flex-grow" />
 				<s-modal-laufbahnplanung-kurswahlen-loeschen @delete="resetFachwahlen" />
 			</svws-ui-sub-nav>
 		</Teleport>
 		<Teleport to=".svws-ui-header--actions" v-if="isMounted">
 			<svws-ui-modal-hilfe> <hilfe-gost-beratung /> </svws-ui-modal-hilfe>
 		</Teleport>
-		<div class="flex-grow">
-			<s-laufbahnplanung-card-planung title="Vorlage für Laufbahnplanungen von Schülern des Abiturjahrgangs"
-				:abiturdaten-manager="abiturdatenManager" :manueller-modus="istManuellerModus"
-				:gost-jahrgangsdaten="jahrgangsdaten()" :set-wahl="setWahl" ignoriere-sprachenfolge />
-		</div>
-		<div class="w-2/5 3xl:w-1/2 min-w-[36rem]">
-			<div class="flex flex-col gap-12">
-				<svws-ui-content-card title="Textvorlagen für die Laufbahnplanung">
-					<div class="flex flex-col gap-4">
-						<svws-ui-textarea-input placeholder="Beratungsbögen" :model-value="jahrgangsdaten().textBeratungsbogen"
-							@change="textBeratungsbogen => doPatch({ textBeratungsbogen })" resizeable="vertical" autoresize />
-						<svws-ui-textarea-input placeholder="Mailversand" :model-value="jahrgangsdaten().textMailversand"
-							@change="textMailversand => doPatch({ textMailversand })" resizeable="vertical" autoresize />
-					</div>
-				</svws-ui-content-card>
-				<svws-ui-content-card v-if="istAbiturjahrgang" title="Beratungslehrer" class="opacity-50">
-					<span>Hier kommen die Beratungslehrer hin.</span>
-				</svws-ui-content-card>
-				<s-laufbahnplanung-card-status :abiturdaten-manager="abiturdatenManager"
-					:fehlerliste="() => gostBelegpruefungErgebnis().fehlercodes" :gost-belegpruefungs-art="gostBelegpruefungsArt" @update:gost-belegpruefungs-art="setGostBelegpruefungsArt" />
-			</div>
+		<s-laufbahnplanung-card-planung title="Vorlage für Schüler des Abiturjahrgangs"
+			:abiturdaten-manager="abiturdatenManager" :manueller-modus="istManuellerModus"
+			:gost-jahrgangsdaten="jahrgangsdaten()" :set-wahl="setWahl" ignoriere-sprachenfolge />
+		<div class="flex flex-col gap-y-16 lg:gap-y-20">
+			<s-laufbahnplanung-card-status :abiturdaten-manager="abiturdatenManager"
+				:fehlerliste="() => gostBelegpruefungErgebnis().fehlercodes" :gost-belegpruefungs-art="gostBelegpruefungsArt" @update:gost-belegpruefungs-art="setGostBelegpruefungsArt" />
+			<svws-ui-content-card v-if="istAbiturjahrgang" title="Beratungslehrer" class="opacity-50">
+				<span>Hier kommen die Beratungslehrer hin.</span>
+			</svws-ui-content-card>
+			<svws-ui-content-card title="Textvorlagen">
+				<svws-ui-input-wrapper>
+					<svws-ui-textarea-input placeholder="Beratungsbögen" :model-value="jahrgangsdaten().textBeratungsbogen"
+						@change="textBeratungsbogen => doPatch({ textBeratungsbogen })" resizeable="vertical" autoresize />
+					<svws-ui-textarea-input placeholder="Mailversand" :model-value="jahrgangsdaten().textMailversand"
+						@change="textMailversand => doPatch({ textMailversand })" resizeable="vertical" autoresize />
+				</svws-ui-input-wrapper>
+			</svws-ui-content-card>
 		</div>
 	</div>
 </template>
@@ -51,7 +48,7 @@
 
 	const istManuellerModus = ref(false)
 	function switchManuellerModus() {
-		istManuellerModus.value = istManuellerModus.value ? false : true;
+		istManuellerModus.value = !istManuellerModus.value;
 	}
 
 	async function doPatch(data: Partial<GostJahrgangsdaten>) {
@@ -65,3 +62,10 @@
 	});
 
 </script>
+
+<style lang="postcss" scoped>
+.page--content {
+  @apply grid;
+  grid-template-columns: 1fr minmax(36rem, 1fr);
+}
+</style>
