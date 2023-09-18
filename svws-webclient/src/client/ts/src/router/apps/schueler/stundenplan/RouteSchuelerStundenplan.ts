de852864fe7d7267cc982f92d1f16897711e332a
Frank Pfotenhauer <pfotenhauer@schildnrw.de>
2023-09-18 09:50:38 +0200

    Stundenplan als DEV markiert.

--- svws-webclient/src/client/ts/src/router/apps/schueler/stundenplan/RouteSchuelerStundenplan.ts
+++ svws-webclient/src/client/ts/src/router/apps/schueler/stundenplan/RouteSchuelerStundenplan.ts
@@ -16,7 +16,7 @@ export class RouteSchuelerStundenplan extends RouteNode<RouteDataSchuelerStunden
 
 	public constructor() {
 		super(Schulform.values(), [ BenutzerKompetenz.KEINE ], "schueler.stundenplan", "stundenplan", SSchuelerStundenplan, new RouteDataSchuelerStundenplan());
-		super.mode = ServerMode.STABLE;
+		super.mode = ServerMode.DEV;
 		super.propHandler = (route) => this.getProps(route);
 		super.text = "Stundenplan";
 		super.children = [
