85f30a9115807d149168c80bbc7d8085479378f7
hmt <dev@hmt.im>
2023-09-20 08:21:31 +0200

    node 20.7.0 und deps

--- buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
+++ buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
@@ -31,10 +31,10 @@ abstract class NodePluginConfig {
     	this.project = p;
     	this.rootProject = p.rootProject;
     	url.convention('https://nodejs.org/dist/v');
-    	version.convention('20.6.1'); // https://nodejs.org/en/download/current
-    	npmVersion.convention('9.8.1');
+    	version.convention('20.7.0'); // https://nodejs.org/en/download/current
+    	npmVersion.convention('10.1.0');
     	tsVersion.convention('5.2.2'); // https://github.com/microsoft/TypeScript/releases
-    	tsNodeTypesVersion.convention('20.6.2'); // https://www.npmjs.com/package/@types/node
+    	tsNodeTypesVersion.convention('20.6.3'); // https://www.npmjs.com/package/@types/node
     }
     
     boolean isLinux() {
