00bfd14558243cf8c940255d06a193fa0c638bb1
Thomas Bachran <thomas.bachran@gmx.de>
2023-09-26 15:31:20 +0200

    Client - Fachwahlen: Reduktion der dargestellten Informationen

--- svws-webclient/client/src/components/gost/fachwahlen/SGostFachwahlenFach.vue
+++ svws-webclient/client/src/components/gost/fachwahlen/SGostFachwahlenFach.vue
@@ -7,7 +7,7 @@
 				</div>
 			</div>
 			<div role="row" class="svws-ui-tr">
-				<div role="cell" class="svws-ui-td col-span-2">Gesamt im Halbjahr</div>
+				<div role="cell" class="svws-ui-td">Halbjahr</div>
 				<div role="cell" class="svws-ui-td">
 					<i-ri-draft-line class="text-sm -my-0.5" />
 					<span>Schriftlich</span>
@@ -33,12 +33,6 @@
 								</span>
 							</div>
 							<div role="cell" class="svws-ui-td">
-								<template v-if="fws.fachwahlen[halbjahr.id].wahlenGK > 0">
-									{{ fws.fachwahlen[halbjahr.id].wahlenGK }}
-								</template>
-								<span v-else class="opacity-25">—</span>
-							</div>
-							<div role="cell" class="svws-ui-td">
 								<template v-if="fws.fachwahlen[halbjahr.id].wahlenGKSchriftlich > 0">
 									{{ fws.fachwahlen[halbjahr.id].wahlenGKSchriftlich }}
 								</template>
@@ -52,7 +46,8 @@
 							</div>
 						</div>
 						<div v-if="aktuell?.id === halbjahr.id" role="row" class="svws-ui-tr">
-							<div role="cell" class="flex flex-col svws-ui-td mb-5 leading-tight" v-for="col in [1, 2, 3]" :key="col" :class="{'col-span-2 -ml-1 text-black/50 dark:text-white/50 hover:text-black focus-within:text-black dark:hover:text-white dark:focus-within:text-white': col === 1}">
+							<div> <!----> </div>
+							<div role="cell" class="flex flex-col svws-ui-td mb-5 leading-tight" v-for="col in [1, 2]" :key="col">
 								<div v-for="schueler in getSchuelerListe(fws.id, halbjahr, col)" :key="schueler.id" class="flex gap-1 py-0.5 px-1 -mx-1 -mt-0.5 hover:bg-black/10 dark:hover:bg-white/10 rounded cursor-pointer" role="link" @click="gotoLaufbahnplanung(schueler.id)">
 									<i-ri-link class="text-sm" />
 									<span class="line-clamp-1 break-all leading-tight -my-0.5" :title="schueler.nachname + ', ' + schueler.vorname">{{ schueler.nachname + ", " + schueler.vorname }}</span>
@@ -92,7 +87,6 @@
 
 	const cols: DataTableColumn[] = [
 		{ key: "HJ", label: "HJ", fixedWidth: 6 },
-		{ key: "GK", label: "GK", span: 1 },
 		{ key: "GKS", label: "GKS", span: 1 },
 		{ key: "GKM", label: "GKM", span: 1 },
 	];
@@ -114,10 +108,9 @@
 
 	function getSchuelerListe(idFach : number, halbjahr: GostHalbjahr, col: number) : List<SchuelerListeEintrag> {
 		const result = new ArrayList<SchuelerListeEintrag>();
-		const schuelermenge = col === 1
-			? props.fachwahlenManager.schuelerGetMengeGKByFachAndHalbjahrAsListOrException(idFach, halbjahr) : col === 2
-				? props.fachwahlenManager.schuelerGetMengeGKSchriftlichByFachAndHalbjahrAsListOrException(idFach, halbjahr)
-				: props.fachwahlenManager.schuelerGetMengeGKMuendlichByFachAndHalbjahrAsListOrException(idFach, halbjahr)
+		const schuelermenge = (col === 1)
+			? props.fachwahlenManager.schuelerGetMengeGKSchriftlichByFachAndHalbjahrAsListOrException(idFach, halbjahr)
+			: props.fachwahlenManager.schuelerGetMengeGKMuendlichByFachAndHalbjahrAsListOrException(idFach, halbjahr)
 		for (const id of schuelermenge) {
 			const schueler = props.mapSchueler.get(id);
 			if (schueler !== undefined)
--- svws-webclient/client/src/components/gost/fachwahlen/SGostFachwahlenFachHalbjahr.vue
+++ svws-webclient/client/src/components/gost/fachwahlen/SGostFachwahlenFachHalbjahr.vue
@@ -10,7 +10,7 @@
 				</div>
 			</div>
 			<div role="row" class="svws-ui-tr">
-				<div role="cell" class="svws-ui-td col-span-2">Gesamt im Halbjahr</div>
+				<div role="cell" class="svws-ui-td">Halbjahr</div>
 				<div role="cell" class="svws-ui-td">
 					<i-ri-draft-line class="text-sm -my-0.5" />
 					<span>Schriftlich</span>
@@ -24,12 +24,7 @@
 		<template #body>
 			<template v-if="fws !== undefined && hatFachwahl(fws)">
 				<div role="row" class="svws-ui-tr">
-					<div role="cell" class="svws-ui-td col-span-2">
-						<template v-if="fws.fachwahlen[halbjahr.id].wahlenGK > 0">
-							{{ fws.fachwahlen[halbjahr.id].wahlenGK }}
-						</template>
-						<span v-else class="opacity-25">—</span>
-					</div>
+					<div> <!----> </div>
 					<div role="cell" class="svws-ui-td">
 						<template v-if="fws.fachwahlen[halbjahr.id].wahlenGKSchriftlich > 0">
 							{{ fws.fachwahlen[halbjahr.id].wahlenGKSchriftlich }}
@@ -44,7 +39,8 @@
 					</div>
 				</div>
 				<div role="row" class="svws-ui-tr">
-					<div role="cell" class="flex flex-col svws-ui-td mb-5 leading-tight" v-for="col in [1, 2, 3]" :key="col" :class="{'col-span-2 !pl-4 text-black/50 dark:text-white/50 hover:text-black focus-within:text-black dark:hover:text-white dark:focus-within:text-white': col === 1}">
+					<div> <!----> </div>
+					<div role="cell" class="flex flex-col svws-ui-td mb-5 leading-tight" v-for="col in [1, 2]" :key="col">
 						<div v-for="schueler in getSchuelerListe(fws.id, col)" :key="schueler.id" class="flex gap-1 py-0.5 px-1 -mx-1 -mt-0.5 hover:bg-black/10 dark:hover:bg-white/10 rounded cursor-pointer" role="link" @click="gotoLaufbahnplanung(schueler.id)">
 							<i-ri-link class="text-sm" />
 							<span class="line-clamp-1 break-all leading-tight -my-0.5" :title="schueler.nachname + ', ' + schueler.vorname">{{ schueler.nachname + ", " + schueler.vorname }}</span>
@@ -75,7 +71,6 @@
 
 	const cols: DataTableColumn[] = [
 		{ key: "HJ", label: "HJ", fixedWidth: 6 },
-		{ key: "GK", label: "GK", span: 1 },
 		{ key: "GKS", label: "GKS", span: 1 },
 		{ key: "GKM", label: "GKM", span: 1 },
 	];
@@ -97,10 +92,9 @@
 
 	function getSchuelerListe(idFach : number, col: number) : List<SchuelerListeEintrag> {
 		const result = new ArrayList<SchuelerListeEintrag>();
-		const schuelermenge = col === 1
-			? props.fachwahlenManager.schuelerGetMengeGKByFachAndHalbjahrAsListOrException(idFach, props.halbjahr) : col === 2
-				? props.fachwahlenManager.schuelerGetMengeGKSchriftlichByFachAndHalbjahrAsListOrException(idFach, props.halbjahr)
-				: props.fachwahlenManager.schuelerGetMengeGKMuendlichByFachAndHalbjahrAsListOrException(idFach, props.halbjahr)
+		const schuelermenge = (col === 1)
+			? props.fachwahlenManager.schuelerGetMengeGKSchriftlichByFachAndHalbjahrAsListOrException(idFach, props.halbjahr)
+			: props.fachwahlenManager.schuelerGetMengeGKMuendlichByFachAndHalbjahrAsListOrException(idFach, props.halbjahr)
 		for (const id of schuelermenge) {
 			const schueler = props.mapSchueler.get(id);
 			if (schueler !== undefined)
