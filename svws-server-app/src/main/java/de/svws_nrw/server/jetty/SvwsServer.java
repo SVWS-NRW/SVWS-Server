d0d76a92f88bb46c10723bc421ae724b56ecd151
Thomas Bachran <thomas.bachran@gmx.de>
2023-10-12 12:41:50 +0200

    SVWS-Server: Refactoring (wip)

--- svws-server-app/src/main/java/de/svws_nrw/server/jetty/SvwsServer.java
+++ svws-server-app/src/main/java/de/svws_nrw/server/jetty/SvwsServer.java
@@ -62,152 +62,16 @@ public final class SvwsServer {
 	private final Server server;
 
 	/** Der im Server verwendete {@link ServletContextHandler} */
-	private ServletContextHandler context_handler;
+	private final ServletContextHandler context_handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
+	/** Die Menge der Handler, welche dem Security Context - Handler des Jetty-Server zugeordnet sind */
+	private final HandlerCollection handlerCollection = new HandlerCollection();
 
+	/** Die Menge der Context-Handler, welche zu den Handlern gehören */
+	private final ContextHandlerCollection contextHandlerCollection = new ContextHandlerCollection();
 
-	/**
-	 * Gibt die Instanz des SVWS-Servers zurück.
-	 *
-	 * @return die SVWS-Server-Instanz
-	 */
-	public static SvwsServer instance() {
-		if (_instance == null)
-			_instance = new SvwsServer();
-		return _instance;
-	}
-
-	/**
-	 * Gibt den Logger zurück, der dem Server zugeordnet ist.
-	 *
-	 * @return der Logger
-	 */
-	public Logger logger() {
-		return _logger;
-	}
-
-
-
-	/**
-	 * Fügt einen neuen {@link SVWSLoginService} zum Server hinzu und konfiguriert diesen basierend
-	 * auf der {@link SVWSKonfiguration}.
-	 */
-	private void addLoginService() {
-		// Login Service
-		final LoginService loginService = new SVWSLoginService("Authentifizierung bei dem SVWS-Server");
-		server.addBean(loginService);
-		final ConstraintSecurityHandler security = new ConstraintSecurityHandler();
-        server.setHandler(security);
-
-        final Constraint constraint = new Constraint();
-        constraint.setName("auth");
-        constraint.setAuthenticate(true);
-        constraint.setRoles(new String[] { "user", "admin" });
-        final ConstraintMapping mapping = new ConstraintMapping();
-        mapping.setPathSpec("/*");
-        mapping.setConstraint(constraint);
-        mapping.setMethodOmissions(new String[] { HttpMethod.OPTIONS });
-        mapping.setConstraint(constraint);
-        security.addConstraintMapping(mapping);
-
-        security.setAuthenticator(new SVWSAuthenticator());
-        security.setLoginService(loginService);
-
-		// Handler Structure
-		final HandlerCollection handlers = new HandlerCollection();
-		final ContextHandlerCollection contexts = new ContextHandlerCollection();
-		handlers.setHandlers(new Handler[] { contexts, new DefaultHandler() });
-		security.setHandler(handlers);
-
-		// logging
-		if (SVWSKonfiguration.get().isLoggingEnabled()) {
-			final String logPath = SVWSKonfiguration.get().getLoggingPath();
-			try {
-				Files.createDirectories(Paths.get(logPath));
-			} catch (final IOException e) {
-				e.printStackTrace();
-			}
-			final RequestLogWriter logWriter = new RequestLogWriter(logPath + "/yyyy_mm_dd.request.log");
-			logWriter.setFilenameDateFormat("yyyy_MM_dd");
-			logWriter.setRetainDays(90);
-			logWriter.setAppend(true);
-			logWriter.setTimeZone("GMT");
-			final CustomRequestLog requestLog = new CustomRequestLog(logWriter, CustomRequestLog.EXTENDED_NCSA_FORMAT);
-			final RequestLogHandler requestLogHandler = new RequestLogHandler();
-			requestLogHandler.setRequestLog(requestLog);
-			handlers.addHandler(requestLogHandler);
-		}
-
-		// create a ServletContextHandle to support multiple servlets
-		context_handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
-		context_handler.setContextPath("/");
-		context_handler.setResourceBase(System.getProperty("java.io.tmpdir"));
-		contexts.addHandler(context_handler);
-	}
-
-
-	/**
-	 * Erzeugt eine neue HTTP- bzw. HTTPS-Konfiguration basierend auf der {@link SVWSKonfiguration}
-	 * und fügt diese zum Jetty-Server hinzu.
-	 */
-	private void addHTTPConfiguration() {
-		final boolean disableTLS = SVWSKonfiguration.get().isTLSDisabled();
-
-		// HTTP Configuration
-		final HttpConfiguration http_config = new HttpConfiguration();
-		if (!disableTLS) {
-			http_config.setSecureScheme("https");
-			http_config.setSecurePort(SVWSKonfiguration.get().getPortHTTPS());
-		}
-		http_config.setOutputBufferSize(32768);
-		http_config.setRequestHeaderSize(8192);
-		http_config.setResponseHeaderSize(8192);
-		http_config.setSendServerVersion(true);
-		http_config.setSendDateHeader(false);
-
-		// SSL Context Factory
-		final SslContextFactory.Server sslContextFactory = new SslContextFactory.Server();
-		sslContextFactory.setKeyStorePath(SVWSKonfiguration.get().getTLSKeystorePath() + "/keystore");
-		sslContextFactory.setKeyStorePassword(SVWSKonfiguration.get().getTLSKeystorePassword());
-		sslContextFactory.setKeyManagerPassword(SVWSKonfiguration.get().getTLSKeystorePassword());
-		sslContextFactory.setTrustStorePath(SVWSKonfiguration.get().getTLSKeystorePath() + "/keystore");
-		sslContextFactory.setTrustStorePassword(SVWSKonfiguration.get().getTLSKeystorePassword());
-		sslContextFactory.setIncludeProtocols("TLSv1.3", "TLSv1.2");
-		sslContextFactory.setIncludeCipherSuites("TLS_AES_256_GCM_SHA384", "TLS_CHACHA20_POLY1305_SHA256", "TLS_AES_128_GCM_SHA256",
-				"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256");
-		sslContextFactory.setSniRequired(false);
-		sslContextFactory.setCipherComparator(HTTP2Cipher.COMPARATOR);
-
-		// SSL HTTP Configuration
-		final HttpConfiguration https_config = new HttpConfiguration(http_config);
-		final SecureRequestCustomizer secureRequestCustomizer = new SecureRequestCustomizer();
-		if (!disableTLS) {
-			secureRequestCustomizer.setSniHostCheck(false);
-		}
-		https_config.addCustomizer(secureRequestCustomizer);
-
-		// HTTP Connection Factory (v1.1 oder v2)
-		final AbstractConnectionFactory connFactoryHTTPv1_1 = new HttpConnectionFactory(https_config);
-		final AbstractConnectionFactory connFactoryHTTPv2 = new HTTP2ServerConnectionFactory(https_config);
-		final ALPNServerConnectionFactory alpn = new ALPNServerConnectionFactory();
-		if (disableTLS) {
-			@SuppressWarnings("resource")
-			final
-			ServerConnector connector = SVWSKonfiguration.get().useHTTPDefaultv11()
-				? new ServerConnector(server, connFactoryHTTPv1_1, connFactoryHTTPv2)
-				: new ServerConnector(server, connFactoryHTTPv2, connFactoryHTTPv1_1);
-			connector.setPort(SVWSKonfiguration.get().getPortHTTP());
-			server.addConnector(connector);
-		} else {
-			@SuppressWarnings("resource")
-			final
-			ServerConnector connector = SVWSKonfiguration.get().useHTTPDefaultv11()
-				? new ServerConnector(server, sslContextFactory, alpn, connFactoryHTTPv1_1, connFactoryHTTPv2)
-				: new ServerConnector(server, sslContextFactory, alpn, connFactoryHTTPv2, connFactoryHTTPv1_1);
-			connector.setPort(SVWSKonfiguration.get().getPortHTTPS());
-			server.addConnector(connector);
-		}
-	}
+	/** Der Login-Service, welcher die Zugriffsberechtigungen auf die API prüft. */
+	private final LoginService loginService = new SVWSLoginService("Authentifizierung bei dem SVWS-Server");
 
 
 	/**
@@ -229,6 +93,51 @@ public final class SvwsServer {
 		server.setDumpAfterStart(false);
 		server.setDumpBeforeStop(false);
 		server.setStopAtShutdown(true);
+
+		// Registriere den Login Service beim Jetty-Server
+		server.addBean(loginService);
+
+		// Initialisieren den ServletContextHandler für die Unterstützung mehrerer Servlets
+		context_handler.setContextPath("/");
+		context_handler.setResourceBase(System.getProperty("java.io.tmpdir"));
+		contextHandlerCollection.addHandler(context_handler);
+
+		// Ordne die Context-Handler den allgemeinen Handlern zu
+		handlerCollection.setHandlers(new Handler[] { contextHandlerCollection, new DefaultHandler() });
+
+		// Erstelle einen ConstraintSecurityHandler mit dem Login-Service, etc.
+        server.setHandler(createConstraintSecurityHandler(server, loginService, handlerCollection));
+
+        // Konfiguriere das Logging für API-Zugriffe
+        addLoggingHandler(handlerCollection);
+
+        // Konfiguriere und ergänze die HTTP-Verbindungen, auf welchen der Server lauscht
+		addHTTPServerConnections();
+
+		// Füge die einzelnen Servlets mit den verschiedenen APIs zum SVWS-Server hinzu.
+		addAPIApplications();
+	}
+
+
+	/**
+	 * Gibt die Instanz des SVWS-Servers zurück.
+	 *
+	 * @return die SVWS-Server-Instanz
+	 */
+	public static SvwsServer instance() {
+		if (_instance == null)
+			_instance = new SvwsServer();
+		return _instance;
+	}
+
+
+	/**
+	 * Gibt den Logger zurück, der dem Server zugeordnet ist.
+	 *
+	 * @return der Logger
+	 */
+	public Logger logger() {
+		return _logger;
 	}
 
 
@@ -239,15 +148,156 @@ public final class SvwsServer {
 	 * @throws Exception   eine Exception beim Starten des Servers wird zurückgemeldet
 	 */
 	public void start() throws Exception {
-		// Add several standard configurations for this application
-		addLoginService();
-		addHTTPConfiguration();
-		addAPIApplications();
-
 		// Start the server
 		server.start();
 		server.join();
 	}
+
+
+	private static ConstraintMapping getSecurityConstraintMapping() {
+        final Constraint constraint = new Constraint();
+        constraint.setName("auth");
+        constraint.setAuthenticate(true);
+        constraint.setRoles(new String[] { "user", "admin" });
+        final ConstraintMapping mapping = new ConstraintMapping();
+        mapping.setPathSpec("/*");
+        mapping.setMethodOmissions(new String[] { HttpMethod.OPTIONS });
+        mapping.setConstraint(constraint);
+		return mapping;
+	}
+
+
+	private static ConstraintSecurityHandler createConstraintSecurityHandler(final Server server, final LoginService loginService, final HandlerCollection handlerCollection) {
+		final ConstraintSecurityHandler security = new ConstraintSecurityHandler();
+        security.addConstraintMapping(getSecurityConstraintMapping());
+        security.setAuthenticator(new SVWSAuthenticator());
+        security.setLoginService(loginService);
+		security.setHandler(handlerCollection);
+        server.setHandler(security);
+        return security;
+	}
+
+
+	/**
+	 * Fügt einen Logging-Handler für die API-Anfragen hinzu
+	 *
+	 * @param handlerCollection   die Handler-Collection, wo der Logging Handler hinzugefügt werden soll
+	 */
+	private static void addLoggingHandler(final HandlerCollection handlerCollection) {
+		// logging
+		if (SVWSKonfiguration.get().isLoggingEnabled()) {
+			final String logPath = SVWSKonfiguration.get().getLoggingPath();
+			try {
+				Files.createDirectories(Paths.get(logPath));
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+			final RequestLogWriter logWriter = new RequestLogWriter(logPath + "/yyyy_mm_dd.request.log");
+			logWriter.setFilenameDateFormat("yyyy_MM_dd");
+			logWriter.setRetainDays(90);
+			logWriter.setAppend(true);
+			logWriter.setTimeZone("GMT");
+			final CustomRequestLog requestLog = new CustomRequestLog(logWriter, CustomRequestLog.EXTENDED_NCSA_FORMAT);
+			final RequestLogHandler requestLogHandler = new RequestLogHandler();
+			requestLogHandler.setRequestLog(requestLog);
+			handlerCollection.addHandler(requestLogHandler);
+		}
+	}
+
+
+	/**
+	 * Gibt einen ServerConnector für das Lauschen auf HTTP-Verbindungen auf dem übergebenen Port zurück.
+	 *
+	 * @param disableTLS         gibt an, ob TLS deaktiviert sein soll
+	 * @param preferHTTPv1_1     gibt an, ob HTTP-Verbindungen mit v1.1 gegenüber v2 bevorzugt werden sollen
+	 * @param port               der Port, auf dem gelauscht werden soll
+	 * @param keyStorePath       der Pfad zum Java-Key-Store für TLS-Verbindungen
+	 * @param keyStorePassword   das Kennwort für den Java-Key-Store
+	 *
+	 * @return der konfigurierte ServerConnector
+	 */
+	private ServerConnector getHttpServerConnector(final boolean disableTLS, final boolean preferHTTPv1_1, final int port,
+			final String keyStorePath, final String keyStorePassword) {
+		// HTTP Configuration
+		final HttpConfiguration http_config = new HttpConfiguration();
+		if (!disableTLS) {
+			http_config.setSecureScheme("https");
+			http_config.setSecurePort(port);
+		}
+		http_config.setOutputBufferSize(32768);
+		http_config.setRequestHeaderSize(8192);
+		http_config.setResponseHeaderSize(8192);
+		http_config.setSendServerVersion(true);
+		http_config.setSendDateHeader(false);
+
+		// SSL Context Factory
+		final SslContextFactory.Server sslContextFactory = new SslContextFactory.Server();
+		sslContextFactory.setKeyStorePath(keyStorePath + "/keystore");
+		sslContextFactory.setKeyStorePassword(keyStorePassword);
+		sslContextFactory.setKeyManagerPassword(keyStorePassword);
+		sslContextFactory.setTrustStorePath(keyStorePath + "/keystore");
+		sslContextFactory.setTrustStorePassword(keyStorePassword);
+		sslContextFactory.setIncludeProtocols("TLSv1.3", "TLSv1.2");
+		sslContextFactory.setIncludeCipherSuites("TLS_AES_256_GCM_SHA384", "TLS_CHACHA20_POLY1305_SHA256", "TLS_AES_128_GCM_SHA256",
+				"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256");
+		sslContextFactory.setSniRequired(false);
+		sslContextFactory.setCipherComparator(HTTP2Cipher.COMPARATOR);
+
+		// SSL HTTP Configuration
+		final HttpConfiguration https_config = new HttpConfiguration(http_config);
+		final SecureRequestCustomizer secureRequestCustomizer = new SecureRequestCustomizer();
+		if (!disableTLS) {
+			secureRequestCustomizer.setSniHostCheck(false);
+		}
+		https_config.addCustomizer(secureRequestCustomizer);
+
+		// HTTP Connection Factory (v1.1 oder v2)
+		final AbstractConnectionFactory connFactoryHTTPv1_1 = new HttpConnectionFactory(https_config);
+		final AbstractConnectionFactory connFactoryHTTPv2 = new HTTP2ServerConnectionFactory(https_config);
+		final ALPNServerConnectionFactory alpn = new ALPNServerConnectionFactory();
+		if (disableTLS) {
+			final ServerConnector connector = preferHTTPv1_1
+				? new ServerConnector(server, connFactoryHTTPv1_1, connFactoryHTTPv2)
+				: new ServerConnector(server, connFactoryHTTPv2, connFactoryHTTPv1_1);
+			connector.setPort(port);
+			return connector;
+		}
+		final ServerConnector connector = preferHTTPv1_1
+			? new ServerConnector(server, sslContextFactory, alpn, connFactoryHTTPv1_1, connFactoryHTTPv2)
+			: new ServerConnector(server, sslContextFactory, alpn, connFactoryHTTPv2, connFactoryHTTPv1_1);
+		connector.setPort(port);
+		return connector;
+	}
+
+
+	/**
+	 * Erzeugt eine neue HTTP- bzw. HTTPS-Konfiguration basierend auf der {@link SVWSKonfiguration}
+	 * und fügt diese zum Jetty-Server hinzu.
+	 */
+	@SuppressWarnings("resource")
+	private void addHTTPServerConnections() {
+		server.addConnector(getHttpServerConnector(
+			SVWSKonfiguration.get().isTLSDisabled(),
+			SVWSKonfiguration.get().useHTTPDefaultv11(),
+			SVWSKonfiguration.get().isTLSDisabled() ? SVWSKonfiguration.get().getPortHTTP() : SVWSKonfiguration.get().getPortHTTPS(),
+			SVWSKonfiguration.get().getTLSKeystorePath(),
+			SVWSKonfiguration.get().getTLSKeystorePassword()
+		));
+	}
+
+
+	/**
+	 * Fügt die Rest-Applikationen zum Server hinzu.
+	 */
+	private void addAPIApplications() {
+		addApplication(RestAppServer.class, "/db/*", "/config/*", "/status/*", "/api/*", "/openapi/server.json", "/openapi/server.yaml");
+		addApplication(RestAppClient.class, "/*");
+		addApplication(RestAppDav.class, "/dav/*");
+		addApplication(RestAppDebug.class, "/debug/*");
+		if (!SVWSKonfiguration.get().isDBRootAccessDisabled())
+			addApplication(RestAppSchemaRoot.class, "/api/schema/root/*", "/openapi/schemaroot.json", "/openapi/schemaroot.yaml");
+	}
+
 
 	/**
 	 * Fügt die angegebene API-Applikation zum Server hinzu.
@@ -263,16 +313,6 @@ public final class SvwsServer {
 		servlet.setInitParameter("jakarta.ws.rs.Application", c.getCanonicalName());
 	}
 
-	/**
-	 * Fügt die Rest-Applikationen zum Server hinzu.
-	 */
-	private void addAPIApplications() {
-		addApplication(RestAppServer.class, "/db/*", "/config/*", "/status/*", "/api/*", "/openapi/server.json", "/openapi/server.yaml");
-		addApplication(RestAppClient.class, "/*");
-		addApplication(RestAppDav.class, "/dav/*");
-		addApplication(RestAppDebug.class, "/debug/*");
-		if (!SVWSKonfiguration.get().isDBRootAccessDisabled())
-			addApplication(RestAppSchemaRoot.class, "/api/schema/root/*", "/openapi/schemaroot.json", "/openapi/schemaroot.yaml");
-	}
 
 }
+
