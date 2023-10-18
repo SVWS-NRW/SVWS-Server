b6c40b0f3ed5cce154a6f87d97875b6593bad9ef
hmt <dev@hmt.im>
2023-10-18 16:34:08 +0200

    node: update deps, vite 4.5

--- buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
+++ buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
@@ -34,7 +34,7 @@ abstract class NodePluginConfig {
     	version.convention('20.8.1'); // https://nodejs.org/en/download/current
     	npmVersion.convention('10.1.0');
     	tsVersion.convention('5.2.2'); // https://github.com/microsoft/TypeScript/releases
-    	tsNodeTypesVersion.convention('20.8.6'); // https://www.npmjs.com/package/@types/node
+    	tsNodeTypesVersion.convention('20.8.7'); // https://www.npmjs.com/package/@types/node
     }
     
     boolean isLinux() {
