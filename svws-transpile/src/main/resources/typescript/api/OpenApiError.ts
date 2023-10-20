90c9b6c6e9166342a9748f78d85db8b8cfc60f1c
Thomas Bachran <thomas.bachran@gmx.de>
2023-10-20 16:39:40 +0200

    API: OpenApiError um vollständige Response erweitert

--- svws-transpile/src/main/resources/typescript/api/OpenApiError.ts
+++ svws-transpile/src/main/resources/typescript/api/OpenApiError.ts
@@ -3,11 +3,13 @@ export class OpenApiError extends Error {
 
 	_status : number | null;
 	_error : Error | null;
+	_response : Response | null;
 
 	public constructor(value : Response | Error, message? : string) {
 		super(message);
 		this._status = (value instanceof Error) ? null : value.status;
 		this._error = (value instanceof Error) ? value : null;
+		this._response = (value instanceof Error) ? null : value;
 	}
 
 	public get status() : number | null {
@@ -16,6 +18,10 @@ export class OpenApiError extends Error {
 
 	public get error() : Error | null {
 		return this._error;
+	}
+	
+	public get response() : Response | null {
+		return this._response;
 	}
 
 }
