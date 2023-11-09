a100353130a7f9566a8c77c7f5ff949c7f0654ac
ThomasBachran <thomas.bachran@gmx.de>
2023-11-09 10:04:22 +0100

    API: Korrektur: Die Methode destroySchema liefert keinen Log.

--- svws-openapi/src/main/java/de/svws_nrw/api/privileged/APISchemaPrivileged.java
+++ svws-openapi/src/main/java/de/svws_nrw/api/privileged/APISchemaPrivileged.java
@@ -50,7 +50,6 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
