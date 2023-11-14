333d43b426a8e49e43cf381129e6964d68d18d93
Thomas Bachran <thomas.bachran@gmx.de>
2023-11-14 15:05:55 +0100

    Benutzerkompetenz für Datenaustausch mit Schüler-Online angelegt

--- svws-core/src/main/java/de/svws_nrw/core/types/benutzer/BenutzerKompetenz.java
+++ svws-core/src/main/java/de/svws_nrw/core/types/benutzer/BenutzerKompetenz.java
@@ -118,6 +118,11 @@ public enum BenutzerKompetenz {
     	46, BenutzerKompetenzGruppe.IMPORT_EXPORT, "Export über XML-Schnittstellen", null
     )),
 
+    /** Es werden Rechte zum Import und Export über die Schüler-Online-Schnittstelle benötigt. */
+    IMPORT_EXPORT_SCHUELER_ONLINE(new BenutzerKompetenzKatalogEintrag(
+    	47, BenutzerKompetenzGruppe.IMPORT_EXPORT, "Datenaustausch mit Schüler-Online", null
+    )),
+
     /** Es werden Rechte zum Ansehen der Schulbezogenen Daten benötigt. */
     SCHULBEZOGENE_DATEN_ANSEHEN(new BenutzerKompetenzKatalogEintrag(
     	61, BenutzerKompetenzGruppe.SCHULBEZOGENE_DATEN, "Ansehen", null
