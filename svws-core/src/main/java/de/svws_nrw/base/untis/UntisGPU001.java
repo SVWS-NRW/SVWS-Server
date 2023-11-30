34e416a00255359adbcb4f8c451aaae5ede25db9
ThomasBachran <thomas.bachran@gmx.de>
2023-11-30 18:21:36 +0100

    Import aus Untis: Einlesen GPU001.txt

---
+++ svws-core/src/main/java/de/svws_nrw/base/untis/UntisGPU001.java
@@ -0,0 +1,82 @@
+package de.svws_nrw.base.untis;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * Diese Klasse dient als DTO für die CSV-Datei {@code GPU001.txt} des
+ * Untis-Datenaustausch-Formates und enthält damit eine Beschreibung des
+ * Stundenplans.
+ */
+public class UntisGPU001 {
+
+	/** Eine numerische ID, welche den Unterricht eindeutig identifiziert (z.B. 42) */
+	public long idUnterricht;
+
+	/** Das Kürzel der Klasse (z.B. "05A") */
+	public String klasseKuerzel;
+
+	/** Das Kürzel des Lehrers (z.B. "BACH") */
+	public String lehrerKuerzel;
+
+	/** Das Kürzel des Fachen (z.B. "D") */
+	public String fachKuerzel;
+
+	/** Das Kürzel des Raumes (z.B. "") */
+	public String raumKuerzel;
+
+	/** Der Wochentag für den Unterricht */
+	public int wochentag;
+
+	/** Die Stunde im Zeitraster des Wochentags */
+	public int stunde;
+
+	/** Ggf. die Stundenlänge in Minuten oder leer */
+	public Integer dauer = null;
+
+	/** Ignorieren - Dummy für das Einlesen der Daten */
+	public String dummy;
+
+
+
+	/** Die Instanz des Object-Readers für die CSV-Daten */
+	private static final ObjectReader reader = new CsvMapper().readerFor(UntisGPU001.class).with(CsvSchema.builder()
+		.addNumberColumn("idUnterricht")
+		.addColumn("klasseKuerzel")
+		.addColumn("lehrerKuerzel")
+		.addColumn("fachKuerzel")
+		.addColumn("raumKuerzel")
+		.addNumberColumn("wochentag")
+		.addNumberColumn("stunde")
+		.addNumberColumn("dauer")
+		.addColumn("dummy")
+		.build()
+		.withColumnSeparator(';')
+		.withQuoteChar('\"')
+		.withNullValue("")
+		.withoutHeader()
+	);
+
+	/**
+	 * Erstellt aus den übergebenen CSV-Daten eine Liste der GPU001-Datensätze
+	 *
+	 * @param csvData   die Daten des CSV-Datei als String
+	 *
+	 * @return eine Liste mit den GPU001-Datensätzen
+	 *
+	 * @throws IOException falls die CSV-Datei nicht korrekt gelesen werden kann
+	 */
+	public static @NotNull List<@NotNull UntisGPU001> readCSV(final String csvData) throws IOException {
+		try (MappingIterator<UntisGPU001> it = reader.readValues(csvData)) {
+			return it.readAll();
+		}
+	}
+
+}
---
+++ svws-core/src/test/java/de/svws_nrw/base/untis/TestGPU.java
@@ -0,0 +1,36 @@
+package de.svws_nrw.base.untis;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import de.svws_nrw.base.ResourceUtils;
+
+/**
+ * Diese Klasse prüft das Einlesen von Untis-GPU-Dateien.
+ */
+class TestGPU {
+
+	private static final String PFAD_DATEN = "de/svws_nrw/base/untis/";
+
+	/**
+	 * Tests zum Einlesen von GPU001-Dateien
+	 */
+	@Test
+	void testGPU001() {
+		try {
+			final Path path = ResourceUtils.getFile(PFAD_DATEN + "GPU001.txt");
+			final String strGPU001 = Files.readString(path);
+			final List<UntisGPU001> unterrichte = UntisGPU001.readCSV(strGPU001);
+			for (final UntisGPU001 unterricht : unterrichte)
+				System.out.println(unterricht.idUnterricht + " -> " + unterricht.klasseKuerzel + " " + unterricht.fachKuerzel + " " + unterricht.lehrerKuerzel);
+		} catch (final Exception e) {
+			fail(e);
+		}
+	}
+
+}
