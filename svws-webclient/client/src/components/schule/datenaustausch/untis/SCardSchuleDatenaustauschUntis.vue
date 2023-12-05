01827d6acae40b96c07196079c9f63f806bee752
hmt <dev@hmt.im>
2023-12-05 22:15:07 +0100

    Import Untis: Anpassung Datenaustausch

--- svws-webclient/client/src/components/schule/datenaustausch/untis/SCardSchuleDatenaustauschUntis.vue
+++ svws-webclient/client/src/components/schule/datenaustausch/untis/SCardSchuleDatenaustauschUntis.vue
@@ -3,9 +3,9 @@
 		<div class="content-card--content">
 			<div class="flex flex-col items-start gap-3 p-2">
 				<svws-ui-input-wrapper :grid="2">
+					<abschnitt-auswahl :disable-headless="true" :akt-abschnitt="aktAbschnitt" :abschnitte="abschnitte" :set-abschnitt="a => setAbschnitt(a)" :akt-schulabschnitt="aktSchulabschnitt" />
 					<svws-ui-text-input placeholder="Bezeichnung" v-model="bezeichnung" type="text" />
 					<svws-ui-text-input placeholder="Gültig ab" v-model="gueltigAb" type="date" />
-					<abschnitt-auswahl :headless="false" :akt-abschnitt="aktAbschnitt" :abschnitte="abschnitte" :set-abschnitt="(a) => abschnitt = a" :akt-schulabschnitt="aktSchulabschnitt" />
 					<div class="col-span-full">
 						<input type="file" accept=".txt" @change="onFileChanged" :disabled="loading">
 					</div>
@@ -26,7 +26,7 @@
 
 	import { ref } from "vue";
 	import type { List, Schuljahresabschnitt, SimpleOperationResponse } from "@core";
-	import { StundenplanListeEintragMinimal, DateUtils } from "@core";
+	import { StundenplanListeEintragMinimal } from "@core";
 
 	const props = defineProps<{
 		setUntisImportGPU: (formData: FormData) => Promise<SimpleOperationResponse>;
@@ -35,8 +35,9 @@
 		aktSchulabschnitt: number;
 	}>();
 
-	const bezeichnung = ref<string>("");
-	const gueltigAb = ref<string>("");
+	const bezeichnung = ref<string>("Import Untis");
+	// eslint-disable-next-line vue/no-setup-props-destructure
+	const gueltigAb = ref<string>(getGueltigAb(props.aktAbschnitt));
 	// eslint-disable-next-line vue/no-setup-props-destructure
 	const abschnitt = ref<Schuljahresabschnitt>(props.aktAbschnitt);
 	const file = ref<File | null>(null);
@@ -51,6 +52,17 @@
 			file.value = target.files[0];
 	}
 
+	function getGueltigAb(ab: Schuljahresabschnitt) {
+		const a = ab.abschnitt;
+		const s = ab.schuljahr;
+		return a === 1 ? `${s}-08-01` : `${s+1}-02-01`
+	}
+
+	function setAbschnitt(a: Schuljahresabschnitt) {
+		gueltigAb.value = getGueltigAb(a);
+		abschnitt.value = a;
+	}
+
 	async function import_file(event: Event) {
 		if (!file.value || abschnitt.value === undefined || bezeichnung.value.length < 1)
 			return;
@@ -60,13 +72,6 @@
 		entry.bezeichnung = bezeichnung.value;
 		entry.idSchuljahresabschnitt = abschnitt.value.id;
 		entry.gueltigAb = gueltigAb.value;
-		try {
-			DateUtils.extractFromDateISO8601(entry.gueltigAb);
-		} catch (e) {
-			const a = abschnitt.value.abschnitt;
-			const s = abschnitt.value.schuljahr;
-			entry.gueltigAb = a === 1 ? `${s}-08-01` : `${s+1}-02-01`
-		}
 		const formData = new FormData();
 		formData.append('entry', StundenplanListeEintragMinimal.transpilerToJSON(entry));
 		formData.append("data", file.value);
