b7b89286ad76be823d5efa50db9e7e9854819688
Christian Ritter <christian.ritter@conet-isb.de>
2024-01-10 15:47:50 +0000

    refs #1417 Docker Container für Integrations-Tests erhalten die
    Co-authored-by: critter <Christian.Ritter@conet-isb.de>

--- testing/svws-dav-api-test/init-scripts/01_davapidaten.sh
+++ testing/svws-dav-api-test/init-scripts/01_davapidaten.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Setze Admin und Lehrerpasswort"
-mariadb -h svws-dav-api-test-db -u ${MariaDB_USER} -p${MariaDB_PASSWORD} < ${INIT_SCRIPTS_DIR}/davapidaten.sql
+mariadb -h $MariaDB_HOST -u ${MariaDB_USER} -p${MariaDB_PASSWORD} < ${INIT_SCRIPTS_DIR}/davapidaten.sql
