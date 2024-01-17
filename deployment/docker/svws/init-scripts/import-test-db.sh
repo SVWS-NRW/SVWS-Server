ee2b854c7bfb27e7d1f5a4a70446d7ff39d25384
mbrass <m.brass@babiel.com>
2024-01-17 13:04:40 +0100

    #1226 tg mit root user name per MariaDB_ROOT_USER für MigrateDB

--- deployment/docker/svws/init-scripts/import-test-db.sh
+++ deployment/docker/svws/init-scripts/import-test-db.sh
@@ -19,11 +19,30 @@ rm -rf $CURRENT_DIR/databases.zip
 # Import (MigrateDB) der Access-Datenbank(en) durchführen ...
 MDBFILE="$CURRENT_DIR/databases/SVWS-TestMDBs-main/GOST_Abitur/Abi-Test-Daten-01/GymAbi.mdb"
 echo "Importiere Datenbank: ${MDBFILE} ..."
-java -cp "svws-server-app-*.jar:./*:./lib/*" de.svws_nrw.db.utils.app.MigrateDB -j -r -1 -sd "MDB" \
-   -sl "${MDBFILE}" -sp "${TESTDB_PASSWORD}" \
-   -td "MARIA_DB" \
-   -tl ${MariaDB_HOST} \
-   -ts ${MariaDB_DATABASE} \
-   -tu ${MariaDB_USER} \
-   -tp ${MariaDB_PASSWORD} \
-   -tr ${MariaDB_ROOT_PASSWORD}
+
+# Überprüfe, ob die Umgebungsvariable MariaDB_ROOT_USER gesetzt ist
+if [ -n "$MariaDB_ROOT_USER" ]; then
+    # Wenn MariaDB_ROOT_USER gesetzt ist, führe den Code für den Fall aus
+	java -cp "svws-server-app-*.jar:./*:./lib/*" de.svws_nrw.db.utils.app.MigrateDB -j -r -1 -sd "MDB" \
+	   -sl "${MDBFILE}" -sp "${TESTDB_PASSWORD}" \
+	   -td "MARIA_DB" \
+	   -tl ${MariaDB_HOST} \
+	   -ts ${MariaDB_DATABASE} \
+	   -tu ${MariaDB_USER} \
+	   -tp ${MariaDB_PASSWORD} \
+	   -tg ${MariaDB_ROOT_USER} \
+	   -tr ${MariaDB_ROOT_PASSWORD}
+else
+    # Wenn MariaDB_ROOT_USER nicht gesetzt ist, führe den Code für den anderen Fall aus
+	java -cp "svws-server-app-*.jar:./*:./lib/*" de.svws_nrw.db.utils.app.MigrateDB -j -r -1 -sd "MDB" \
+	   -sl "${MDBFILE}" -sp "${TESTDB_PASSWORD}" \
+	   -td "MARIA_DB" \
+	   -tl ${MariaDB_HOST} \
+	   -ts ${MariaDB_DATABASE} \
+	   -tu ${MariaDB_USER} \
+	   -tp ${MariaDB_PASSWORD} \
+	   -tr ${MariaDB_ROOT_PASSWORD}
+fi
+
+
+
