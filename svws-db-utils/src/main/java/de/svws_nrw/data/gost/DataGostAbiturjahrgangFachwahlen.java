e65dd29e81633413a13642c2623e436d823d0003
ThomasBachran <thomas.bachran@gmx.de>
2024-01-20 20:26:13 +0100

    Oberstufe-Fachwahlen: Performance-Optimierung (Teil 3)

--- svws-db-utils/src/main/java/de/svws_nrw/data/gost/DataGostAbiturjahrgangFachwahlen.java
+++ svws-db-utils/src/main/java/de/svws_nrw/data/gost/DataGostAbiturjahrgangFachwahlen.java
@@ -7,9 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import de.svws_nrw.core.data.gost.AbiturFachbelegung;
-import de.svws_nrw.core.data.gost.AbiturFachbelegungHalbjahr;
-import de.svws_nrw.core.data.gost.Abiturdaten;
+import de.svws_nrw.core.data.gost.GostFachwahl;
 import de.svws_nrw.core.data.gost.GostJahrgangFachwahlen;
 import de.svws_nrw.core.data.gost.GostJahrgangFachwahlenHalbjahr;
 import de.svws_nrw.core.data.gost.GostStatistikFachwahl;
@@ -22,7 +20,6 @@ import de.svws_nrw.db.DBEntityManager;
 import de.svws_nrw.db.dto.current.schild.faecher.DTOFach;
 import de.svws_nrw.db.dto.current.schild.schueler.DTOSchueler;
 import de.svws_nrw.db.dto.current.schild.schule.DTOSchuljahresabschnitte;
-import de.svws_nrw.db.dto.current.views.gost.DTOViewGostSchuelerAbiturjahrgang;
 import de.svws_nrw.db.utils.OperationError;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -88,69 +85,64 @@ public final class DataGostAbiturjahrgangFachwahlen extends DataManager<Long> {
 	 * @return die Statistik zu den Fachwahlen des Abiturjahrgangs dieses Objektes
 	 */
 	public List<GostStatistikFachwahl> getFachwahlen() {
-		DBUtilsGost.pruefeSchuleMitGOSt(conn);
 		// Lese die Fachliste aus der DB
 		final Map<Long, DTOFach> faecher = conn.queryAll(DTOFach.class).stream().collect(Collectors.toMap(f -> f.ID, f -> f));
 		if ((faecher == null) || (faecher.size() == 0))
 			return new ArrayList<>();
-    	// Bestimme alle Schüler-IDs des angegebenen Abiturjahrgangs
-		final List<DTOViewGostSchuelerAbiturjahrgang> schuelerAbijahrgang = conn.queryNamed("DTOViewGostSchuelerAbiturjahrgang.abiturjahr", abijahr, DTOViewGostSchuelerAbiturjahrgang.class);
-		if ((schuelerAbijahrgang == null) || (schuelerAbijahrgang.isEmpty()))
-			return new ArrayList<>();
-		final HashMap<Long, GostStatistikFachwahl> fachwahlen = new HashMap<>();
-		for (final DTOViewGostSchuelerAbiturjahrgang schueler : schuelerAbijahrgang) {
-			final Abiturdaten abidaten = DBUtilsGostLaufbahn.get(conn, schueler.ID);
-			for (final AbiturFachbelegung belegung : abidaten.fachbelegungen) {
-				final DTOFach fach = faecher.get(belegung.fachID);
+		// Bestimme die Fachwahlen des Abiturjahrganges
+		final GostJahrgangFachwahlen wahlen = this.getSchuelerFachwahlen();
+		final HashMap<Long, GostStatistikFachwahl> matrixFachwahlen = new HashMap<>();
+		for (final GostHalbjahr halbjahr : GostHalbjahr.values()) {
+			if (wahlen.halbjahr[halbjahr.id] == null)
+				continue;
+			for (final GostFachwahl wahl : wahlen.halbjahr[halbjahr.id].fachwahlen) {
+				final DTOFach fach = faecher.get(wahl.fachID);
 				if (fach == null)
 					continue;
-		        if ((belegung.belegungen[GostHalbjahr.EF1.id] == null) && (belegung.belegungen[GostHalbjahr.EF2.id] == null) && (belegung.belegungen[GostHalbjahr.Q11.id] == null)
-		        		&& (belegung.belegungen[GostHalbjahr.Q12.id] == null) && (belegung.belegungen[GostHalbjahr.Q21.id] == null) && (belegung.belegungen[GostHalbjahr.Q22.id] == null))
-		            continue;
-		        GostStatistikFachwahl statfw = fachwahlen.get(belegung.fachID);
+				GostStatistikFachwahl statfw = matrixFachwahlen.get(fach.ID);
 		        if (statfw == null) {
 		        	statfw = new GostStatistikFachwahl();
 		        	statfw.abiturjahr = abijahr;
-		        	statfw.id = fach.ID;
+		        	statfw.id = wahl.fachID;
 		            statfw.kuerzel = fach.Kuerzel;
 		            statfw.bezeichnung = fach.Bezeichnung;
 		            statfw.kuerzelStatistik = fach.StatistikFach.daten.kuerzelASD;
-		            for (final GostHalbjahr halbjahr : GostHalbjahr.values())
-		            	statfw.fachwahlen[halbjahr.id] = new GostStatistikFachwahlHalbjahr();
-		            fachwahlen.put(statfw.id, statfw);
+		            for (final GostHalbjahr hj : GostHalbjahr.values())
+		            	statfw.fachwahlen[hj.id] = new GostStatistikFachwahlHalbjahr();
+		            matrixFachwahlen.put(statfw.id, statfw);
 		        }
-				for (final GostHalbjahr halbjahr : GostHalbjahr.values()) {
-					if (belegung.belegungen[halbjahr.id] != null) {
-						final AbiturFachbelegungHalbjahr belegungHj = belegung.belegungen[halbjahr.id];
-				        final GostKursart kursart = GostKursart.fromKuerzel(belegungHj.kursartKuerzel);
-				        if (kursart != null) {
-							switch (kursart) {
-								case GK -> {
-									statfw.fachwahlen[halbjahr.id].wahlenGK++;
-									if (belegungHj.schriftlich)
-										statfw.fachwahlen[halbjahr.id].wahlenGKSchriftlich++;
-									else
-										statfw.fachwahlen[halbjahr.id].wahlenGKMuendlich++;
-								}
-								case LK -> statfw.fachwahlen[halbjahr.id].wahlenLK++;
-								case PJK, VTF -> {
-									statfw.fachwahlen[halbjahr.id].wahlenGK++;
-									statfw.fachwahlen[halbjahr.id].wahlenGKMuendlich++;
-								}
-								case ZK -> statfw.fachwahlen[halbjahr.id].wahlenZK++;
-							}
+		        final GostKursart kursart = GostKursart.fromIDorNull(wahl.kursartID);
+		        if (kursart != null) {
+					switch (kursart) {
+						case GK -> {
+							statfw.fachwahlen[halbjahr.id].wahlenGK++;
+							if (wahl.istSchriftlich)
+								statfw.fachwahlen[halbjahr.id].wahlenGKSchriftlich++;
+							else
+								statfw.fachwahlen[halbjahr.id].wahlenGKMuendlich++;
 						}
+						case LK -> statfw.fachwahlen[halbjahr.id].wahlenLK++;
+						case PJK, VTF -> {
+							statfw.fachwahlen[halbjahr.id].wahlenGK++;
+							statfw.fachwahlen[halbjahr.id].wahlenGKMuendlich++;
+						}
+						case ZK -> statfw.fachwahlen[halbjahr.id].wahlenZK++;
 					}
 				}
-		        if (belegung.abiturFach != null) {
-		        	if (belegung.abiturFach == 3)
-		        		statfw.wahlenAB3++;
-		        	if (belegung.abiturFach == 4)
-		        		statfw.wahlenAB4++;
-		        }
 			}
 		}
-		return fachwahlen.values().stream()
+		for (final GostFachwahl wahl : wahlen.abitur.fachwahlen) {
+			final GostStatistikFachwahl statfw = matrixFachwahlen.get(wahl.fachID);
+			if (statfw == null)
+				continue;
+	        if (wahl.abiturfach != null) {
+	        	if (wahl.abiturfach == 3)
+	        		statfw.wahlenAB3++;
+	        	if (wahl.abiturfach == 4)
+	        		statfw.wahlenAB4++;
+	        }
+		}
+		return matrixFachwahlen.values().stream()
 				.sorted((a, b) -> Integer.compare(faecher.get(a.id).SortierungAllg, faecher.get(b.id).SortierungAllg))
 				.toList();
 	}
