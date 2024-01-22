8f90da480925fdbf4f2ad3d6e817bc3c7c30addd
ThomasBachran <thomas.bachran@gmx.de>
2024-01-22 09:20:21 +0100

    Optimierung des MDB-Zugriffs

--- svws-db/src/main/java/de/svws_nrw/db/ConnectionManager.java
+++ svws-db/src/main/java/de/svws_nrw/db/ConnectionManager.java
@@ -1,5 +1,7 @@
 package de.svws_nrw.db;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,6 +15,9 @@ import java.util.Random;
 import org.eclipse.persistence.exceptions.DatabaseException;
 import org.eclipse.persistence.sessions.server.ConnectionPool;
 import org.eclipse.persistence.sessions.server.ServerSession;
+
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.DatabaseBuilder;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -129,17 +134,26 @@ public final class ConnectionManager {
 	private @NotNull EntityManagerFactory createEntityManagerFactory() {
 		final HashMap<String, Object> propertyMap = new HashMap<>();
 		propertyMap.put("jakarta.persistence.jdbc.driver", config.getDBDriver().getJDBCDriver());
+		final String username = config.getUsername();
+		String password = config.getPassword();
 		String url = config.getDBDriver().getJDBCUrl(config.getDBLocation(), config.getDBSchema());
-		if (config.getDBDriver() == DBDriver.MDB && config.createDBFile())
-			url += ";newdatabaseversion=V2000";
+		if (config.getDBDriver() == DBDriver.MDB) {
+			try (Database db = DatabaseBuilder.open(new File(config.getDBLocation()))) {
+				password = db.getDatabasePassword();
+			} catch (@SuppressWarnings("unused") final IOException e) {
+				password = "";
+			}
+			if (config.createDBFile())
+				url += ";newdatabaseversion=V2000";
+		}
 		final String sessionName = "SVWSDB_url=" + url + "_user=" + config.getUsername() + "_random=" + random.ints(48, 123)  // from 0 to z
 	        .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))  // filter some unicode characters
 	        .limit(40)
 	        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
 	        .toString();
 		propertyMap.put("jakarta.persistence.jdbc.url", url);
-		propertyMap.put("jakarta.persistence.jdbc.user", config.getUsername());
-		propertyMap.put("jakarta.persistence.jdbc.password", config.getPassword());
+		propertyMap.put("jakarta.persistence.jdbc.user", username);
+		propertyMap.put("jakarta.persistence.jdbc.password", password);
 		propertyMap.put("eclipselink.session-name", sessionName);
 		propertyMap.put("eclipselink.flush", "true");
 		propertyMap.put("eclipselink.persistence-context.flush-mode", "commit");
