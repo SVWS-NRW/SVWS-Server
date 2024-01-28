0fd1c95070529619018ec1b4e69ae0c74237dbb5
Benjamin A. Bartsch <benjamin.bartsch@gymnasium-eickel.nrw.schule>
2024-01-29 00:06:17 +0100

    KlausurblockungNachschreiberAlgorithmus fertig

--- svws-core/src/main/java/de/svws_nrw/core/utils/gost/klausurplanung/KlausurblockungNachschreiberAlgorithmus.java
+++ svws-core/src/main/java/de/svws_nrw/core/utils/gost/klausurplanung/KlausurblockungNachschreiberAlgorithmus.java
@@ -8,7 +8,10 @@ import de.svws_nrw.core.adt.Pair;
 import de.svws_nrw.core.data.gost.klausurplanung.GostKlausurtermin;
 import de.svws_nrw.core.data.gost.klausurplanung.GostSchuelerklausur;
 import de.svws_nrw.core.data.gost.klausurplanung.GostSchuelerklausurTermin;
+import de.svws_nrw.core.exceptions.DeveloperNotificationException;
+import de.svws_nrw.core.kursblockung.KursblockungMatrix;
 import de.svws_nrw.core.logger.Logger;
+import de.svws_nrw.core.utils.ListUtils;
 import jakarta.validation.constraints.NotNull;
 
 /**
@@ -23,6 +26,8 @@ public class KlausurblockungNachschreiberAlgorithmus {
 
 	/** Ein Logger für Debug-Zwecke. */
 	private final @NotNull Logger _logger;
+
+	private boolean _regel_nachschreiber_der_selben_klausur_auf_selbe_termine_verteilen = false;
 
 	/**
 	 * Der Konstruktor.
@@ -41,32 +46,129 @@ public class KlausurblockungNachschreiberAlgorithmus {
 	}
 
 	/**
-	 * @param pNachschreiber        Die Eingabe beinhaltet alle Schülerklausurtermine, die auf die Klausurtermine in der Liste pTermine verteilt werden müssen.
-	 * @param pTermine				Die Liste der GostKlausurtermine, auf die die Nachschreiber verteilt werden sollen
-	 * @param pManager				Der Kursklausur-Manager
-	 * @param pMaxTimeMillis  Logger für Benutzerhinweise, Warnungen und Fehler.
+	 * Aktiviert/Deaktiviert die Regel. Falls TRUE, werden NachschreiberInnen der selben Klausur auf den selben Termin geblockt.
+	 *
+	 * @param b falls TRUE, wird die Regel angewandt.
+	 */
+	public void set_regel_nachschreiber_der_selben_klausur_auf_selbe_termine_verteilen(final boolean b) {
+		this._regel_nachschreiber_der_selben_klausur_auf_selbe_termine_verteilen = b;
+	}
+
+	/**
+	 * @param nachschreiber   Alle "GostSchuelerklausurTermin", die auf die "GostKlausurtermin" verteilt werden sollen.
+	 * @param termine         Alle "GostKlausurtermin", auf die potentiell Nachschreiber verteilt werden.
+	 * @param klausurManager  Der Kursklausur-Manager.
+	 * @param maxTimeMillis   Die maximal erlaubte Berechnungszeit (in Millisekunden).
+	 *
 	 * @return Eine Liste von Paaren: 1. Element = GostSchuelerklausurtermin (Nachschreiber), 2. Element = ID des Termins / der Schiene
 	 */
-	public @NotNull List<@NotNull Pair<@NotNull GostSchuelerklausurTermin, @NotNull Long>> berechne(final @NotNull List<@NotNull GostSchuelerklausurTermin> pNachschreiber, final @NotNull List<@NotNull GostKlausurtermin> pTermine, final @NotNull GostKursklausurManager pManager, final long pMaxTimeMillis) {
+	public @NotNull List<@NotNull Pair<@NotNull GostSchuelerklausurTermin, @NotNull Long>> berechne(
+					final @NotNull List<@NotNull GostSchuelerklausurTermin> nachschreiber,
+					final @NotNull List<@NotNull GostKlausurtermin> termine,
+					final @NotNull GostKursklausurManager klausurManager,
+					final long maxTimeMillis) {
 
-		for (GostSchuelerklausurTermin skt : pNachschreiber) {
-			// Diese Schleife läuft über alle Nachschreiber, die verteilt werden müssen
+		// 1) Bilde Gruppen von Nachschreibern, falls dies bestimmte Kriterien/Regeln erfordern.
+		final @NotNull List<@NotNull List<@NotNull GostSchuelerklausurTermin>> nachschreiberGruppen = new ArrayList<>(); // Liste der Gruppen.
 
-			// ZU VERTEILENDER SCHÜLER
-			GostSchuelerklausur sk = pManager.schuelerklausurBySchuelerklausurtermin(skt);
-			long idSchueler = sk.idSchueler; // die ID des Schülers, die innerhalb jedes Klausurtermins / Schiene einzigartig sein muss
-			long idKurs = sk.idKursklausur; // die ID der Kursklausur als Gütekriterium, möglichst alle mit gleicher Kursklausur innerhalb eines Termins
+		for (final @NotNull GostSchuelerklausurTermin skt : nachschreiber) {
+			final GostSchuelerklausur sk = klausurManager.schuelerklausurBySchuelerklausurtermin(skt);
+			final long idSchueler = sk.idSchueler; // die ID des Schülers, die innerhalb jedes Klausurtermins / Schiene einzigartig sein muss
+			final long idKurs = sk.idKursklausur; // die ID der Kursklausur als Gütekriterium, möglichst alle mit gleicher Kursklausur innerhalb eines Termins
+			DeveloperNotificationException.ifTrue("Ungültige Schüler-ID = " + idSchueler, idSchueler < 0);
+			DeveloperNotificationException.ifTrue("Ungültige Kurs-ID = " + idKurs, idKurs < 0);
 
-			// SCHIENE MIT SCHON VORHANDENEN SCHÜLERN
-			GostKlausurtermin schiene1 = pTermine.getFirst();
-			if (schiene1 != null) {
-				List<GostSchuelerklausur> schuelerklausuren = pManager.schuelerklausurGetMengeByTerminid(schiene1.id);
-			}
-			// alle Schülerklausuren, die an dem Termin bereits in der Schiene sitzen
+			// Überprüfe, ob "skt" in eine bereits vorhandene Gruppe darf.
+			boolean added = false;
+			for (final @NotNull List<@NotNull GostSchuelerklausurTermin> gruppe : nachschreiberGruppen)
+				if (istHinzufuegenErlaubt(gruppe, skt, klausurManager)) {
+					gruppe.add(skt);
+					added = true;
+					break;
+				}
 
+			// Erzeuge eine neue Gruppe für "skt".
+			if (!added)
+				nachschreiberGruppen.add(ListUtils.create1(skt));
 		}
 
-		return new ArrayList<>();
+
+		// 2) Matrix für den Algorithmus erzeugen (Gruppe r wird Termin c zugeordnet).
+		final int rows = nachschreiberGruppen.size();
+		final int cols = termine.size();
+		final @NotNull KursblockungMatrix matrix = new KursblockungMatrix(_random, rows, cols);
+		final @NotNull long @NotNull [][] data = matrix.getMatrix();
+		for (int r = 0; r < nachschreiberGruppen.size(); r++)
+			for (int c = 0; c < termine.size(); c++)
+				data[r][c] = gibBewertung(nachschreiberGruppen.get(r), termine.get(c), klausurManager); // Darf Gruppe r dem Termin c zugeordnet werden?
+
+
+		// 3) Optimale Zuordnung berechnen.
+		final int[] r2c = matrix.gibMaximalesBipartitesMatching(true);
+
+
+		// 4) Ergebnis erzeugen.
+		final @NotNull List<@NotNull Pair<@NotNull GostSchuelerklausurTermin, @NotNull Long>> ergebnis = new ArrayList<>();
+
+		long dummyTerminID = -1;
+		for (int r = 0; r < nachschreiberGruppen.size(); r++) {
+			// Bestimme, ob die ID von einem echten Termin kommt, oder eine Dummy-ID verwendet wird.
+			long idTermin = dummyTerminID;
+
+			final int c = r2c[r];
+			if (c >= 0) {
+				idTermin = termine.get(c).id;
+			} else {
+				dummyTerminID--;
+			}
+
+			// Verteile alle "GostSchuelerklausurTermin" der Gruppe auf den Termin.
+			for (final @NotNull GostSchuelerklausurTermin nachschreiberDerGruppe : nachschreiberGruppen.get(r))
+				ergebnis.add(new Pair<>(nachschreiberDerGruppe, idTermin));
+		}
+
+		return ergebnis;
+	}
+
+	private static long gibBewertung(
+					final @NotNull List<@NotNull GostSchuelerklausurTermin> nachschreiberGruppe,
+					final @NotNull GostKlausurtermin termin,
+					final @NotNull GostKursklausurManager klausurManager) {
+
+		for (final @NotNull GostSchuelerklausur sk1 : klausurManager.schuelerklausurGetMengeByTerminid(termin.id)) { // Alle Schüler des Termins...
+			for (final @NotNull GostSchuelerklausurTermin skt : nachschreiberGruppe) { // Alle Schüler der Gruppe ...
+				final GostSchuelerklausur sk2 = klausurManager.schuelerklausurBySchuelerklausurtermin(skt);
+				if (sk1.idSchueler == sk2.idSchueler)
+					return 0; // Die Gruppe darf nicht an dem Termin liegen.
+			}
+		}
+
+		return 1;  // Die Gruppe darf an dem Termin liegen.
+	}
+
+	private boolean istHinzufuegenErlaubt(
+					final @NotNull List<@NotNull GostSchuelerklausurTermin> gruppe,
+					final @NotNull GostSchuelerklausurTermin skt1,
+					final @NotNull GostKursklausurManager klausurManager) {
+
+		final @NotNull GostSchuelerklausur sk1 = klausurManager.schuelerklausurBySchuelerklausurtermin(skt1);
+
+		// Verbiete, dass ein Schüler doppelt in einer Gruppe ist.
+		for (final @NotNull GostSchuelerklausurTermin skt2 : gruppe) {
+			final @NotNull GostSchuelerklausur sk2 = klausurManager.schuelerklausurBySchuelerklausurtermin(skt2);
+			if (sk1.idSchueler == sk2.idSchueler)
+				return false;
+		}
+
+		// Sollen Schüler der selben Kurs-Klausur in der selben Gruppe landen?
+		if (_regel_nachschreiber_der_selben_klausur_auf_selbe_termine_verteilen) {
+			final @NotNull GostSchuelerklausurTermin skt2 = ListUtils.getNonNullElementAtOrException(gruppe, 0);
+			final @NotNull GostSchuelerklausur sk2 = klausurManager.schuelerklausurBySchuelerklausurtermin(skt2);
+			return (sk1.idKursklausur == sk2.idKursklausur);
+		}
+
+		// Ein Hinzufügen zu dieser Gruppe ist nicht erlaubt.
+		return false;
 	}
 
 }
--- svws-webclient/core/src/core/utils/gost/klausurplanung/KlausurblockungNachschreiberAlgorithmus.ts
+++ svws-webclient/core/src/core/utils/gost/klausurplanung/KlausurblockungNachschreiberAlgorithmus.ts
@@ -1,11 +1,14 @@
 import { JavaObject } from '../../../../java/lang/JavaObject';
-import { Random } from '../../../../java/util/Random';
-import { GostKursklausurManager } from '../../../../core/utils/gost/klausurplanung/GostKursklausurManager';
 import { ArrayList } from '../../../../java/util/ArrayList';
-import type { List } from '../../../../java/util/List';
+import { DeveloperNotificationException } from '../../../../core/exceptions/DeveloperNotificationException';
 import { Logger, cast_de_svws_nrw_core_logger_Logger } from '../../../../core/logger/Logger';
 import { GostSchuelerklausurTermin } from '../../../../core/data/gost/klausurplanung/GostSchuelerklausurTermin';
 import { GostSchuelerklausur } from '../../../../core/data/gost/klausurplanung/GostSchuelerklausur';
+import { Random } from '../../../../java/util/Random';
+import { KursblockungMatrix } from '../../../../core/kursblockung/KursblockungMatrix';
+import { GostKursklausurManager } from '../../../../core/utils/gost/klausurplanung/GostKursklausurManager';
+import type { List } from '../../../../java/util/List';
+import { ListUtils } from '../../../../core/utils/ListUtils';
 import { GostKlausurtermin } from '../../../../core/data/gost/klausurplanung/GostKlausurtermin';
 import { Pair } from '../../../../core/adt/Pair';
 
@@ -17,6 +20,8 @@ export class KlausurblockungNachschreiberAlgorithmus extends JavaObject {
 	 * Ein Logger für Debug-Zwecke.
 	 */
 	private readonly _logger : Logger;
+
+	private _regel_nachschreiber_der_selben_klausur_auf_selbe_termine_verteilen : boolean = false;
 
 
 	/**
@@ -45,23 +50,88 @@ export class KlausurblockungNachschreiberAlgorithmus extends JavaObject {
 	}
 
 	/**
-	 * @param pNachschreiber        Die Eingabe beinhaltet alle Schülerklausurtermine, die auf die Klausurtermine in der Liste pTermine verteilt werden müssen.
-	 * @param pTermine				Die Liste der GostKlausurtermine, auf die die Nachschreiber verteilt werden sollen
-	 * @param pManager				Der Kursklausur-Manager
-	 * @param pMaxTimeMillis  Logger für Benutzerhinweise, Warnungen und Fehler.
+	 * Aktiviert/Deaktiviert die Regel. Falls TRUE, werden NachschreiberInnen der selben Klausur auf den selben Termin geblockt.
+	 *
+	 * @param b falls TRUE, wird die Regel angewandt.
+	 */
+	public set_regel_nachschreiber_der_selben_klausur_auf_selbe_termine_verteilen(b : boolean) : void {
+		this._regel_nachschreiber_der_selben_klausur_auf_selbe_termine_verteilen = b;
+	}
+
+	/**
+	 * @param nachschreiber   Alle "GostSchuelerklausurTermin", die auf die "GostKlausurtermin" verteilt werden sollen.
+	 * @param termine         Alle "GostKlausurtermin", auf die potentiell Nachschreiber verteilt werden.
+	 * @param klausurManager  Der Kursklausur-Manager.
+	 * @param maxTimeMillis   Die maximal erlaubte Berechnungszeit (in Millisekunden).
+	 *
 	 * @return Eine Liste von Paaren: 1. Element = GostSchuelerklausurtermin (Nachschreiber), 2. Element = ID des Termins / der Schiene
 	 */
-	public berechne(pNachschreiber : List<GostSchuelerklausurTermin>, pTermine : List<GostKlausurtermin>, pManager : GostKursklausurManager, pMaxTimeMillis : number) : List<Pair<GostSchuelerklausurTermin, number>> {
-		for (let skt of pNachschreiber) {
-			let sk : GostSchuelerklausur | null = pManager.schuelerklausurBySchuelerklausurtermin(skt);
-			let idSchueler : number = sk.idSchueler;
-			let idKurs : number = sk.idKursklausur;
-			let schiene1 : GostKlausurtermin | null = pTermine.getFirst();
-			if (schiene1 !== null) {
-				let schuelerklausuren : List<GostSchuelerklausur | null> | null = pManager.schuelerklausurGetMengeByTerminid(schiene1.id);
+	public berechne(nachschreiber : List<GostSchuelerklausurTermin>, termine : List<GostKlausurtermin>, klausurManager : GostKursklausurManager, maxTimeMillis : number) : List<Pair<GostSchuelerklausurTermin, number>> {
+		const nachschreiberGruppen : List<List<GostSchuelerklausurTermin>> = new ArrayList();
+		for (const skt of nachschreiber) {
+			const sk : GostSchuelerklausur | null = klausurManager.schuelerklausurBySchuelerklausurtermin(skt);
+			const idSchueler : number = sk.idSchueler;
+			const idKurs : number = sk.idKursklausur;
+			DeveloperNotificationException.ifTrue("Ungültige Schüler-ID = " + idSchueler, idSchueler < 0);
+			DeveloperNotificationException.ifTrue("Ungültige Kurs-ID = " + idKurs, idKurs < 0);
+			let added : boolean = false;
+			for (const gruppe of nachschreiberGruppen)
+				if (this.istHinzufuegenErlaubt(gruppe, skt, klausurManager)) {
+					gruppe.add(skt);
+					added = true;
+					break;
+				}
+			if (!added)
+				nachschreiberGruppen.add(ListUtils.create1(skt));
+		}
+		const rows : number = nachschreiberGruppen.size();
+		const cols : number = termine.size();
+		const matrix : KursblockungMatrix = new KursblockungMatrix(KlausurblockungNachschreiberAlgorithmus._random, rows, cols);
+		const data : Array<Array<number>> = matrix.getMatrix();
+		for (let r : number = 0; r < nachschreiberGruppen.size(); r++)
+			for (let c : number = 0; c < termine.size(); c++)
+				data[r][c] = KlausurblockungNachschreiberAlgorithmus.gibBewertung(nachschreiberGruppen.get(r), termine.get(c), klausurManager);
+		const r2c : Array<number> | null = matrix.gibMaximalesBipartitesMatching(true);
+		const ergebnis : List<Pair<GostSchuelerklausurTermin, number>> = new ArrayList();
+		let dummyTerminID : number = -1;
+		for (let r : number = 0; r < nachschreiberGruppen.size(); r++) {
+			let idTermin : number = dummyTerminID;
+			const c : number = r2c[r];
+			if (c >= 0) {
+				idTermin = termine.get(c).id;
+			} else {
+				dummyTerminID--;
+			}
+			for (const nachschreiberDerGruppe of nachschreiberGruppen.get(r))
+				ergebnis.add(new Pair(nachschreiberDerGruppe, idTermin));
+		}
+		return ergebnis;
+	}
+
+	private static gibBewertung(nachschreiberGruppe : List<GostSchuelerklausurTermin>, termin : GostKlausurtermin, klausurManager : GostKursklausurManager) : number {
+		for (const sk1 of klausurManager.schuelerklausurGetMengeByTerminid(termin.id)) {
+			for (const skt of nachschreiberGruppe) {
+				const sk2 : GostSchuelerklausur | null = klausurManager.schuelerklausurBySchuelerklausurtermin(skt);
+				if (sk1.idSchueler === sk2.idSchueler)
+					return 0;
 			}
 		}
-		return new ArrayList();
+		return 1;
+	}
+
+	private istHinzufuegenErlaubt(gruppe : List<GostSchuelerklausurTermin>, skt1 : GostSchuelerklausurTermin, klausurManager : GostKursklausurManager) : boolean {
+		const sk1 : GostSchuelerklausur = klausurManager.schuelerklausurBySchuelerklausurtermin(skt1);
+		for (const skt2 of gruppe) {
+			const sk2 : GostSchuelerklausur = klausurManager.schuelerklausurBySchuelerklausurtermin(skt2);
+			if (sk1.idSchueler === sk2.idSchueler)
+				return false;
+		}
+		if (this._regel_nachschreiber_der_selben_klausur_auf_selbe_termine_verteilen) {
+			const skt2 : GostSchuelerklausurTermin = ListUtils.getNonNullElementAtOrException(gruppe, 0);
+			const sk2 : GostSchuelerklausur = klausurManager.schuelerklausurBySchuelerklausurtermin(skt2);
+			return (sk1.idKursklausur === sk2.idKursklausur);
+		}
+		return false;
 	}
 
 	transpilerCanonicalName(): string {
