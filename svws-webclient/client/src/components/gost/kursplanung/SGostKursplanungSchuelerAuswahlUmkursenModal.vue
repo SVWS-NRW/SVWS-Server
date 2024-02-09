69c2b9418608bd322c146dc8643de4bf5a1e7b3f
ThomasBachran <thomas.bachran@gmx.de>
2024-02-09 20:15:18 +0100

    Kursplanung - Modal Kurs-Schüler-Zuordnung ein wenig gestaltet

--- svws-webclient/client/src/components/gost/kursplanung/SGostKursplanungSchuelerAuswahlUmkursenModal.vue
+++ svws-webclient/client/src/components/gost/kursplanung/SGostKursplanungSchuelerAuswahlUmkursenModal.vue
@@ -1,35 +1,36 @@
 <template>
 	<slot :open-modal="openModal" />
 	<svws-ui-modal :show="showModal" size="medium" class="hidden">
-		<template #modalTitle>Umkursen</template>
+		<template #modalTitle>Kurs-Schüler-Zuordnung</template>
 		<template #modalDescription>
-			<div class="flex gap-2">
-				<!-- Die Tabelle mit den Kursschülern	 -->
-				<div>
-					{{ kursname }}
+			<div class="flex gap-8">
+				<!-- Die Tabelle mit den Kursschülern -->
+				<div class="w-96">
+					im Kurs {{ kursname }}
 					<svws-ui-table :items="schuelerFilter().filtered.value" :columns="[{key: 'name', label: 'Name'}]" :no-data="schuelerFilter().filtered.value.length <= 0">
 						<template #cell(name)="{ rowData }">
 							<div @click="remove(rowData.id)" class="cursor-pointer">{{ rowData.nachname }}, {{ rowData.vorname }}</div>
 						</template>
 					</svws-ui-table>
 				</div>
-				<!-- Die Tabelle mit den Schülern gleicher Fachwahl, aber nicht in diesem Kurs	 -->
-				<div>
-					{{ fachname }}
-					<svws-ui-table :items="fachwahlschueler" :columns="[{key: 'name', label: 'Name'}, {key: 'kurs', label: 'Kurs'}]" :no-data="fachwahlschueler.length <= 0">
+				<!-- Die Tabelle mit den Schülern gleicher Fachwahl, aber nicht in diesem Kurs -->
+				<div class="w-128">
+					mit Fachwahl {{ fachname }} {{ kursart }}
+					<svws-ui-table :items="fachwahlschueler" :columns="[{key: 'name', label: 'Name'}, {key: 'kurs', label: 'andere Kurszuordnung'}]" :no-data="fachwahlschueler.length <= 0">
 						<template #cell(name)="{ rowData }">
 							<div @click="move(rowData.id)" class="cursor-pointer">{{ rowData.nachname }}, {{ rowData.vorname }}</div>
 						</template>
 						<template #cell(kurs)="{ rowData }">
-							<div @click="move(rowData.id)" class="cursor-pointer">{{ kurswahl(rowData.id).value }}</div>
+							<svws-ui-select title="Anderer Kurs" :items="kurse" :item-text="getKursBezeichnung"
+								:model-value="getKurs(rowData)" @update:model-value="(value : GostBlockungsergebnisKurs | undefined) => updateZuordnung(rowData, value)"
+								class="w-full" headless removable />
 						</template>
 					</svws-ui-table>
 				</div>
 			</div>
 		</template>
 		<template #modalActions>
-			<svws-ui-button type="secondary" @click="showModal().value = false">Abbrechen</svws-ui-button>
-			<svws-ui-button type="primary" @click="true">Ja</svws-ui-button>
+			<svws-ui-button type="secondary" @click="showModal().value = false">Schließen</svws-ui-button>
 		</template>
 	</svws-ui-modal>
 </template>
@@ -38,8 +39,8 @@
 
 	import { computed, ref } from 'vue';
 	import type { GostKursplanungSchuelerFilter } from './GostKursplanungSchuelerFilter';
-	import type { GostBlockungsergebnisManager, Schueler } from '@core';
-	import { GostBlockungsergebnisKursSchuelerZuordnung } from '@core';
+	import type { GostBlockungsergebnisKurs, GostBlockungsergebnisManager, Schueler, List } from '@core';
+	import { ArrayList, GostBlockungsergebnisKursSchuelerZuordnung, GostKursart } from '@core';
 
 	const props = defineProps<{
 		updateKursSchuelerZuordnung: (idSchueler: number, idKursNeu: number, idKursAlt: number | undefined) => Promise<boolean>;
@@ -54,17 +55,33 @@
 	const kursname = computed(() => {
 		const kurs = props.schuelerFilter().kurs;
 		if (kurs === undefined)
-			return '??';
+			return '—';
 		return props.getErgebnismanager().getOfKursName(kurs.id);
-	})
+	});
+
+	const kursart = computed(() => {
+		const kurs = props.schuelerFilter().kurs;
+		if (kurs === undefined)
+			return '—';
+		const kursart = GostKursart.fromIDorNull(kurs.kursart);
+		return kursart?.kuerzel ?? "—";
+	});
+
+	const kurse = computed<List<GostBlockungsergebnisKurs>>(() => {
+		const kurs = props.schuelerFilter().kurs;
+		if (kurs === undefined)
+			return new ArrayList<GostBlockungsergebnisKurs>();
+		const fachart = GostKursart.getFachartID(kurs.fach_id, kurs.kursart);
+		return props.getErgebnismanager().getOfFachartKursmenge(fachart);
+	});
 
 	const fachname = computed(() => {
 		const kurs = props.schuelerFilter().kurs;
 		if (kurs === undefined)
-			return '??';
+			return '—';
 		const fach = props.getErgebnismanager().getFach(kurs.fach_id);
-		return fach.bezeichnung || '??';
-	})
+		return fach.bezeichnung || '—';
+	});
 
 	const fachwahlschueler = computed(() => {
 		const kurswahlschueler = props.schuelerFilter().filtered.value;
@@ -85,10 +102,10 @@
 	const kurswahl = (id: number) => computed(() => {
 		const kurs = props.schuelerFilter().kurs;
 		if (kurs === undefined)
-			return 'ohne Kurszuordnung';
+			return '———';
 		const kurswahl = props.getErgebnismanager().getOfSchuelerOfFachZugeordneterKurs(id, kurs.fach_id);
 		if (kurswahl === null)
-			return 'ohne Kurszuordnung';
+			return '———';
 		return props.getErgebnismanager().getOfKursName(kurswahl.id);
 	})
 
@@ -108,6 +125,35 @@
 			return;
 		const kurswahl = props.getErgebnismanager().getOfSchuelerOfFachZugeordneterKurs(id, kurs.fach_id);
 		await props.updateKursSchuelerZuordnung(id, kurs.id, kurswahl?.id);
+	}
+
+	async function updateZuordnung(schueler: Schueler, neuer_kurs: GostBlockungsergebnisKurs | undefined) {
+		const kurs = props.schuelerFilter().kurs;
+		if (kurs === undefined)
+			return;
+		const alter_kurs = props.getErgebnismanager().getOfSchuelerOfFachZugeordneterKurs(schueler.id, kurs.fach_id);
+		if ((neuer_kurs === undefined) && (alter_kurs !== null)) {
+			const zuordnung = new GostBlockungsergebnisKursSchuelerZuordnung();
+			zuordnung.idSchueler = schueler.id;
+			zuordnung.idKurs = alter_kurs.id;
+			await props.removeKursSchuelerZuordnung([ zuordnung ]);
+		} else if (neuer_kurs !== undefined) {
+			await props.updateKursSchuelerZuordnung(schueler.id, neuer_kurs.id, alter_kurs?.id ?? undefined);
+		}
+	}
+
+	function getKurs(schueler: Schueler) : GostBlockungsergebnisKurs | undefined {
+		const kurs = props.schuelerFilter().kurs;
+		if (kurs === undefined)
+			return;
+		const alter_kurs = props.getErgebnismanager().getOfSchuelerOfFachZugeordneterKurs(schueler.id, kurs.fach_id);
+		return alter_kurs ?? undefined;
+	}
+
+	function getKursBezeichnung(k : GostBlockungsergebnisKurs | undefined) : string {
+		if (k === undefined)
+			return '———';
+		return props.getErgebnismanager().getOfKursName(k.id);
 	}
 
 	const openModal = () => {
