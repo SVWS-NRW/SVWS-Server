98fef14b3a89f66c8ae2d6079dd9650a33374cad
hmt <dev@hmt.im>
2024-03-07 18:22:48 +0100

    Components: LogBox Log kopieren

--- svws-webclient/components/src/components/logbox/LogBox.vue
+++ svws-webclient/components/src/components/logbox/LogBox.vue
@@ -1,10 +1,15 @@
 <template>
 	<div v-if="logs != null" class="w-full overflow-x-auto" :class="hfull ? ['h-full','overflow-y-hidden'] : ['mt-4', 'h-84']">
 		<div :class="{ 'text-error': status === false, 'text-success': status === true }">
-			<span class="flex mb-4 text-headline-md">
+			<span class="flex mb-4 text-headline-md gap-4">
 				<i-ri-checkbox-circle-fill v-if="(status === true)" class="mr-1" />
 				<i-ri-alert-line v-else-if="(status === false)" class="mr-1" />
-				Log
+				<svws-ui-button v-if="log !== undefined" type="transparent" @click="copyToClipboard" class="pb-2">
+					<span>Log Kopieren</span>
+					<i-ri-clipboard-line v-if="copied === null" />
+					<i-ri-error-warning-fill v-else-if="copied === false" />
+					<i-ri-check-line v-else class="text-success" />
+				</svws-ui-button>
 			</span>
 		</div>
 		<div :class="hfull ? ['h-full', 'overflow-y-auto'] : []">
@@ -16,8 +21,8 @@
 
 <script setup lang="ts">
 
-	import { type List } from '@core';
-	import { computed } from "vue";
+	import { computed, ref } from "vue";
+	import type { List } from '@core';
 
 	const props = withDefaults(defineProps<{
 		logs?: List<string | null>;
@@ -29,6 +34,8 @@
 		hfull: false,
 	});
 
+	const copied = ref<boolean|null>(null);
+
 	const log = computed(()=> {
 		if (props.logs === undefined)
 			return;
@@ -39,4 +46,14 @@
 		return result;
 	});
 
+	async function copyToClipboard() {
+		if (log.value === undefined)
+			return;
+		try {
+			await navigator.clipboard.writeText("```\n"+log.value+"\n```");
+		} catch(e) {
+			copied.value = false;
+		}
+		copied.value = true;
+	}
 </script>
