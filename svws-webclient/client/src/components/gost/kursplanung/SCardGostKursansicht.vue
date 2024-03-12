826c9eb8fedb777ee8115b4d78e4b9ad7cda6224
ThomasBachran <thomas.bachran@gmx.de>
2024-03-12 12:34:48 +0100

    Client-Kursplanung: Echtes Ausblenden der Kursansicht

--- svws-webclient/client/src/components/gost/kursplanung/SCardGostKursansicht.vue
+++ svws-webclient/client/src/components/gost/kursplanung/SCardGostKursansicht.vue
@@ -1,5 +1,6 @@
 <template>
-	<svws-ui-content-card overflow-scroll :class="{'mr-16': blockungstabelleVisible}">
+	<svws-ui-content-card v-if="!blockungstabelleVisible" />
+	<svws-ui-content-card v-else overflow-scroll class="mr-16">
 		<svws-ui-table :items="GostKursart.values()" :columns="cols" disable-footer scroll has-background :style="!blockungstabelleVisible ? 'margin-left: 0; margin-right: 0; opacity: 0;' : ''">
 			<template #header>
 				<div role="row" class="svws-ui-tr select-none">
