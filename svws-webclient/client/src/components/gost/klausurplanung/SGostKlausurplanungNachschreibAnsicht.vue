248749bf384307abe9ea7cb0c816fb1f7d767ae3
Jonas Kunst <info@jonaskun.st>
2024-04-30 16:46:46 +0200

    UI Klausurplanung: Nachreib Ansicht styling

--- svws-webclient/client/src/components/gost/klausurplanung/SGostKlausurplanungNachschreibAnsicht.vue
+++ svws-webclient/client/src/components/gost/klausurplanung/SGostKlausurplanungNachschreibAnsicht.vue
@@ -2,40 +2,41 @@
 	<Teleport to=".router-tab-bar--subnav" v-if="isMounted">
 		<s-gost-klausurplanung-quartal-auswahl :quartalsauswahl="quartalsauswahl" :halbjahr="halbjahr" />
 	</Teleport>
-	<div class="page--content page--content--full relative flex-col">
-		<div class="text-headline">Nachschreibplan {{ jahrgangsdaten.jahrgang }}, {{ halbjahr.halbjahr }}. Halbjahr{{ quartalsauswahl.value === 0 ? "" : ", " + quartalsauswahl.value + ". Quartal" }}</div>
-		<svws-ui-table v-model:sort-by-and-order="sortByAndOrder" :columns="cols" :items="itemsSorted">
-			<template #noData>
-				<slot name="noData">
-					Keine Nachschreibklausuren geplant
-				</slot>
-			</template>
+	<div class="page--content relative flex-col">
+		<svws-ui-content-card class="col-span-full" :title="`Nachschreibplan ${jahrgangsdaten.jahrgang}, ${halbjahr.halbjahr}. Halbjahr${quartalsauswahl.value === 0 ? '' : ', ' + quartalsauswahl.value + '. Quartal'}`">
+			<svws-ui-table v-model:sort-by-and-order="sortByAndOrder" :columns="cols" :items="itemsSorted">
+				<template #noData>
+					<slot name="noData">
+						Keine Nachschreibklausuren geplant
+					</slot>
+				</template>
 
-			<template #cell(nachname)="{ rowData }">
-				{{ kMan().getSchuelerMap().get(props.kMan().schuelerklausurGetByIdOrException(rowData.idSchuelerklausur).idSchueler)?.nachname }}
-			</template>
-			<template #cell(vorname)="{ rowData }">
-				{{ kMan().getSchuelerMap().get(props.kMan().schuelerklausurGetByIdOrException(rowData.idSchuelerklausur).idSchueler)?.vorname }}
-			</template>
-			<template #cell(kurs)="{ rowData }">
-				<span class="svws-ui-badge" :style="`--background-color: ${ kMan().fachBgColorByKursklausur(kMan().kursklausurBySchuelerklausurTermin(rowData)) };`">{{ kMan().kursKurzbezeichnungByKursklausur(kMan().kursklausurBySchuelerklausurTermin(rowData)) }}</span>
-			</template>
-			<template #cell(kuerzel)="{ rowData }">
-				{{ kMan().kursLehrerKuerzelByKursklausur(kMan().kursklausurBySchuelerklausurTermin(rowData)) }}
-			</template>
-			<template #cell(datum)="{ rowData }">
-				{{ DateUtils.gibDatumGermanFormat(kMan().terminOrExceptionBySchuelerklausurTermin(rowData).datum!) }}
-			</template>
-			<template #cell(startzeit)="{ rowData }">
-				{{ DateUtils.getStringOfUhrzeitFromMinuten(rowData.startzeit !== null ? rowData.startzeit : kMan().terminOrExceptionBySchuelerklausurTermin(rowData).startzeit!) }}
-			</template>
-			<template #cell(dauer)="{ rowData }">
-				{{ kMan().vorgabeBySchuelerklausurTermin(rowData).dauer }}
-			</template>
+				<template #cell(nachname)="{ rowData }">
+					{{ kMan().getSchuelerMap().get(props.kMan().schuelerklausurGetByIdOrException(rowData.idSchuelerklausur).idSchueler)?.nachname }}
+				</template>
+				<template #cell(vorname)="{ rowData }">
+					{{ kMan().getSchuelerMap().get(props.kMan().schuelerklausurGetByIdOrException(rowData.idSchuelerklausur).idSchueler)?.vorname }}
+				</template>
+				<template #cell(kurs)="{ rowData }">
+					<span class="svws-ui-badge" :style="`--background-color: ${ kMan().fachBgColorByKursklausur(kMan().kursklausurBySchuelerklausurTermin(rowData)) };`">{{ kMan().kursKurzbezeichnungByKursklausur(kMan().kursklausurBySchuelerklausurTermin(rowData)) }}</span>
+				</template>
+				<template #cell(kuerzel)="{ rowData }">
+					{{ kMan().kursLehrerKuerzelByKursklausur(kMan().kursklausurBySchuelerklausurTermin(rowData)) }}
+				</template>
+				<template #cell(datum)="{ rowData }">
+					{{ DateUtils.gibDatumGermanFormat(kMan().terminOrExceptionBySchuelerklausurTermin(rowData).datum!) }}
+				</template>
+				<template #cell(startzeit)="{ rowData }">
+					{{ DateUtils.getStringOfUhrzeitFromMinuten(rowData.startzeit !== null ? rowData.startzeit : kMan().terminOrExceptionBySchuelerklausurTermin(rowData).startzeit!) }}
+				</template>
+				<template #cell(dauer)="{ rowData }">
+					{{ kMan().vorgabeBySchuelerklausurTermin(rowData).dauer }}
+				</template>
 			<!--<template #cell(raum)>
 				d
 			</template>-->
-		</svws-ui-table>
+			</svws-ui-table>
+		</svws-ui-content-card>
 	</div>
 </template>
 
@@ -84,7 +85,7 @@
 			{ key: "kuerzel", label: "Lehrkraft" },
 			{ key: "datum", label: "Datum" },
 			{ key: "startzeit", label: "Startzeit" },
-			{ key: "dauer", label: "Dauer", tooltip: "Dauer in Minuten", span: 0.5, align: "right", minWidth: 3.25 },
+			{ key: "dauer", label: "Dauer", tooltip: "Dauer in Minuten", span: 0.5, minWidth: 3.25 },
 			{ key: "raum", label: "Raum" },
 		];
 		return cols;
