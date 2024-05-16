4594309be4290e9d4b3b5654e019ab17355c5aed
hmt <dev@hmt.im>
2024-05-16 11:11:35 +0000

    Client/Lehrer: Ergänze Checkbox für Statistik und Sichtbarkeit
    fixes https://github.com/SVWS-NRW/SVWS-Server/issues/195

--- svws-webclient/client/src/components/lehrer/individualdaten/SCardLehrerBasisdaten.vue
+++ svws-webclient/client/src/components/lehrer/individualdaten/SCardLehrerBasisdaten.vue
@@ -2,13 +2,13 @@
 	<svws-ui-content-card title="Allgemein">
 		<svws-ui-input-wrapper :grid="2">
 			<svws-ui-input-wrapper>
-				<svws-ui-checkbox v-model="istSichtbar"> Ist sichtbar </svws-ui-checkbox>
-				<svws-ui-checkbox v-model="istStatistikrelevant"> Ist statistikrelevant </svws-ui-checkbox>
+				<svws-ui-checkbox :model-value="data.istSichtbar" @update:model-value="istSichtbar => patch({istSichtbar: istSichtbar === true})"> Ist sichtbar </svws-ui-checkbox>
+				<svws-ui-checkbox :model-value="data.istRelevantFuerStatistik" @update:model-value="istRelevantFuerStatistik => patch({istRelevantFuerStatistik: istRelevantFuerStatistik === true})"> Ist Relevant für Statistik </svws-ui-checkbox>
 			</svws-ui-input-wrapper>
-			<svws-ui-text-input placeholder="Kürzel" :model-value="data.kuerzel" @change="kuerzel => patch({kuerzel})" type="text" required />
+			<svws-ui-text-input placeholder="Kürzel" :model-value="data.kuerzel" @change="kuerzel => patch({kuerzel})" required />
 			<svws-ui-select title="Personal-Typ" v-model="inputPersonalTyp" :items="PersonalTyp.values()" :item-text="i => i.bezeichnung" required />
-			<svws-ui-text-input placeholder="Nachname" :model-value="data.nachname" @change="nachname => patch({nachname})" type="text" required />
-			<svws-ui-text-input placeholder="Rufname" :model-value="data.vorname" @change="vorname => patch({vorname})" type="text" required />
+			<svws-ui-text-input placeholder="Nachname" :model-value="data.nachname" @change="nachname => patch({nachname})" required />
+			<svws-ui-text-input placeholder="Rufname" :model-value="data.vorname" @change="vorname => patch({vorname})" required />
 			<svws-ui-spacing />
 			<svws-ui-select title="Geschlecht" v-model="inputGeschlecht" :items="Geschlecht.values()" :item-text="i=>i.text" required />
 			<svws-ui-text-input placeholder="Geburtsdatum" :model-value="data.geburtsdatum" @change="geburtsdatum => geburtsdatum && patch({geburtsdatum})" type="date" required />
@@ -17,16 +17,16 @@
 				:item-filter="staatsangehoerigkeitKatalogEintragFilter" required autocomplete />
 			<svws-ui-spacing />
 			<svws-ui-text-input placeholder="Akadademischer Grad" :model-value="data.titel" @change="titel => patch({titel})" type="text" />
-			<svws-ui-text-input placeholder="Amtsbezeichnung" :model-value="data.amtsbezeichnung" @change="amtsbezeichnung => patch({amtsbezeichnung})" type="text" />
+			<svws-ui-text-input placeholder="Amtsbezeichnung" :model-value="data.amtsbezeichnung" @change="amtsbezeichnung => patch({amtsbezeichnung})" />
 		</svws-ui-input-wrapper>
 	</svws-ui-content-card>
 </template>
 
 <script setup lang="ts">
 
-	import type { LehrerListeManager, LehrerStammdaten } from "@core";
 	import { computed } from "vue";
-	import { DeveloperNotificationException, Geschlecht, Nationalitaeten, PersonalTyp } from "@core";
+	import type { LehrerListeManager, LehrerStammdaten } from "@core";
+	import { Geschlecht, Nationalitaeten, PersonalTyp } from "@core";
 	import { staatsangehoerigkeitKatalogEintragFilter, staatsangehoerigkeitKatalogEintragSort } from "~/utils/helfer";
 
 	const props = defineProps<{
@@ -50,16 +50,5 @@
 		get: () => Nationalitaeten.getByISO3(data.value.staatsangehoerigkeitID) || Nationalitaeten.DEU,
 		set: (value) => void props.patch({ staatsangehoerigkeitID: value.daten.iso3 })
 	});
-
-	const istSichtbar = computed<boolean>({
-		get: () => true,
-		set: (value) => {throw new DeveloperNotificationException("Sichtbarkeit ist noch nicht implementiert")}//doPatch({ istSichtbar: value })
-	});
-
-	const istStatistikrelevant = computed<boolean>({
-		get: () => true,
-		set: (value) => {throw new DeveloperNotificationException("Sichtbarkeit ist noch nicht implementiert")}//doPatch({ istStatistikrelevant: value })
-	});
-
 
 </script>
