55e5f5f3a6f9453e66e9e55f8f77db63de3c4aa4
ThomasBachran <thomas.bachran@gmx.de>
2024-05-16 14:01:55 +0200

    Schuldatei: Einlesen der Daten aus der lokalen Version der Schuldatei (Checkstyle)

--- svws-core/src/main/java/de/svws_nrw/schuldatei/v1/SchuldateiReader.java
+++ svws-core/src/main/java/de/svws_nrw/schuldatei/v1/SchuldateiReader.java
@@ -1,9 +1,7 @@
 package de.svws_nrw.schuldatei.v1;
 
 import java.io.IOException;
-import java.util.List;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.svws_nrw.base.ResourceUtils;
