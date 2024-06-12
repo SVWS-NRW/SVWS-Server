f1ee9b0fbf627599c64ab56bfa0b529fef45fc55
ThomasBachran <thomas.bachran@gmx.de>
2024-06-12 14:41:07 +0200

    Client: Anzeige der Leitungsfunktionen von Lehrern bei den Individualdaten

--- svws-webclient/client/src/components/lehrer/individualdaten/SLehrerIndividualdaten.vue
+++ svws-webclient/client/src/components/lehrer/individualdaten/SLehrerIndividualdaten.vue
@@ -2,6 +2,7 @@
 	<div class="page--content">
 		<s-card-lehrer-basisdaten :lehrer-liste-manager="lehrerListeManager" :patch="patch" />
 		<s-card-lehrer-kontaktdaten :lehrer-liste-manager="lehrerListeManager" :patch="patch" :map-orte="mapOrte" :map-ortsteile="mapOrtsteile" />
+		<s-lehrer-individualdaten-leitungsfunktionen :lehrer-liste-manager="lehrerListeManager" />
 	</div>
 </template>
 
---
+++ svws-webclient/client/src/components/lehrer/individualdaten/SLehrerIndividualdatenLeitungsfunktionen.vue
@@ -0,0 +1,31 @@
+<template>
+	<svws-ui-content-card title="Leitungsfunktionen">
+		<svws-ui-table :columns="cols" :items="lehrerListeManager().daten().leitungsfunktionen" count>
+			<template #cell(funktion)="{ rowData }">
+				{{ LehrerLeitungsfunktion.getByID(rowData.idLeitungsfunktion)?.daten.bezeichnung }}
+			</template>
+			<template #cell(von)="{ rowData }">
+				<svws-ui-text-input placeholder="Geburtsdatum" :model-value="rowData.beginn" @change="datum => datum && patch({ beginn: datum })" type="date" headless />
+			</template>
+			<template #cell(bis)="{ rowData }">
+				<svws-ui-text-input placeholder="Geburtsdatum" :model-value="rowData.ende" @change="datum => datum && patch({ ende: datum })" type="date" headless />
+			</template>
+		</svws-ui-table>
+	</svws-ui-content-card>
+</template>
+
+<script setup lang="ts">
+
+	import { LehrerLeitungsfunktion, type LehrerListeManager } from "@core";
+
+	const props = defineProps<{
+		lehrerListeManager: () => LehrerListeManager;
+	}>();
+
+	const cols = [
+		{key: 'funktion', label: 'Funktion', span: 2 },
+		{key: 'von', label: 'Von', span: 1 },
+		{key: 'bis', label: 'Bis', span: 1 }
+	]
+
+</script>
