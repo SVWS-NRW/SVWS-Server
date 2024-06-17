916ab4865c7a40962fa8edc9bedb02beb6c64778
hmt <dev@hmt.im>
2024-06-17 06:12:36 +0000

    node: Update vue et al und Typo fixes #1693

--- svws-webclient/admin/src/components/schemaneu/SSchemaNeuMigrate.vue
+++ svws-webclient/admin/src/components/schemaneu/SSchemaNeuMigrate.vue
@@ -1,5 +1,5 @@
 <template>
-	<svws-ui-action-button title="Schild2-Datenbank migrieren" description="Eine Schild2-Datenbank wird in ein neues Schema migrier." icon="i-ri-database-2-line" action-label="Migrieren" :action-function :action-disabled="(zielSchema.length === 0) || (zielUsername.length === 0) || (zielUserPassword.length === 0) || (statusFunction().value != undefined) || (zielUsername === 'root')" :is-loading="loadingFunction().value" :is-active>
+	<svws-ui-action-button title="Schild2-Datenbank migrieren" description="Eine Schild2-Datenbank wird in ein neues Schema migriert." icon="i-ri-database-2-line" action-label="Migrieren" :action-function :action-disabled="(zielSchema.length === 0) || (zielUsername.length === 0) || (zielUserPassword.length === 0) || (statusFunction().value != undefined) || (zielUsername === 'root')" :is-loading="loadingFunction().value" :is-active>
 		<div class="input-wrapper">
 			<svws-ui-select v-model="migrationQuellinformationen().dbms" :items="items.keys()" :item-text="i => items.get(i) || ''" title="Datenbank-Typ" />
 			<svws-ui-spacing />
@@ -7,7 +7,7 @@
 				<div class="font-bold text-button">Quell-Datenbank:</div>
 				<svws-ui-text-input v-model.trim="migrationQuellinformationen().location" placeholder="Datenbank-Host (hostname:port) oder (ip:port)" />
 				<svws-ui-text-input v-model.trim="migrationQuellinformationen().schema" placeholder="Datenbank-Schema (z.B. schild_nrw)" />
-				<svws-ui-text-input v-model.trim="migrationQuellinformationen().user" placeholder="Name des Datenbankbenutzers" :valid="validateUsername" />
+				<svws-ui-text-input v-model.trim="migrationQuellinformationen().user" placeholder="Name des Datenbankbenutzers" :valid="validatorUsername" />
 				<svws-ui-text-input v-model.trim="migrationQuellinformationen().password" placeholder="Passwort des Datenbankbenutzers" type="password" />
 				<svws-ui-spacing />
 				<svws-ui-checkbox v-model="migrationQuellinformationen().schildzentral">Migration aus einer Schild-2-Zentral-Instanz</svws-ui-checkbox>
