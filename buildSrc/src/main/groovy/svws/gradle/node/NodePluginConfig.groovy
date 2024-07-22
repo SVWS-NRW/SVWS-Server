4eecbc615a49056f8e1f7d5a4fbcfcb1a7f9a5a4
hmt <dev@hmt.im>
2024-07-22 09:27:53 +0000

    update node 20.15.1 und deps

--- buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
+++ buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
@@ -31,10 +31,10 @@ abstract class NodePluginConfig {
     	this.project = p;
     	this.rootProject = p.rootProject;
     	url.convention('https://nodejs.org/dist/v');
-    	version.convention('20.15.0'); // https://nodejs.org/en/download/prebuilt-installer
+    	version.convention('20.15.1'); // https://nodejs.org/en/download/prebuilt-installer
     	npmVersion.convention('10.7.0');
     	tsVersion.convention('5.5.3'); // https://github.com/microsoft/TypeScript/releases
-    	tsNodeTypesVersion.convention('20.14.9'); // https://www.npmjs.com/package/@types/node
+    	tsNodeTypesVersion.convention('20.14.11'); // https://www.npmjs.com/package/@types/node
     }
 
     boolean isLinux() {
