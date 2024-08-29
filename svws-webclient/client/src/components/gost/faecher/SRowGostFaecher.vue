b52d4b3744013f3a0ebef0ff4d1fbe7b354d3732
dschuhmann <dominik.schuhmann@init.de>
2024-08-29 17:55:27 +0200

    #823 Stundenzahl Oberstufenfächer fokussierbar und auswählbar

--- svws-webclient/client/src/components/gost/faecher/SRowGostFaecher.vue
+++ svws-webclient/client/src/components/gost/faecher/SRowGostFaecher.vue
@@ -13,7 +13,7 @@
 			<input :disabled="!hatUpdateKompetenz || true" type="checkbox" class="svws-ui-checkbox svws-headless" v-model="fach.istFremdSpracheNeuEinsetzend">
 		</div>
 		<div role="cell" class="svws-ui-td svws-align-center svws-divider svws-no-padding" :class="{ 'cursor-pointer': istProjektkurs && hatUpdateKompetenz }" @click="set_pjk_stunden">
-			<div v-if="istProjektkurs && hatUpdateKompetenz" class="flex items-center gap-0.5 border border-black/25 border-dashed hover:border-black/50 hover:border-solid hover:bg-white my-auto p-[0.1rem] rounded">
+			<div v-if="istProjektkurs && hatUpdateKompetenz" class="flex items-center gap-0.5 border border-black/25 border-dashed hover:border-black/50 hover:border-solid hover:bg-white my-auto p-[0.1rem] rounded" @keydown.enter="set_pjk_stunden" tabindex="0">
 				<span :class="{ 'opacity-100 font-bold': fach.wochenstundenQualifikationsphase === 2, 'opacity-25 hover:opacity-100 font-medium': fach.wochenstundenQualifikationsphase === 3}">2</span>
 				<span class="opacity-50">/</span>
 				<span :class="{ 'opacity-100 font-bold': fach.wochenstundenQualifikationsphase === 3, 'opacity-25 hover:opacity-100 font-medium': fach.wochenstundenQualifikationsphase === 2}">3</span>
