49e6c420756d72ec48100ec756583167792321d8
hmt <dev@hmt.im>
2024-09-24 09:33:16 +0200

    node: update deps

--- buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
+++ buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
@@ -34,7 +34,7 @@ abstract class NodePluginConfig {
     	version.convention('20.17.0'); // https://nodejs.org/en/download/prebuilt-installer
     	npmVersion.convention('10.8.2');
     	tsVersion.convention('5.6.2'); // https://github.com/microsoft/TypeScript/releases
-    	tsNodeTypesVersion.convention('20.16.5'); // https://www.npmjs.com/package/@types/node
+    	tsNodeTypesVersion.convention('20.16.6'); // https://www.npmjs.com/package/@types/node
     }
 
     boolean isLinux() {
