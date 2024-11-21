be6f4ca1ddb8923349d9c95a63161ce54a9a9dc7
ThomasBachran <thomas.bachran@gmx.de>
2024-11-21 17:50:28 +0100

    fixed sonar lint warning

--- svws-module-dav-api/src/test/java/de/svws_nrw/davapi/api/VCalendarParseTest.java
+++ svws-module-dav-api/src/test/java/de/svws_nrw/davapi/api/VCalendarParseTest.java
@@ -15,22 +15,44 @@ import de.svws_nrw.davapi.util.icalendar.VCalendarTyp;
  * Testklasse für {@link VCalendar} (VCalendar-Parser)
  *
  */
-public class VCalendarParseTest {
+class VCalendarParseTest {
 
 	/** ein beispielhaftes .ics file */
-	private static final String examplePayload = "BEGIN:VCALENDAR\r\n"
-			+ "PRODID:-//Mozilla.org/NONSGML Mozilla Calendar V1.1//EN\r\n" + "VERSION:2.0\r\n" + "BEGIN:VTIMEZONE\r\n"
-			+ "TZID:Europe/Berlin\r\n" + "BEGIN:DAYLIGHT\r\n" + "TZOFFSETFROM:+0100\r\n" + "TZOFFSETTO:+0200\r\n"
-			+ "TZNAME:CEST\r\n" + "DTSTART:19700329T020000\r\n" + "RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=3\r\n"
-			+ "END:DAYLIGHT\r\n" + "BEGIN:STANDARD\r\n" + "TZOFFSETFROM:+0200\r\n" + "TZOFFSETTO:+0100\r\n"
-			+ "TZNAME:CET\r\n" + "DTSTART:19701025T030000\r\n" + "RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=10\r\n"
-			+ "END:STANDARD\r\n" + "END:VTIMEZONE\r\n" + "BEGIN:VEVENT\r\n" + "CREATED:20221020T213623Z\r\n"
-			+ "LAST-MODIFIED:20221020T213634Z\r\n" + "DTSTAMP:20221020T213634Z\r\n"
-			+ "UID:ce5935b0-f96c-8346-922c-ac6930faef32\r\n" + "SUMMARY:test\r\n"
-			+ "DTSTART;TZID=Europe/Berlin:20221021T230000\r\n" + "DTEND;TZID=Europe/Berlin:20221022T000000\r\n"
-			+ "TRANSP:OPAQUE\r\n" + "LOCATION:testo\r\n"
-			+ "DESCRIPTION;ALTREP=\"data:text/html,%3 Cbody%3Et esd%3 C%2F body%3E\":tesd\r\n" + "END:VEVENT\r\n"
-			+ "END:VCALENDAR\r\n";
+	private static final String examplePayload = """
+		BEGIN:VCALENDAR\r
+		PRODID:-//Mozilla.org/NONSGML Mozilla Calendar V1.1//EN\r
+		VERSION:2.0\r
+		BEGIN:VTIMEZONE\r
+		TZID:Europe/Berlin\r
+		BEGIN:DAYLIGHT\r
+		TZOFFSETFROM:+0100\r
+		TZOFFSETTO:+0200\r
+		TZNAME:CEST\r
+		DTSTART:19700329T020000\r
+		RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=3\r
+		END:DAYLIGHT\r
+		BEGIN:STANDARD\r
+		TZOFFSETFROM:+0200\r
+		TZOFFSETTO:+0100\r
+		TZNAME:CET\r
+		DTSTART:19701025T030000\r
+		RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=10\r
+		END:STANDARD\r
+		END:VTIMEZONE\r
+		BEGIN:VEVENT\r
+		CREATED:20221020T213623Z\r
+		LAST-MODIFIED:20221020T213634Z\r
+		DTSTAMP:20221020T213634Z\r
+		UID:ce5935b0-f96c-8346-922c-ac6930faef32\r
+		SUMMARY:test\r
+		DTSTART;TZID=Europe/Berlin:20221021T230000\r
+		DTEND;TZID=Europe/Berlin:20221022T000000\r
+		TRANSP:OPAQUE\r
+		LOCATION:testo\r
+		DESCRIPTION;ALTREP=\"data:text/html,%3 Cbody%3Et esd%3 C%2F body%3E\":tesd\r
+		END:VEVENT\r
+		END:VCALENDAR\r
+		""";
 
 	/**
 	 * Testet das Parsen eines VCalendars mit {@link VCalendar#parse(String)}
