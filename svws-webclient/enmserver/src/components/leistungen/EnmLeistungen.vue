62303d6fbaf3fadbf4f1ac096beefdb51ee5301f
ThomasBachran <thomas.bachran@gmx.de>
2025-01-07 09:36:23 +0100

    Darstellungsbereich für den Floskel-Editor

--- svws-webclient/enmserver/src/components/leistungen/EnmLeistungen.vue
+++ svws-webclient/enmserver/src/components/leistungen/EnmLeistungen.vue
@@ -1,11 +1,11 @@
 <template>
-	<div class="page--content h-full w-full overflow-hidden">
-		<div class="w-full h-full overflow-hidden">
+	<div class="page--content">
+		<div class="enm-leistung-uebersicht-bereich">
 			<enm-leistungen-uebersicht :manager :patch-leistung />
 		</div>
-		<!-- <div class="w-full h-full overflow-hidden">
+		<div class="enm-floskel-editor-bereich">
 			<enm-floskeleditor />
-		</div> -->
+		</div>
 	</div>
 </template>
 
@@ -20,7 +20,20 @@
 <style lang="postcss" scoped>
 
 	.page--content {
-		@apply inline-block;
+		@apply flex flex-row gap-2;
+		@apply h-full w-full overflow-hidden;
+		@apply max-w-full;
+	}
+
+	.enm-leistung-uebersicht-bereich {
+		@apply w-full h-full overflow-hidden;
+		@apply flex-grow;
+		@apply max-w-[100rem];
+	}
+
+	.enm-floskel-editor-bereich {
+		@apply w-full h-full overflow-hidden;
+		@apply max-w-196;
 	}
 
 </style>
