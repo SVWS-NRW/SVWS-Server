3a24ad518beeadb0adec3de81c80f5407090330f
ThomasBachran <thomas.bachran@gmx.de>
2025-02-10 18:46:35 +0100

    ENM-Client: Mögliche Umsetzung draggable divider

--- svws-webclient/enmserver/src/components/leistungen/EnmKlassenleitung.vue
+++ svws-webclient/enmserver/src/components/leistungen/EnmKlassenleitung.vue
@@ -1,17 +1,23 @@
 <template>
 	<div class="page page-flex-row">
-		<div class="grow w-full h-full overflow-hidden">
-			<enm-klassenleitung-uebersicht :manager :patch-bemerkungen :patch-lernabschnitt :columns-visible :set-columns-visible @hauptgruppe="erlaubteHauptgruppe = $event" />
-		</div>
-		<div v-if="floskelEditorVisible" class="min-w-6 max-w-196 h-full overflow-hidden">
-			<enm-floskeleditor :manager :patch="doPatchBemerkungen" :erlaubte-hauptgruppe />
+		<div class="flex flex-row h-full w-full overflow-hidden" @dragover="dragOver">
+			<div class="grow w-full h-full overflow-hidden">
+				<enm-klassenleitung-uebersicht :manager :patch-bemerkungen :patch-lernabschnitt :columns-visible :set-columns-visible @hauptgruppe="erlaubteHauptgruppe = $event" />
+			</div>
+			<div class="h-full content-center text-center cursor-w-resize min-w-8 max-w-8 lg:min-w-12 lg:max-w-12 bg-ui" draggable="true">
+				<span class="icon i-ri-arrow-left-s-line" />
+				<span class="icon i-ri-arrow-right-s-line" />
+			</div>
+			<div v-if="floskelEditorVisible" class="h-full overflow-hidden" :style="{ 'min-width': posDivider + 'rem', 'max-width': posDivider + 'rem' }">
+				<enm-floskeleditor :manager :patch="doPatchBemerkungen" :erlaubte-hauptgruppe />
+			</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 
-	import { computed, ref, watchEffect } from 'vue';
+	import { computed, onMounted, ref, watchEffect } from 'vue';
 	import type { BemerkungenHauptgruppe } from './EnmManager';
 	import type { EnmKlassenleitungProps } from './EnmKlassenleitungProps';
 
@@ -51,6 +57,27 @@
 		}
 		await props.patchBemerkungen(schueler.id, schueler.bemerkungen);
 		props.manager.update();
+	}
+
+	// Default-Breite von 49 rem für den Floskel-Editor
+	const posDivider = ref<number>(45);
+
+	function dragOver(event: DragEvent) {
+		// Bestimme die Anzahl der Pixel pro 1 rem
+		const pxPerRem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+		// Bestimme den Abstand vom rechten Rand des Content-Bereich in Pixeln
+		const posFromRight = (event.currentTarget as Element).getBoundingClientRect().right - event.pageX;
+		// Bestimme die neue Breite des Floskel-Editors in rem
+		let width = posFromRight / pxPerRem;
+		// Erlaube dabei eine maximale Breits von 60 rem
+		if (width > 60)
+			width = 60;
+		// Kleinere Werte als 10 rem blenden den Floskel-Editor komplett aus
+		if (width < 10)
+			width = 0;
+		// Setze die Position des Dividers nur neu, wenn er stark genug von der alten Position abweicht (mehr als 1 rem) -> Performance
+		if (Math.abs(width - posDivider.value) > 1)
+			posDivider.value = width;
 	}
 
 </script>
