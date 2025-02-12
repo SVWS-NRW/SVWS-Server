676341515571e2b31406781597ad00a8c846c79c
ThomasBachran <thomas.bachran@gmx.de>
2025-02-12 18:12:19 +0100

    Client: Scrolling bei den Benutzereinstellungen korrigiert

--- svws-webclient/client/src/components/einstellungen/benutzer/daten/SBenutzer.vue
+++ svws-webclient/client/src/components/einstellungen/benutzer/daten/SBenutzer.vue
@@ -1,8 +1,9 @@
 <template>
-	<div class="page page-grid-cards">
-		<div class="flex flex-col gap-12 lg:gap-16">
-			<svws-ui-content-card :title="`Benutzertyp ${BenutzerTyp.getByID(getBenutzerManager().daten().typ)?.bezeichnung}`">
-				<svws-ui-input-wrapper :grid="2">
+	<div class="page page-flex-row">
+		<div class="h-full flex flex-col gap-4">
+			<div class="flex flex-col gap-8">
+				<div class="text-headline-md">{{ `Benutzertyp ${BenutzerTyp.getByID(getBenutzerManager().daten().typ)?.bezeichnung}` }}</div>
+				<div class="grid grid-cols-2 gap-2">
 					<svws-ui-text-input class="contentFocusField" :model-value="getBenutzerManager().getAnmeldename()" @change="setAnmeldename" placeholder="Benutzername" />
 					<svws-ui-text-input :model-value="getBenutzerManager().getAnzeigename()" @change="setAnzeigename" placeholder="Anzeigename" :readonly="getBenutzerManager().daten().typ !== BenutzerTyp.ALLGEMEIN.id" />
 					<svws-ui-text-input v-model.trim="kennwort1" type="password" placeholder="Neues Passwort" />
@@ -10,59 +11,56 @@
 					<div>
 						<svws-ui-button :disabled="!kennwort1 || !kennwort2" @click="setPwd()"> Passwort speichern </svws-ui-button>
 					</div>
-				</svws-ui-input-wrapper>
-			</svws-ui-content-card>
-			<svws-ui-content-card>
-				<svws-ui-table selectable :items="listBenutzergruppen" :disable-footer="true" :columns>
-					<template #header>
-						<div role="row" class="svws-ui-tr gruppen-tr">
-							<!--<div class="svws-ui-td svws-align-center" role="columnheader" aria-label="Alle auswählen">
-						<svws-ui-checkbox :model-value="selected" />
-					</div>-->
-							<div class="svws-ui-td col-span-2" role="columnheader">
-								Zugewiesene Benutzergruppen
+				</div>
+			</div>
+			<svws-ui-table scroll selectable :items="listBenutzergruppen" :disable-footer="true" :columns>
+				<template #header>
+					<div role="row" class="svws-ui-tr gruppen-tr">
+						<!--<div class="svws-ui-td svws-align-center" role="columnheader" aria-label="Alle auswählen">
+					<svws-ui-checkbox :model-value="selected" />
+				</div>-->
+						<div class="svws-ui-td col-span-2" role="columnheader">
+							Zugewiesene Benutzergruppen
+						</div>
+					</div>
+				</template>
+				<template #body>
+					<div role="row" class="svws-ui-tr gruppen-tr" v-for="row in listBenutzergruppen" :key="row.id">
+						<div class="svws-ui-td svws-align-center" role="cell">
+							<svws-ui-checkbox type="toggle" :model-value="getBenutzerManager().istInGruppe(row.id)" @update:model-value="val => val ? addBenutzerToBenutzergruppe(row.id) : removeBenutzerFromBenutzergruppe(row.id)" />
+						</div>
+						<div class="svws-ui-td" role="cell">
+							<div class="flex items-center gap-0.5">
+								<svws-ui-button type="icon" @click="gotoBenutzergruppe(row.id)">
+									<span class="icon i-ri-link" />
+								</svws-ui-button>
+								{{ row.bezeichnung }}
 							</div>
 						</div>
-					</template>
-					<template #body>
-						<div role="row" class="svws-ui-tr gruppen-tr" v-for="row in listBenutzergruppen" :key="row.id">
-							<div class="svws-ui-td svws-align-center" role="cell">
-								<svws-ui-checkbox type="toggle" :model-value="getBenutzerManager().istInGruppe(row.id)" @update:model-value="val => val ? addBenutzerToBenutzergruppe(row.id) : removeBenutzerFromBenutzergruppe(row.id)" />
-							</div>
-							<div class="svws-ui-td" role="cell">
-								<div class="flex items-center gap-0.5">
-									<svws-ui-button type="icon" @click="gotoBenutzergruppe(row.id)">
-										<span class="icon i-ri-link" />
-									</svws-ui-button>
-									{{ row.bezeichnung }}
-								</div>
-							</div>
-							<div class="svws-ui-td" role="cell">
-								<svws-ui-tooltip v-if="row.istAdmin">
-									<span class="icon i-ri-shield-star-line h-5 w-5 -m-0.5" />
-									<template #content>Administrative Gruppe</template>
-								</svws-ui-tooltip>
-							</div>
+						<div class="svws-ui-td" role="cell">
+							<svws-ui-tooltip v-if="row.istAdmin">
+								<span class="icon i-ri-shield-star-line h-5 w-5 -m-0.5" />
+								<template #content>Administrative Gruppe</template>
+							</svws-ui-tooltip>
 						</div>
-					</template>
-				</svws-ui-table>
-			</svws-ui-content-card>
+					</div>
+				</template>
+			</svws-ui-table>
 		</div>
-		<svws-ui-content-card title="Einstellungen">
-			<template #actions>
-				<template v-if="getBenutzerManager().istInAdminGruppe()">
-					<span class="inline-flex gap-1 leading-none">
-						<span class="icon i-ri-shield-star-line shrink-0 -mt-0.5" />
-						<span>Administrator mit allen Kompetenzen</span>
-					</span>
-				</template>
-				<template v-else>
-					<svws-ui-checkbox type="toggle" v-model="inputIstAdmin">
-						Alle Kompetenzen freigeben
-					</svws-ui-checkbox>
-				</template>
-			</template>
-			<svws-ui-table :items="kompetenzgruppen">
+		<div class="flex flex-col gap-4">
+			<div class="text-headline-md">Einstellungen</div>
+			<div v-if="getBenutzerManager().istInAdminGruppe()">
+				<span class="inline-flex gap-1 leading-none">
+					<span class="icon i-ri-shield-star-line shrink-0 -mt-0.5" />
+					<span>Administrator mit allen Kompetenzen</span>
+				</span>
+			</div>
+			<div v-else>
+				<svws-ui-checkbox type="toggle" v-model="inputIstAdmin">
+					Alle Kompetenzen freigeben
+				</svws-ui-checkbox>
+			</div>
+			<svws-ui-table :items="kompetenzgruppen" scroll>
 				<template #header>
 					<div class="svws-ui-tr kompetenz-tr" role="row">
 						<div class="svws-ui-td" role="columnheader" :class="{'col-span-2': getBenutzerManager().istAdmin()}">Kompetenz</div>
@@ -78,7 +76,7 @@
 					</template>
 				</template>
 			</svws-ui-table>
-		</svws-ui-content-card>
+		</div>
 	</div>
 </template>
 
