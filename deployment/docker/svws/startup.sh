4801e8d447735381359286c9bbb24b317aa7e0f9
Michael Ulrich <michael.ulrich@netcologne-its.de>
2025-02-26 13:32:21 +0100

    Verbesserung des Docker Images: - Hinzufügen eines mysqladmin CLI - Einbau einer Schleife zur überprüfung ob die Datenbank bereit für Verbindungen ist - Verbessern der Standard Umgebungsvariablen - TLS und TLS 1.1 über Umgebungsvariablen konfigurierbar * Docker Compose Compilance verbessert

--- deployment/docker/svws/startup.sh
+++ deployment/docker/svws/startup.sh
@@ -18,6 +18,18 @@ if [[ -d $INIT_SCRIPTS_DIR ]]; then
 	done
 fi
 
+# prüfen ob MariaDB läuft und ggf. warten
+for seconds in {1..60};
+do 
+	STARTED=$(mysqladmin ping -h $$MARIADB_HOST -u $$MARIADB_USER --password=$$MARIADB_PASSWORD | grep "mysqld is alive")
+	if [ "$STARTED" != "" ]; then
+		echo "MariaDB is running"
+		break
+	else
+		sleep 1
+	fi
+done
+
 # SVWS-Server starten
 echo "Starte SVWS-Server ..."
 java -cp "svws-server-app-*.jar:./*:./lib/*" de.svws_nrw.server.jetty.Main
