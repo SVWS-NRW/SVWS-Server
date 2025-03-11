18c37a5cf8090a7a7b9de5c7bc873481fc386782
hmt <dev@hmt.im>
2025-03-11 10:32:10 +0100

    update deps und Bugfix TW Vite-Plugin

--- svws-webclient/admin/src/main.ts
+++ svws-webclient/admin/src/main.ts
@@ -9,14 +9,6 @@ import SWrapper from "~/components/SWrapper.vue";
 const app = createApp(SWrapper);
 app.use(router);
 
-app.mixin({
-	created() {
-		const title = this.$options.title;
-		if (title !== undefined) {
-			document.title = title;
-		}
-	},
-});
 router.isReady()
 	.then(() => app.mount("#app"))
 	.catch((e: unknown) => { throw e });
--- svws-webclient/enmserver/src/main.ts
+++ svws-webclient/enmserver/src/main.ts
@@ -2,8 +2,7 @@ import { createApp } from "vue";
 import { router } from "./router/RouteManager";
 
 import "@ui/assets/styles/index.css";
-import "@ui/assets/styles/colors.css";
-import "./main.css";
+// import "./main.css";
 
 import SWrapper from "~/components/SWrapper.vue";
 
