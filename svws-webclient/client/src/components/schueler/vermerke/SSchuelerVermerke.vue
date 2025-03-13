dc04bc52e2683bbc07aafb9ed9ab07a0b656856f
Robin Haensse ]init[ <robin.haensse@init.de>
2025-03-13 08:39:38 +0000

    Resolve "Client: Bugs/Verbesserungsvorschläge SchülerVermerke"
    Co-authored-by: hmt <dev@hmt.im>

--- svws-webclient/client/src/components/schueler/vermerke/SSchuelerVermerke.vue
+++ svws-webclient/client/src/components/schueler/vermerke/SSchuelerVermerke.vue
@@ -1,20 +1,21 @@
 <template>
 	<div class="page page-grid-cards">
 		<div>
-			<svws-ui-checkbox :model-value="filterNurSichtbare" @update:model-value="value => setFilterNurSichtbare(value)">Unsichtbare ausblenden</svws-ui-checkbox>
-			<svws-ui-button v-autofocus class="contentFocusField mt-4" @click="addWrapper">
-				<span class="icon i-ri-chat-new-line" />
-				<span class="ml-2">Neuen Vermerk hinzufügen</span>
-			</svws-ui-button>
+			<div class="flex justify-between">
+				<svws-ui-button v-autofocus class="contentFocusField min-h-[32px]" @click="addWrapper">
+					<span class="icon i-ri-chat-new-line" />
+					<span class="ml-2">Neuen Vermerk hinzufügen</span>
+				</svws-ui-button>
+				<svws-ui-checkbox class="self-center" :model-value="filterNurSichtbare" @update:model-value="value => setFilterNurSichtbare(value)">Nur <b>sichtbare</b> Vermerkarten anzeigen</svws-ui-checkbox>
+			</div>
 			<div class="flex flex-col gap-4 mt-4">
 				<template v-for="vermerk of listVermerke" :key="vermerk.id">
-					<ui-card icon="i-ri-message-line" :title="getTitle(vermerk)" :subtitle="getDescription(vermerk)" :is-open="activeVermerk?.id === vermerk.id"
-						@update:is-open="(isOpen) => setCurrentVermerk(vermerk, isOpen)">
+					<ui-card icon="i-ri-message-line" :title="getTitle(vermerk)" :subtitle="getDescription(vermerk)" :is-open="activeVermerk?.id === vermerk.id">
 						<svws-ui-input-wrapper class="px-6">
 							<svws-ui-textarea-input	v-model="vermerk.bemerkung" :autoresize="true" :rows="4" @change="bemerkung => patch({ bemerkung: String(bemerkung) }, vermerk.id)" />
-							<div class="flex w-144">
+							<div class="flex w-200">
 								<p class="my-auto mr-4">Vermerkart:</p>
-								<svws-ui-select class="bg-ui-contrast-0" title="Bitte wählen" headless :model-value="mapVermerkArten.get(vermerk.idVermerkart || -1)" :items="mapVermerkArten.values()" :item-text="item => item.bezeichnung"
+								<svws-ui-select class="bg-ui-contrast-0 mr-4" title="Bitte wählen" headless :model-value="mapVermerkArten.get(vermerk.idVermerkart || -1)" :items="mapVermerkArten.values()" :item-text="item => getItemText(item)"
 									@update:model-value="art => (art !== null) && patch({ idVermerkart: art?.id ?? -1 }, vermerk.id)" />
 							</div>
 							<div class="w-full flex justify-between">
@@ -44,43 +45,29 @@
 
 <script setup lang="ts">
 
+	import { computed, ref } from "vue";
 	import type { SchuelerVermerkeProps } from "./SSchuelerVermerkeProps";
+	import { ArrayList, DateUtils, type SchuelerVermerke, type VermerkartEintrag} from "@core";
 
 	const props = defineProps<SchuelerVermerkeProps>();
 
-	import { computed, ref } from "vue";
-	import { ArrayList, DateUtils, type SchuelerVermerke } from "@core";
-
 	const activeVermerk = ref<SchuelerVermerke>();
-	const oldAction = ref<{ vermerk: SchuelerVermerke | undefined; open: boolean }>({
-		vermerk: undefined,
-		open: false,
-	});
 
 	const listVermerke = computed(() => {
 		if (!props.filterNurSichtbare)
 			return props.schuelerVermerke();
 		const liste = new ArrayList<SchuelerVermerke>();
 		for (const item of props.schuelerVermerke()) {
-			if (item.idVermerkart === null)
+			if (item.idVermerkart === null) {
+				liste.add(item);
 				continue;
+			}
 			const art = props.mapVermerkArten.get(item.idVermerkart);
-			if (art !== undefined && art.istSichtbar)
+			if ((art !== undefined) && art.istSichtbar)
 				liste.add(item);
 		}
 		return liste;
 	})
-
-	function setCurrentVermerk(newAction: SchuelerVermerke | undefined, open: boolean) {
-		if(newAction?.id === oldAction.value.vermerk?.id && !open)
-			return;
-		oldAction.value.vermerk = activeVermerk.value;
-		oldAction.value.open = (activeVermerk.value === undefined) ? false : true;
-		if(open === true)
-			activeVermerk.value = newAction;
-		else
-			activeVermerk.value = undefined;
-	}
 
 	function getDate(vermerk: SchuelerVermerke) {
 		return DateUtils.gibDatumGermanFormat(vermerk.datum ?? new Date().toISOString());
@@ -88,11 +75,17 @@
 
 	async function addWrapper() {
 		await props.add();
-		activeVermerk.value = [... props.schuelerVermerke()][props.schuelerVermerke().size() -1];
+		activeVermerk.value = props.schuelerVermerke().getLast();
 	}
 
 	function getTitle(vermerk: SchuelerVermerke) {
-		return `${props.mapVermerkArten.get(vermerk.idVermerkart ?? -1)?.bezeichnung ?? "Neuer Vermerk"}: ${vermerk.bemerkung}`;
+		const title = `${props.mapVermerkArten.get(vermerk.idVermerkart ?? -1)?.bezeichnung ?? "Neuer Vermerk"}: ${vermerk.bemerkung}`
+		return title.length > 50 ? title.substring(0, 50) + "..." : title;
+	}
+
+	function getItemText(item: VermerkartEintrag) {
+		const vermerkArtIsVisible = !item.istSichtbar;
+		return `${item.bezeichnung} ${vermerkArtIsVisible ? '(nicht sichtbar)' : ''}`
 	}
 
 	function getDescription(vermerk: SchuelerVermerke) : string {
