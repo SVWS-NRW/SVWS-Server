6d83aaca76f8f129423f16bf60775f29f9982087
hmt <dev@hmt.im>
2025-03-19 17:54:30 +0100

    Client/Stundenplan: Korrektur Pausenzeiten Lehrerliste
    [ci-skip]

--- svws-webclient/client/src/components/stundenplan/pausen/StundenplanPausen.vue
+++ svws-webclient/client/src/components/stundenplan/pausen/StundenplanPausen.vue
@@ -11,12 +11,11 @@
 		</Teleport>
 		<div v-if="hatUpdateKompetenz" class="min-w-72 h-full overflow-y-auto w-72 border-2 rounded-xl relative" :class="[dragFromPausenzeit === undefined ? 'border-ui-neutral' : 'border-ui-danger ring-4 ring-ui-danger border-dashed']" @drop.stop="onDrop" @dragover.prevent="dropZone = true" @dragleave="setDragLeave">
 			<div class="fixed flex items-center justify-center h-3/4 w-64 z-20 pointer-events-none"><span :class="dragFromPausenzeit === undefined ? '':'icon-lg icon-ui-danger-secondary i-ri-delete-bin-line scale-[4]'" /></div>
-			<div class="svws-ui-table svws-type-navigation" style="scrollbar-gutter: stable; scrollbar-width: thin; scrollbar-color: rgba(0,0,0,0.2) transparent;">
+			<div class="svws-ui-table" style="scrollbar-gutter: stable; scrollbar-width: thin; scrollbar-color: rgba(0,0,0,0.2) transparent;">
 				<div class="svws-ui-tbody">
-					<div v-for="lehrer in stundenplanManager().lehrerGetMengeAsList()" :key="lehrer.id" class="svws-ui-tr" :class="dragLehrer ? 'cursor-grabbing' : 'cursor-grab'">
-						<div class="svws-ui-td">
-							<div class="svws-ui-badge select-none group flex place-items-center w-full"
-								@dragstart="onDrag(lehrer)" @dragend="dragEnd" draggable="true">
+					<div v-for="lehrer in stundenplanManager().lehrerGetMengeAsList()" :key="lehrer.id" class="svws-ui-tr w-full" :class="dragLehrer ? 'cursor-grabbing' : 'cursor-grab'">
+						<div class="svws-ui-td border-0!">
+							<div class="svws-ui-badge select-none group flex place-items-center w-full" @dragstart="onDrag(lehrer)" @dragend="dragEnd" draggable="true">
 								<span class="icon i-ri-draggable inline-block icon-ui-contrast-75 opacity-60 group-hover:opacity-100 group-hover:icon-ui-contrast-75 rounded-xs" />
 								<span class="truncate grow p-1"> {{ lehrer.kuerzel }} ({{ lehrer.vorname[0] }}. {{ lehrer.nachname }})</span>
 								<span class="rounded-lg bg-ui-brand text-ui-onbrand px-1 py-0.5 text-sm break-normal" v-if="mapLehrerPausenaufsichten.get(lehrer.id)?.size()">
