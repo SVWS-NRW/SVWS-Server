ed8314db67e21ca875e42bf113ab1b620b88d0b1
Thomas Hann <hat@gesa-soest.de>
2025-05-05 13:00:50 +0000

    Resolve "Abmelde-Button im Client der Laufbahnplanung (LuPO)"

--- svws-webclient/laufbahnplanung/src/components/LaufbahnplanungOberstufe.vue
+++ svws-webclient/laufbahnplanung/src/components/LaufbahnplanungOberstufe.vue
@@ -8,8 +8,7 @@
 			<div class="flex flex-col gap-2">
 				<div class="flex gap-3">
 					<svws-ui-modal-hilfe> <hilfe-laufbahnplanung /> </svws-ui-modal-hilfe>
-					<a href="https://www.svws.nrw.de/faq/impressum"> <svws-ui-button type="secondary"> Impressum </svws-ui-button> </a>
-					<a href="#"> <svws-ui-button type="secondary"> Datenschutz </svws-ui-button> </a>
+					<svws-ui-button type="danger" @click="handleClick"><span class="icon-sm i-ri-logout-circle-line" /> Abmelden </svws-ui-button>
 				</div>
 				<div><span>{{ version }}</span> <span v-if="version.includes('SNAPSHOT')">{{ githash.substring(0, 8) }}</span></div>
 			</div>
@@ -19,8 +18,8 @@
 	<svws-ui-tab-bar :tab-manager="() => tabManager">
 		<Teleport defer to=".svws-sub-nav-target">
 			<svws-ui-sub-nav>
-				<svws-ui-button type="transparent" @click="export_laufbahnplanung"><span class="icon-sm i-ri-upload-2-line" />Exportieren</svws-ui-button>
-				<svws-ui-button type="transparent" @click="showModalImport = true"><span class="icon-sm i-ri-download-2-line" /> Importieren…</svws-ui-button>
+				<svws-ui-button type="transparent" @click="export_laufbahnplanung"><span class="icon-sm i-ri-upload-2-line" /> Speichern</svws-ui-button>
+				<svws-ui-button type="transparent" @click="showModalImport = true"><span class="icon-sm i-ri-download-2-line" /> Öffnen</svws-ui-button>
 				<s-laufbahnplanung-import-modal :show="showModalImport" :import-laufbahnplanung="import_laufbahnplanung" @update:show="val => showModalImport = val" />
 				<svws-ui-button :type="zwischenspeicher === undefined ? 'transparent' : 'error'" @click="saveLaufbahnplanung">Planung merken</svws-ui-button>
 				<svws-ui-button type="danger" @click="restoreLaufbahnplanung" v-if="zwischenspeicher !== undefined">Planung wiederherstellen</svws-ui-button>
@@ -44,6 +43,17 @@
 		</div>
 		<div v-else class="page page-flex-row">Die Laufbahnplanung hat kein gültiges Abiturjahr, bitte prüfen Sie die importierte Datei.</div>
 	</svws-ui-tab-bar>
+	<!-- Modal zum Speichern der Laufbandatei -->
+	<svws-ui-modal :show>
+		<template #modalTitle>Laufbahnplanung speichern</template>
+		<template #modalContent>
+			Möchten Sie die Laufbahnplanungsdatei vor der Abmeldung speichern?
+			<div class="mt-7 flex flex-row gap-4 justify end">
+				<svws-ui-button type="secondary" @click="exitLaufbahnplanung">Nein</svws-ui-button>
+				<svws-ui-button @click="exit_laufbahnplanung">Speichern</svws-ui-button>
+			</div>
+		</template>
+	</svws-ui-modal>
 </template>
 
 <script setup lang="ts">
@@ -57,6 +67,8 @@
 	import { LaufbahnplanungUiManager } from "@ui/components/gost/laufbahnplanung/LaufbahnplanungUiManager";
 
 	const props = defineProps<LaufbahnplanungOberstufeProps>();
+
+	const show = ref<boolean>(false);
 
 	const manager = computed<LaufbahnplanungUiManager>(() =>
 		new LaufbahnplanungUiManager(props.serverMode, props.abiturdatenManager, props.config, () => props.gostJahrgangsdaten, props.setWahl, false, true));
@@ -75,8 +87,17 @@
 		URL.revokeObjectURL(link.href);
 	}
 
+	async function exit_laufbahnplanung() {
+		await export_laufbahnplanung();
+		await props.exitLaufbahnplanung();
+	}
+
 	async function import_laufbahnplanung(formData: FormData) {
 		await props.importLaufbahnplanung(formData);
+	}
+
+	async function handleClick() {
+		return props.dirty() ? (show.value = true) : await props.exitLaufbahnplanung();
 	}
 
 </script>
