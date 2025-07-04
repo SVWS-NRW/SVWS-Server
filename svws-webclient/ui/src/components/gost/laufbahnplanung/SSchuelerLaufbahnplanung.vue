185cf5bd6030e4fb2da04cbfff6397b53c2c36b4
hmt <dev@hmt.im>
2025-07-04 11:24:33 +0200

    Clients/Laufbahnplanung nur mit einer Hilfe

--- svws-webclient/ui/src/components/gost/laufbahnplanung/SSchuelerLaufbahnplanung.vue
+++ svws-webclient/ui/src/components/gost/laufbahnplanung/SSchuelerLaufbahnplanung.vue
@@ -1,7 +1,4 @@
 <template>
-	<Teleport to=".svws-ui-header--actions" defer>
-		<svws-ui-modal-hilfe> <hilfe-schueler-laufbahnplanung /> </svws-ui-modal-hilfe>
-	</Teleport>
 	<div class="page page-flex-row max-w-480">
 		<Teleport v-if="hatUpdateKompetenz" defer to=".svws-sub-nav-target">
 			<svws-ui-sub-nav>
@@ -44,6 +41,7 @@
 	import { LaufbahnplanungUiManager } from "./LaufbahnplanungUiManager";
 
 	const props = defineProps<SchuelerLaufbahnplanungProps>();
+	console.log(props)
 
 	const manager = computed<LaufbahnplanungUiManager>(() => new LaufbahnplanungUiManager(props.serverMode, props.abiturdatenManager, props.config, () => props.gostJahrgangsdaten, props.setWahl, { faecherZeigen: "app.schueler.laufbahnplanung.faecher.anzeigen", modus: "app.schueler.laufbahnplanung.modus" }));
 
