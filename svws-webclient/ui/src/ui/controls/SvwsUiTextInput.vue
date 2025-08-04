66bc6a38594ebdaff0852e1cf910a411d2771fa5
hmt <dev@hmt.im>
2025-08-04 18:14:18 +0200

    UI/InputNumber: Schloss ergänzen, Stepper raus, wenn readonly
    siehe #2459 [ci-skip]

--- svws-webclient/ui/src/ui/controls/SvwsUiTextInput.vue
+++ svws-webclient/ui/src/ui/controls/SvwsUiTextInput.vue
@@ -90,7 +90,7 @@
 					</template>
 				</svws-ui-tooltip>
 			</span>
-			<span v-if="readonly && !isSelectInput" class="icon-xs i-ri-lock-line ml-1" :class="{ 'icon-ui-statistic': statistics }" />
+			<span v-if="readonly && !isSelectInput" class="icon-xs i-ri-lock-line" />
 		</span>
 		<span v-if="removable && (type === 'date') && (!readonly)" @keydown.enter="updateData('')" @click.stop="updateData('')" class="svws-icon--remove icon i-ri-close-line" tabindex="0" />
 		<span v-if="(type === 'date') && !firefox()" class="svws-icon icon i-ri-calendar-2-line" />
