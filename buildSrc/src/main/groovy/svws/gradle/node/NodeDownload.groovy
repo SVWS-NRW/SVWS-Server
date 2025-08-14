060052454542431edc443c04487c58580b74aa55
ThomasBachran <thomas.bachran@gmx.de>
2025-08-14 15:17:20 +0200

    fixed issues regarding gradle deprecation warnings

--- buildSrc/src/main/groovy/svws/gradle/node/NodeDownload.groovy
+++ buildSrc/src/main/groovy/svws/gradle/node/NodeDownload.groovy
@@ -1,11 +1,13 @@
 package svws.gradle.node;
 
+import javax.inject.Inject
 import org.apache.tools.ant.Project
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.Copy;
 import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.file.FileSystemOperations
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.file.RelativePath;
 
@@ -13,6 +15,9 @@ abstract class NodeDownload extends DefaultTask {
 
 	@Internal
 	NodePluginConfig cfg;
+	
+	@Inject
+	abstract FileSystemOperations getFs()
 
 	NodeDownload() {
 		group = 'node'
@@ -28,7 +33,7 @@ abstract class NodeDownload extends DefaultTask {
 		if (file.exists()) {
 			println "Datei ${filename} wurde bereits heruntergeladen, skipping download"
 		} else {
-			project.mkdir(downloadPath);
+			fs.mkdir(downloadPath);
 			def url = this.cfg.getDownloadURL();
 			println "Downloading ${filename} from ${url}...";
 			def username = this.cfg.getDownloadUser();
@@ -42,14 +47,14 @@ abstract class NodeDownload extends DefaultTask {
 
 		def compFilename = this.cfg.getCompressedFilename();
 		if ("zip".equals(this.cfg.getCompressedFileType())) {
-			project.copy {
+			fs.copy {
 				eachFile { f ->	f.relativePath = new RelativePath(true, f.relativePath.segments.drop(1)) }
 				from project.zipTree("${downloadPath}/${filename}").matching{ include "${compFilename}/**" };				
 				includeEmptyDirs = false    
 				into this.cfg.getNodeDirectory()
 			}
 		} else if ("tar.gz".equals(this.cfg.getCompressedFileType())) {
-			project.exec {
+			fs.exec {
 				commandLine 'tar', '-xzf', "${downloadPath}/${filename}", '--strip-components=1','-C', this.cfg.getNodeDirectory() + "/";
 			}
 		} else if ("tar.xz".equals(this.cfg.getCompressedFileType())) {
