07e3426f5056ac247b947ef583b6d57fdb152df1
hmt <dev@hmt.im>
2025-11-24 10:01:24 +0100

    notenmodul: Zeige Config und Sync nur, wenn Verbindung vorhanden

--- svws-webclient/client/src/router/apps/notenmodul/RouteNotenmodulKonfiguration.ts
+++ svws-webclient/client/src/router/apps/notenmodul/RouteNotenmodulKonfiguration.ts
@@ -1,11 +1,12 @@
-import { BenutzerKompetenz, Schulform, ServerMode } from "@core";
+import { BenutzerKompetenz, Schulform, ServerMode, type DeveloperNotificationException } from "@core";
 
 import { RouteNode } from "~/router/RouteNode";
 import { routeNotenmodulAdministration, type RouteNotenmodulAdministration } from "./RouteNotenmodulAdministration";
-import type { RouteLocationNormalized } from "vue-router";
+import type { RouteLocationNormalized, RouteParams } from "vue-router";
 import type { NotenmodulKonfigurationProps } from "~/components/notenmodul/NotenmodulKonfigurationProps";
 import { api } from "~/router/Api";
 import { ConfigElement } from "@ui";
+import { routeError } from "~/router/error/RouteError";
 
 const NotenmodulKonfiguration = () => import("~/components/notenmodul/NotenmodulKonfiguration.vue");
 
@@ -18,10 +19,22 @@ export class RouteNotenmodulKonfiguration extends RouteNode<any, RouteNotenmodul
 		super.mode = ServerMode.DEV;
 		super.propHandler = (route) => this.getProps(route);
 		super.text = "Konfiguration";
+		this.isHidden = (params?: RouteParams) => this.checkHidden(params);
 		api.config.addElements([
 			new ConfigElement("notenmodul.leistungen.tabelle.spaltenanzeige", "global", `[["Kurs", "true"], ["Kursart", "true"], ["Lehrer", "true"], ["Quartal", "true"], ["Note", "true"], ["Mahnung", "true"], ["FS", "true"], ["FSU", "true"], ["Bemerkung", " true"]]`),
 			new ConfigElement("notenmodul.teilleistungen.tabelle.spaltenanzeige", "global", "null"),
 		]);
+	}
+
+	protected checkHidden(params?: RouteParams) {
+		try {
+			const { id } = (params === undefined) ? { id: undefined } : RouteNode.getIntParams(params, ["id"]);
+			if ((id !== undefined) && !routeNotenmodulAdministration.data.manager.getConnectionResponse(id).success)
+				return routeNotenmodulAdministration.getRouteDefaultChild({ id });
+			return false;
+		} catch (e) {
+			return routeError.getSimpleErrorRoute(e as DeveloperNotificationException);
+		}
 	}
 
 
--- svws-webclient/client/src/router/apps/notenmodul/RouteNotenmodulSynchronisation.ts
+++ svws-webclient/client/src/router/apps/notenmodul/RouteNotenmodulSynchronisation.ts
@@ -26,6 +26,8 @@ export class RouteNotenmodulSynchronisation extends RouteNode<any, RouteNotenmod
 			const { id } = (params === undefined) ? { id: undefined } : RouteNode.getIntParams(params, ["id"]);
 			if (id === -1)
 				return routeNotenmodulAdministration.getRouteDefaultChild({ id });
+			if ((id !== undefined) && !routeNotenmodulAdministration.data.manager.getConnectionResponse(id).success)
+				return routeNotenmodulAdministration.getRouteDefaultChild({ id });
 			return false;
 		} catch (e) {
 			return routeError.getSimpleErrorRoute(e as DeveloperNotificationException);
