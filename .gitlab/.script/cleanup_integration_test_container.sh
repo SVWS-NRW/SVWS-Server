44790eb96ecd2632f8108b4f8bdd63003f1c7eb2
Tim Hermbecker ]init[ <tim.hermbecker@init.de>
2025-11-27 08:17:14 +0000

    Gitlab CI: Integrationtest Container Cleanup Job Verbesserungen

--- .gitlab/.script/cleanup_integration_test_container.sh
+++ .gitlab/.script/cleanup_integration_test_container.sh
@@ -11,8 +11,8 @@ CONTAINER_ID="$1"
 echo "🧹 Entferne Integration-Test Container und Images (ID: ${CONTAINER_ID}) ..."
 
 # stop and remove running integration-test-job containers
-docker ps --filter name=$CONTAINER_ID --filter status=running --filter name=gradled -aq | xargs -r docker stop
-docker ps -a --filter name=$CONTAINER_ID --filter name=gradled -aq | xargs -r docker rm
-docker images --filter reference="*${CONTAINER_ID}*gradled" -q | xargs -r docker rmi
+docker ps --filter name="-${CONTAINER_ID}" --filter status=running -aq | xargs -r docker stop
+docker ps -a --filter name="-${CONTAINER_ID}" -aq | xargs -r docker rm
+docker images --filter reference="-${CONTAINER_ID}" -q | xargs -r docker rmi
 
 echo "✅ Integration-Test Container und Images wurden erfolgreich entfernt! (ID: ${CONTAINER_ID}) ..."
