949255702b5bb7ba9bb04b7938146b0dcd518eb4
hmt <dev@hmt.im>
2025-12-15 08:34:45 +0100

    node: update node und deps

--- buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
+++ buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
@@ -37,10 +37,10 @@ abstract class NodePluginConfig {
 		this.node_download_user = p.hasProperty('node_download_user') ? p.node_download_user : System.getenv("NODE_DOWNLOAD_USER")
 		this.node_download_passwd = p.hasProperty('node_download_passwd') ? p.node_download_passwd : System.getenv("NODE_DOWNLOAD_PASSWD")
 		url.convention('https://nodejs.org/dist/v')
-		version.convention('24.11.1') // https://nodejs.org/en/download/prebuilt-installer
+		version.convention('24.12.0') // https://nodejs.org/en/download/prebuilt-installer
 		npmVersion.convention('11.6.2')
 		tsVersion.convention('5.9.3') // https://github.com/microsoft/TypeScript/releases
-		tsNodeTypesVersion.convention('24.9.1') // https://www.npmjs.com/package/@types/node
+		tsNodeTypesVersion.convention('25.0.2') // https://www.npmjs.com/package/@types/node
 	}
 
 	boolean isLinux() {
