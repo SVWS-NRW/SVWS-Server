d905269fa4c70d786cc14bfc692cfab2c273e4d3
hmt <dev@hmt.im>
2026-01-20 08:20:06 +0100

    node: update 24.13 und deps

--- buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
+++ buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
@@ -37,7 +37,7 @@ abstract class NodePluginConfig {
 		this.node_download_user = p.hasProperty('node_download_user') ? p.node_download_user : System.getenv("NODE_DOWNLOAD_USER")
 		this.node_download_passwd = p.hasProperty('node_download_passwd') ? p.node_download_passwd : System.getenv("NODE_DOWNLOAD_PASSWD")
 		url.convention('https://nodejs.org/dist/v')
-		version.convention('24.12.0') // https://nodejs.org/en/download/prebuilt-installer
+		version.convention('24.13.0') // https://nodejs.org/en/download/prebuilt-installer
 		npmVersion.convention('11.6.2')
 		tsVersion.convention('5.9.3') // https://github.com/microsoft/TypeScript/releases
 		tsNodeTypesVersion.convention('25.0.2') // https://www.npmjs.com/package/@types/node
