f7d2a7dfd51cf95f032dae7a9419a4eb0774e530
hmt <dev@hmt.im>
2026-04-02 18:17:02 +0200

    🥳 Revert "Korrektur fokus und listener im Select"
    This reverts commit 35997c1ff96830f424e8726706b1c37a546da7a5.

--- svws-webclient/ui/src/ui/SvwsUiAvatar.vue
+++ svws-webclient/ui/src/ui/SvwsUiAvatar.vue
@@ -44,8 +44,7 @@
 				</template>
 			</template>
 			<template v-else>
-				<img v-if="isEE" src="../assets/img/ee.svg" alt="">
-				<svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
 					<path d="M20 22h-2v-2a3 3 0 0 0-3-3H9a3 3 0 0 0-3 3v2H4v-2a5 5 0 0 1 5-5h6a5 5 0 0 1 5 5v2zm-8-9a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8 4 4 0 0 0 0 8z" />
 				</svg>
 				<img :src :alt @error="onError">
@@ -74,8 +73,6 @@
 		'image:captured': [value: Blob | null];
 		'image:base64': [val: string | null];
 	}>();
-
-	const isEE = computed<boolean>(() => JSON.parse(localStorage.getItem('ee') ?? ''));
 
 	const hasVideoDevice = ref(false);
 
