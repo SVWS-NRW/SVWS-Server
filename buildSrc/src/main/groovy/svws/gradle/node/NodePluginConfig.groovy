b5222b08b78d495c98f7ada77fa43a942944ea61
Bernd Homuth <dev@hmt.im>
2026-04-07 10:20:11 +0200

    Hmt/vitestupdate

--- buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
+++ buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
@@ -37,7 +37,7 @@ abstract class NodePluginConfig {
 		this.node_download_user = p.hasProperty('node_download_user') ? p.node_download_user : System.getenv("NODE_DOWNLOAD_USER")
 		this.node_download_passwd = p.hasProperty('node_download_passwd') ? p.node_download_passwd : System.getenv("NODE_DOWNLOAD_PASSWD")
 		url.convention('https://nodejs.org/dist/v')
-		version.convention('24.14.0') // https://nodejs.org/en/download/prebuilt-installer
+		version.convention('24.14.1') // https://nodejs.org/en/download/prebuilt-installer
 		npmVersion.convention('11.9.0')
 		tsVersion.convention('5.9.3') // https://github.com/microsoft/TypeScript/releases
 		tsNodeTypesVersion.convention('25.3.5') // https://www.npmjs.com/package/@types/node
