16967b5734e64ebad680615c8d01f2574a31d00c
Bernd Homuth <dev@hmt.im>
2026-05-27 12:20:08 +0200

    Hmt/2605upgradedeps

--- buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
+++ buildSrc/src/main/groovy/svws/gradle/node/NodePluginConfig.groovy
@@ -37,10 +37,10 @@ abstract class NodePluginConfig {
 		this.node_download_user = p.hasProperty('node_download_user') ? p.node_download_user : System.getenv("NODE_DOWNLOAD_USER")
 		this.node_download_passwd = p.hasProperty('node_download_passwd') ? p.node_download_passwd : System.getenv("NODE_DOWNLOAD_PASSWD")
 		url.convention('https://nodejs.org/dist/v')
-		version.convention('24.15.0') // https://nodejs.org/en/download/prebuilt-installer
-		npmVersion.convention('11.12.1')
-		tsVersion.convention('5.9.3') // https://github.com/microsoft/TypeScript/releases
-		tsNodeTypesVersion.convention('25.3.5') // https://www.npmjs.com/package/@types/node
+		version.convention('24.16.0') // https://nodejs.org/en/download/prebuilt-installer
+		npmVersion.convention('11.13.0')
+		tsVersion.convention('6.0.3') // https://github.com/microsoft/TypeScript/releases
+		tsNodeTypesVersion.convention('25.9.1') // https://www.npmjs.com/package/@types/node
 	}
 
 	boolean isLinux() {
