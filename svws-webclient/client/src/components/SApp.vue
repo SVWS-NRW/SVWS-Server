73e67eddde5d0649feef7da90620bb088fd16ccc
hmt <dev@hmt.im>
2026-06-01 09:23:55 +0200

    client/Menü: Disable Abschnittauswahl, wenn Notenmodul aktiv

--- svws-webclient/client/src/components/SApp.vue
+++ svws-webclient/client/src/components/SApp.vue
@@ -62,7 +62,7 @@
 				<div class="h-full flex flex-col">
 					<div class="secondary-menu--headline">
 						<h1> {{ menu.mainEntry.text }} </h1>
-						<div><abschnitt-auswahl :daten="schuljahresabschnittsauswahl" /></div>
+						<div><abschnitt-auswahl :disabled="menu.current.name.startsWith('notenmodul')" :daten="schuljahresabschnittsauswahl" /></div>
 					</div>
 					<div class="secondary-menu--header" />
 					<div class="secondary-menu--content">
