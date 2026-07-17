490f82958b4ce3f015e01e7600bc8531b0b9c3ad
ThomasBachran <thomas.bachran@gmx.de>
2026-07-17 20:40:02 +0200

    Version auf 1.5.0-SNAPSHOT gestellt

--- svws-webclient/enmserver/src/php/app/Version.php
+++ svws-webclient/enmserver/src/php/app/Version.php
@@ -1,6 +1,6 @@
 <?php
 namespace wenom;
 class Version {
-    public const VERSION = "1.4.0-SNAPSHOT";
-    public const GITHASH = "f5e33d7ed0b224d4ea7a91f3374adfbcfe1c343a";
+    public const VERSION = "1.5.0-SNAPSHOT";
+    public const GITHASH = "ec3b0a362b70359562cd68e418ed4a5dc55aaace";
 }
