3aba199c47b611d885db05675be28faba2c9309c
Benjamin Bartsch <benjamin.bartsch@gymnasium-eickel.nrw.schule>
2026-09-17 10:08:42 +0000

    Resolve "GostKursBelegungService: Potentieller Fehler in Zeile 105"

--- svws-db-utils/src/main/java/de/svws_nrw/service/gost/GostKursBelegungService.java
+++ svws-db-utils/src/main/java/de/svws_nrw/service/gost/GostKursBelegungService.java
@@ -102,6 +102,7 @@ public final class GostKursBelegungService {
 		return (schueler != null)
 				&& (schuljahresabschnitt != null)
 				&& !schueler.Geloescht
+				&& (schueler.idStatus != null)
 				&& validStatus.contains(schueler.idStatus)
 				&& istKursartSchriftlich(leistung.Kursart, lernabschnitt.ASDJahrgang, schuljahresabschnitt.Abschnitt);
 	}
